ed78dd111cb0fc42de6794a5dd696d78e6bfdbe6
Darryl Yeo <dev@darryl-yeo.com>
2020-10-12 02:25:23 -0700

    UX changes

--- src/routes/trade/index.svelte
+++ src/routes/trade/index.svelte
@@ -9,9 +9,15 @@
 <h1>Trade</h1>
 
 <h2>Pick your mode of transport:</h2>
-<a href="trade/vega"><button>Vega</button></a>
-<a href="trade/uniswap"><button>Uniswap</button></a>
+<div>
+	<a href="trade/vega"><button>Vega</button></a>
+	<a href="trade/uniswap"><button>Uniswap</button></a>
+	<a href="trade/balancer"><button>Balancer</button></a>
+</div>
 
 <h2>Express</h2>
-<p>Need to swap tokens right away? Let one of these fine travel agencies decide the optimal transport method for you!</p>
-<a href="trade/1inch"><button>1inch</button></a>
+<div>
+	<p>Need to swap tokens right away? Let one of these fine travel agencies decide the optimal transport method for you!</p>
+	<a href="trade/1inch"><button>1inch</button></a>
+	<a href="trade/matcha"><button>Matcha</button></a>
+</div>
