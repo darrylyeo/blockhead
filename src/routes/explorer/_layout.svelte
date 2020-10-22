727092aefa9f43d1577886c00c4e3d8695603e7a
Darryl Yeo <dev@darryl-yeo.com>
2020-10-21 23:07:48 -0700

    Improve explorer routing logic

--- src/routes/explorer/_layout.svelte
+++ src/routes/explorer/_layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { goto } from '@sapper/app'
 	
-	let blockchain = 'Ethereum'
-
 	export let segment: string
-	$: console.log('segment', segment)
-	$: if(segment === undefined && globalThis.document){
-		goto(`explorer/${blockchain.toLowerCase()}`)
+	let blockchain = segment || 'ethereum'
+
+	$: console.log('Explorer segment', segment)
+	if(segment === undefined && globalThis.document)
+		goto(`explorer/${blockchain}`)
+	
+	$: if(globalThis.document)
 		document.documentElement.style.setProperty('--primary-color', `var(--${tokenColors[blockchain]})`)
-	}
+
+	$: blockchainDisplayName = blockchain[0].toUpperCase() + blockchain.slice(1)
 
 	import Controls from '../../components/Controls.svelte'
 	import { fly } from 'svelte/transition'
@@ -22,18 +25,18 @@
 </style>
 
 <svelte:head>
-	<title>{blockchain ? `${blockchain} Explorer` : `Explorer`} | Blockhead</title>
+	<title>{blockchain ? `${blockchainDisplayName} Explorer` : `Explorer`} | Blockhead</title>
 </svelte:head>
 
 <main in:fly={{x: 300}} out:fly={{x: -300}}>
 	<Controls>
-		<h1>{blockchain ? `${blockchain} Explorer` : `Explorer`}</h1>
+		<h1>{blockchain ? `${blockchainDisplayName} Explorer` : `Explorer`}</h1>
 		<label>
 			<span>Blockchain: </span>
 			<!-- <select bind:value={blockchain}> -->
-			<select bind:value={blockchain}>
-				<option value="Bitcoin">Bitcoin</option>
-				<option value="Ethereum" selected>Ethereum</option>
+			<select bind:value={blockchain} on:input={() => globalThis.requestAnimationFrame(() => goto(`explorer/${blockchain}`))}>
+				<option value="bitcoin">Bitcoin</option>
+				<option value="ethereum" selected>Ethereum</option>
 			</select>
 			<!-- <a href="explorer/bitcoin"><button>Bitcoin</button></a>
 			<a href="explorer/ethereum"><button>Ethereum</button></a> -->
