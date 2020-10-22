e9ccd7dd2f318297c4cbcd28a3b5e2a43f87b2b7
Darryl Yeo <dev@darryl-yeo.com>
2020-10-22 09:47:39 -0700

    Add autofocus attribute

--- src/components/AddressField.svelte
+++ src/components/AddressField.svelte
@@ -2,10 +2,12 @@
 	import type { CryptoAddress } from '../data/CryptoAddress'
 	
 	export let address: CryptoAddress = ''
+
+	export let autofocus = false
 	export let required = false
 
 	const isValid = address =>
 		address !== undefined
 </script>
 
-<input type="text" class:is-valid={isValid(address)} placeholder="Address (0xabcd...6789) / ENS (example.eth)" bind:value={address} {required} />
+<input class="address-field" type="text" class:is-valid={isValid(address)} placeholder="Address (0xabcd...6789) / ENS (mywallet.eth)" bind:value={address} {autofocus} {required} />
