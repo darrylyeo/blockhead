d3d55b280c0f1e8d70de164edfcb46998c08e7d3
Darryl Yeo <dev@darryl-yeo.com>
2021-05-03 22:59:44 -0700

    Refactor layout; update copy

--- src/components/EnsQuery.svelte
+++ src/components/EnsQuery.svelte
@@ -1,35 +1,41 @@
 <script lang="ts">
-	import type { ENS } from '../data/ens'
 	import { queryENSDomain, queryENSDomainsContaining } from '../data/ens'
 	
 	export let query: string
 	
 	const sortByLength = (a, b) => a.name.length - b.name.length
 	
+	import Address from './Address.svelte'
 	import EnsDomain from './EnsDomain.svelte'
 	import Loader from './Loader.svelte'
 </script>
 
 <style>
-	.explorer {
+	.ens-query {
 		gap: 1.5em;
 	}
 </style>
 
-<section class="explorer">
-	<Loader
-		fromStore={() => queryENSDomain(query)}
-		loadingIcon="/logos/ens.svg"
-		loadingIconName="The Graph"
-		loadingMessage="Querying the Ethereum Name Service..."
-		let:then={{domains}}
-	>
+<Loader
+	fromStore={() => queryENSDomain(query)}
+	loadingIcon="/logos/ens.svg"
+	loadingIconName="The Graph"
+	loadingMessage="Querying the Ethereum Name Service..."
+	let:then={{domains}}
+>
+	<div class="ens-query column">
 		{#each domains.sort(sortByLength) as domain (domain.id)}
 			<EnsDomain {domain}/>
 		{:else}
 			<div class="card">
-				<p>The ENS name "{query}" doesn't have an owner.</p>
-				<a href="https://app.ens.domains/name/{domain}" target="_blank"><button>Register On ENS</button></a>
+				<div class="bar">
+					<h2><Address address={query} /></h2>
+					<span class="card-annotation">ENS Name</span>
+				</div>
+				<div class="bar">
+					<p>The ENS name "{query}" hasn't been registered by anyone. Perhaps you could claim it for yourself!</p>
+					<a href="https://app.ens.domains/name/{query}" target="_blank"><button class="medium">Register On ENS</button></a>
+				</div>
 			</div>
 		{/each}
 
@@ -47,33 +53,10 @@
 					<h2>Similar ENS Names</h2>
 				{/if}
 			</svelte:fragment>
-	
+
 			{#each domains.sort(sortByLength) as domain (domain.id)}
 				<EnsDomain {domain}/>
-			{:else}
-				<div class="card">
-					<p>The ENS name "{query}" doesn't have an owner.</p>
-					<a href="https://app.ens.domains/name/{domain}" target="_blank"><button>Register On ENS</button></a>
-				</div>
 			{/each}
 		</Loader>
-	</Loader>
-</section>
-
-
-<!--
-{#await $domainQuery}
-	<Loading>Querying the Ethereum Name Service...</Loading>
-{:then domainQuery}
-	<h2>{$domainQuery.name}</h2>
-	<p>{$domainQuery.labelName}</p>
-	<p>Hash: {$domainQuery.labelhash}</p>
-	<p>Owner: {$domainQuery.owner}</p>
-	<p>Resolver: {$domainQuery.resolver}</p>
-	{#each $domainQuery.events || [] as event}
-		{event}
-	{/each}
-{:catch error}
-
-{/await}
--->
+	</div>
+</Loader>
