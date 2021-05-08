09125b0e1bc034f4230aac9e0e0bfadcae130802
Darryl Yeo <dev@darryl-yeo.com>
2021-05-08 02:55:27 -0700

    Add formatting options, overridable slot

--- src/components/EthereumBlockNumber.svelte
+++ src/components/EthereumBlockNumber.svelte
@@ -1,15 +1,38 @@
 <script lang="ts">
 	import type { Ethereum } from '../data/ethereum/types'
-
+	
 	export let network: Ethereum.Network
 	export let blockNumber: Ethereum.BlockNumber
+
+	export let format: 'full' | 'number-only' = 'full'
 	export let linked = true
 
-	$: formattedBlockNumber = `block #${blockNumber}`
+	$: formattedBlockNumber =
+		format === 'full' ?
+			`block #${blockNumber}`
+		:
+			blockNumber
+
 </script>
 
+<style>
+	.format {
+		font-family: var(--monospace-fonts), var(--base-fonts);
+	}
+</style>
+
 {#if linked}
-	<a class="block-number" href="explorer/{network.slug}/{blockNumber}">{formattedBlockNumber}</a>
+	<a class="block-number" href="explorer/{network.slug}/{blockNumber}">
+		<slot {formattedBlockNumber}>
+			<span class="format">
+				{formattedBlockNumber}
+			</span>
+		</slot>
+	</a>
 {:else}
-	<span class="block-number">{formattedBlockNumber}</span>
+	<slot {formattedBlockNumber}>
+		<span class="block-number format">
+			{formattedBlockNumber}
+		</span>
+	</slot>
 {/if}
