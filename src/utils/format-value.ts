3ff5cbf777d76135f32cee083249d68c36c69c22
Darryl Yeo <dev@darryl-yeo.com>
2021-05-09 01:43:29 -0700

    Format based on fraction digits for now

--- src/utils/format-value.ts
+++ src/utils/format-value.ts
@@ -3,9 +3,11 @@ export function formatValue(value: number, currency?: string){
 		return globalThis.navigator
 			? new Intl.NumberFormat(globalThis.navigator.languages, {
 				... currency ? {style: 'currency', currency} : {},
-				minimumFractionDigits: 2,
-				minimumSignificantDigits: value < 1 ? 3 : undefined,
-				maximumSignificantDigits: value < 1 ? 3 : undefined,
+				// minimumFractionDigits: 2,
+				// minimumSignificantDigits: value < 1 ? 3 : undefined,
+				// maximumSignificantDigits: value < 1 ? 3 : undefined,
+				minimumFractionDigits: 3,
+				maximumFractionDigits: 3,
 			}).format(value)
 			: value
 	}catch(e){
