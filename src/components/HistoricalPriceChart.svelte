c283c6627188aa3d7a20fa4a4cf167c133d7bf83
Darryl Yeo <dev@darryl-yeo.com>
2021-07-05 02:27:47 -0700

    Use tabs

--- src/components/HistoricalPriceChart.svelte
+++ src/components/HistoricalPriceChart.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import type { AnalyticsProvider } from '../data/analytics/provider'
+	import type { AnalyticsProvider } from '../data/analytics/provider'
 	import type { Ethereum } from '../data/ethereum/types'
-    import type { QuoteCurrency, TickerSymbol } from '../data/currency/currency'
+	import type { QuoteCurrency, TickerSymbol } from '../data/currency/currency'
 
-	export let analyticsProvider: AnalyticsProvider
+	export let analyticsProvider: AnalyticsProvider = 'Covalent'
 	export let currencies: (TickerSymbol | Ethereum.ContractAddress)[]
 	export let quoteCurrency: QuoteCurrency
 
@@ -23,21 +23,20 @@
 </div>
 
 <CovalentPriceChart
-    {analyticsProvider}
-    {currencies}
-    {quoteCurrency}
-    {priceScale}
+	{analyticsProvider}
+	{currencies}
+	{quoteCurrency}
+	{priceScale}
 />
 
 <div class="bar">
-    <h4>Show</h4>
-    <label>
-        <span>Price Scale</span>
-        <select bind:value={priceScale}>
-            <option value="logarithmic">Logarithmic</option>
-            <option value="linear">Linear</option>
-            <option value="linearFromZero">Linear From Zero</option>
-        </select>
-    </label>
+	<h4>Show</h4>
+	<label>
+		<span>Price Scale</span>
+		<select bind:value={priceScale}>
+			<option value="logarithmic">Logarithmic</option>
+			<option value="linear">Linear</option>
+			<option value="linearFromZero">Linear From Zero</option>
+		</select>
+	</label>
 </div>
-
