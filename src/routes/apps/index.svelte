477e564d8a8e8e4fd9aee709306b2b0c5ccd2cf3
Darryl Yeo <dev@darryl-yeo.com>
2021-07-07 10:31:44 +0000

    Styling/layout tweaks

--- src/routes/apps/index.svelte
+++ src/routes/apps/index.svelte
@@ -12,8 +12,17 @@
 
 
 <style>
+	.column {
+		--padding-inner: 1.5rem;
+	}
+
+	.featured {
+		font-size: 1.2em;
+	}
+
 	.row {
 		align-content: start;
+		--padding-inner: 1rem;
 
 		/* display: grid;
 		grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
@@ -27,9 +36,11 @@
 
 
 <div class="column" in:fly={{x: 300}} out:fly={{x: -300}}>
+	<hr>
+
 	<h2>Featured Apps</h2>
 
-	<section class="row">
+	<section class="row featured">
 		{#each featuredDefiApps as {name, slug, colors}, i}
 			<div class="card" transition:scale={{delay: i * 10}} style={cardStyle(colors)}>
 				<h3><a href="apps/{slug}" on:click={() => globalThis.requestAnimationFrame(() => goto(`apps/${slug}`))}>{name}</a></h3>
