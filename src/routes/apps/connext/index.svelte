c365cba351d0847cb0819877282a24f525e65018
Darryl Yeo <dev@darryl-yeo.com>
2021-07-07 08:17:05 +0000

    Add back TransferAssets interface for Connext dashboard

---
+++ src/routes/apps/connext/index.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import TransferAssets from '../../../components/TransferAssets.svelte'
+
+
+	import { fly } from 'svelte/transition'
+</script>
+
+
+<section class="column" in:fly={{x: 100}} out:fly={{x: -100}}>
+	<div class="card">
+		<div class="bar">
+			<h3>Cross-Chain Transfer</h3>
+			<div class="card-annotation">Connext</div>
+		</div>
+
+		<hr>
+
+		<TransferAssets
+			transferSolution="Connext"
+		/>
+	</div>
+</section>
