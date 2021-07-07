485aae0868be4631e474349cae1b4da5b5a73816
Darryl Yeo <dev@darryl-yeo.com>
2021-07-07 09:11:48 +0000

    Embed Aavote, Aave Governance v2 Interface

---
+++ src/routes/apps/aave/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition'
+</script>
+
+
+<style>
+	iframe {
+		width: 100%;
+		height: 100%;
+		max-height: 80vh;
+		justify-self: center;
+		border-radius: 0.5em;
+	}
+</style>
+
+
+<section class="column" in:fly={{x: 100}} out:fly={{x: -100}}>
+	<div class="bar">
+		<h3>Aave Governance v2</h3>
+	</div>
+
+	<iframe
+		title="Aavote - Aave Governance v2 Interface"
+		src={'https://aavote.netlify.app'}
+	/>
+</section>
