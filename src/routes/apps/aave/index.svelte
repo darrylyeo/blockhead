0495d285a3f798c0cb2df88ba14757bd99fa0955
Darryl Yeo <dev@darryl-yeo.com>
2021-07-07 09:17:26 +0000

    Layout improvements

--- src/routes/apps/aave/index.svelte
+++ src/routes/apps/aave/index.svelte
@@ -6,8 +6,7 @@
 <style>
 	iframe {
 		width: 100%;
-		height: 100%;
-		max-height: 80vh;
+		height: 80vh;
 		justify-self: center;
 		border-radius: 0.5em;
 	}
@@ -15,12 +14,17 @@
 
 
 <section class="column" in:fly={{x: 100}} out:fly={{x: -100}}>
-	<div class="bar">
-		<h3>Aave Governance v2</h3>
-	</div>
+	<div class="card">
+		<div class="bar">
+			<h3>Aave Governance v2</h3>
+			<div class="card-annotation">The Graph, IPFS</div>
+		</div>
 
-	<iframe
-		title="Aavote - Aave Governance v2 Interface"
-		src={'https://aavote.netlify.app'}
-	/>
+		<hr>
+
+		<iframe
+			title="Aavote - Aave Governance v2 Interface"
+			src={'https://aavote.netlify.app'}
+		/>
+	</div>
 </section>
