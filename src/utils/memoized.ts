ef724017eaff96069038868b46dd4e0f6ad32a56
Darryl Yeo <dev@darryl-yeo.com>
2021-07-09 08:12:01 +0000

    Use JSON.stringify()

--- src/utils/memoized.ts
+++ src/utils/memoized.ts
@@ -1,7 +1,7 @@
 export function memoized<A, T>(func: (...args: A[]) => T) {
 	const cache: Record<string, T> = {}
 	return function(...args: A[]): T {
-		const key = args.toString()
+		const key = JSON.stringify(args)
 		return cache[key] ??= func(...args)
 	}
 }
@@ -9,7 +9,7 @@ export function memoized<A, T>(func: (...args: A[]) => T) {
 export function memoizedAsync<A, T>(func: (...args: A[]) => Promise<T>) {
 	const cache: Record<string, T> = {}
 	return async function(...args: A[]): Promise<T> {
-		const key = args.toString()
+		const key = JSON.stringify(args)
 		return cache[key] ??= await func(...args)
 	}
 }
