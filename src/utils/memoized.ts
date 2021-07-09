66b3bdc36977caca98a2a632871bb6952a8d8e1e
Darryl Yeo <dev@darryl-yeo.com>
2021-07-09 01:54:02 +0000

    Improve types

--- src/utils/memoized.ts
+++ src/utils/memoized.ts
@@ -1,15 +1,15 @@
-export function memoized<T>(func: () => T): () => T{
-	const cache = {}
-	return function(...args){
+export function memoized<A, T>(func: (...args: A[]) => T) {
+	const cache: Record<string, T> = {}
+	return function(...args: A[]): T {
 		const key = args.toString()
-		return cache[key] ||= func(...args)
+		return cache[key] ??= func(...args)
 	}
 }
 
-export function memoizedAsync<T>(func: () => Promise<T>): () => Promise<T> {
-	const cache = {}
-	return async function(...args){
+export function memoizedAsync<A, T>(func: (...args: A[]) => Promise<T>) {
+	const cache: Record<string, T> = {}
+	return async function(...args: A[]): Promise<T> {
 		const key = args.toString()
-		return cache[key] ||= await func(...args)
+		return cache[key] ??= await func(...args)
 	}
 }
