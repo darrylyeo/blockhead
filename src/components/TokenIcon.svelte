113e09ea35b1ed9b1e0cc931aa113c85456de3da
Darryl Yeo <dev@darryl-yeo.com>
2021-07-09 10:38:35 +0000

    Display liquidity pool tokens as their underlying tokens

--- src/components/TokenIcon.svelte
+++ src/components/TokenIcon.svelte
@@ -34,36 +34,46 @@
 	$: Icon = token && !['DAI', 'LINK', 'LRC'].includes(token) && CryptoIcons[token[0].toUpperCase() + token.slice(1).toLowerCase()]
 </script>
 
-<picture title={token + (tokenAddress ? ` (${tokenAddress})` : '')}>
-	{#if Icon}
-		<svelte:component this={Icon} size="1.25em" />
-	<!-- {:else if !loadingError && (tokenAddress || tokenIcon || token)}
-		{#if tokenAddress}
-			<source src="https://token-icons.s3.amazonaws.com/{tokenAddress.toLowerCase()}.png">
-			<source src="https://tokens.1inch.exchange/{tokenAddress.toLowerCase()}.png">
-			<source src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/{tokenAddress}/logo.png">
+
+{#if token?.includes(' / ')}
+	{#each token.split(' / ') as token}
+		<svelte:self {token} />
+	{/each}
+{:else}
+	<picture class="token-icon" title={token + (tokenAddress ? ` (${tokenAddress})` : '')}>
+		{#if Icon}
+			<svelte:component this={Icon} size="1.25em" />
+		<!-- {:else if !loadingError && (tokenAddress || tokenIcon || token)}
+			{#if tokenAddress}
+				<source src="https://token-icons.s3.amazonaws.com/{tokenAddress.toLowerCase()}.png">
+				<source src="https://tokens.1inch.exchange/{tokenAddress.toLowerCase()}.png">
+				<source src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/{tokenAddress}/logo.png">
+			{/if}
+			{#if tokenIcon}
+				<source src={tokenIcon}>
+			{/if}
+			{#if token}
+				<source src="https://zapper.fi/images/{token}-icon.png">
+			{/if}
+			<img on:error={e => loadingError = e}>
+			<img src={tokenIcon || `https://zapper.fi/images/${token}-icon.png`} on:error={e => loadingError = e}> -->
+		{:else if imageSources[i]}
+			<img src={imageSources[i]} on:error={e => i++} alt={token} />
+		{:else}
+			<i class="placeholder-icon" data-icon={token?.slice(0, 4) ?? '?'}></i>
 		{/if}
-		{#if tokenIcon}
-			<source src={tokenIcon}>
-		{/if}
-		{#if token}
-			<source src="https://zapper.fi/images/{token}-icon.png">
-		{/if}
-		<img on:error={e => loadingError = e}>
-		<img src={tokenIcon || `https://zapper.fi/images/${token}-icon.png`} on:error={e => loadingError = e}> -->
-	{:else if imageSources[i]}
-		<img src={imageSources[i]} on:error={e => i++} alt={token} />
-	{:else}
-		<i class="placeholder-icon" data-icon={token?.slice(0, 4) ?? '?'}></i>
-	{/if}
-</picture>
+	</picture>
+{/if}
 
 <style>
-	picture {
+	.token-icon {
 		display: inline-flex;
 		height: 1em;
 		align-self: center;
 		align-items: center;
+	}
+	.token-icon + :global(.token-icon) {
+		margin-left: calc(-25% - var(--padding-inner));
 	}
 
 	source {
@@ -73,6 +83,7 @@
 	img, .placeholder-icon {
 		width: 1.25em;
 		height: 1.25em;
+		border-radius: 0.3em;
 	}
 
 	.placeholder-icon {
