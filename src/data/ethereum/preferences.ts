9d2340281968fda465d81898cfa7241149cd7c05
Darryl Yeo <dev@darryl-yeo.com>
2021-08-05 01:22:22 -0700

    Default to Ethers Quorum provider option

--- src/data/ethereum/preferences.ts
+++ src/data/ethereum/preferences.ts
@@ -6,7 +6,7 @@ import type { AnalyticsProvider } from '../analytics/provider'
 import type { QuoteCurrency } from '../currency/currency'
 
 export const ethereumChainID = localStorageWritable<Ethereum.ChainID>('ethereum-network', 1)
-export const preferredEthereumProvider = localStorageWritable<Ethereum.ProviderName>('preferred-ethereum-provider', 'Portis')
+export const preferredEthereumProvider = localStorageWritable<Ethereum.ProviderName>('preferred-ethereum-provider', 'Ethers')
 export const preferredDeFiProvider = localStorageWritable<DeFiProvider>('preferred-defi-provider', 'Zapper')
 export const preferredOracleProvider = localStorageWritable<PriceFeedProvider>('preferred-price-feed-provider', 'Chainlink')
 export const preferredAnalyticsProvider = localStorageWritable<AnalyticsProvider>('preferred-analytics-provider', 'Covalent')
