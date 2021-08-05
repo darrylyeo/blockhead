40c191ab4824646c83486c68cb17d1d277fd3912
Darryl Yeo <dev@darryl-yeo.com>
2021-08-05 01:25:29 -0700

    Correct type to PriceFeedProvider

--- src/data/ethereum/price/price-feed-provider.ts
+++ src/data/ethereum/price/price-feed-provider.ts
@@ -1,5 +1,5 @@
-export type CurrentPriceProvider = APIPriceProvider | OracleProvider
+export type CurrentPriceProvider = APIPriceProvider | PriceFeedProvider
 
 export type APIPriceProvider = 'Covalent'
 
-export type OracleProvider = 'Chainlink' | 'Tellor' | 'Umbrella' | 'Compound Open Price Feed'
+export type PriceFeedProvider = 'Chainlink' | 'Tellor' | 'Umbrella' | 'Compound Open Price Feed'
