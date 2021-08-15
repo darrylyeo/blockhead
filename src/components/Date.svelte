a4de8add8f95e549d78f7a4c3f2c7853ff978ef6
Darryl Yeo <dev@darryl-yeo.com>
2021-08-15 00:41:31 -0700

    Allow hiding time in <Date>

--- src/components/Date.svelte
+++ src/components/Date.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	export let date: string | Date
+
+
 	export let layout: 'horizontal' | 'vertical' = 'vertical'
+	export let showTime = true
+
 
 	$: dateObject = new Date(date)
 </script>
@@ -18,5 +22,5 @@
 
 <time class="date layout-{layout}" datetime={dateObject.toISOString()}>
 	<span class="day">{dateObject.toLocaleDateString()}</span>
-	<span class="time">{dateObject.toLocaleTimeString()}</span>
+	{#if showTime}<span class="time">{dateObject.toLocaleTimeString()}</span>{/if}
 </time>
