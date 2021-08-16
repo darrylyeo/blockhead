1ba1f42d4b48d3f1c402b267fc9e1c483557018f
Darryl Yeo <dev@darryl-yeo.com>
2021-08-15 19:37:01 -0700

    Configure Houdini with ENS subgraph

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import preprocess from 'svelte-preprocess'
 import adapterStatic from '@sveltejs/adapter-static'
 
+
+import houdini from 'houdini-preprocess'
+import path from 'path'
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess(),
+		houdini()
+	],
 
 	kit: {
 		// hydrate the <div id='svelte'> element in src/app.html
@@ -48,7 +56,8 @@ const config = {
 
 			resolve: {
 				alias: {
-					process: 'process/browser',
+					'process': 'process/browser',
+					'$houdini': path.resolve('.', '$houdini')
 				}
 			}
 		},
