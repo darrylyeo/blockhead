479af57274d3b1cad4bfe6361edb9ebf6d6e53b5
Darryl Yeo <dev@darryl-yeo.com>
2021-09-27 22:27:44 -0700

    Refactor Apps screen layout; show icon of the first token for each app

--- src/routes/apps/index.svelte
+++ src/routes/apps/index.svelte
@@ -2,6 +2,7 @@
 	import { featuredBlockchainApps, notFeaturedBlockchainApps } from '../../data/blockchain-apps'
 
 
+	import TokenIcon from '../../components/TokenIcon.svelte'
 	import { cardStyle } from '../../utils/card-background'
 	import { fly, scale } from 'svelte/transition'
 </script>
@@ -20,14 +21,28 @@
 	.row {
 		align-content: start;
 		--padding-inner: 1rem;
-
-		/* display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-		align-items: stretch; */
 	}
 	.row > * {
 		flex: 1 auto;
 		text-align: center;
+		justify-content: center;
+	}
+
+	.app {
+		text-decoration: none;
+	}
+	.app:hover {
+		transform: scale(1.04);
+	}
+
+	.app h3 {
+		gap: 0.66em;
+	}
+
+	.app span {
+		color: rgba(var(--rgb-light-dark-inverse), calc(0.6 + 0.3 * var(--is-dark)));
+		text-shadow: 0 0 0 var(--primary-color);
+		opacity: calc(0.75 + 0.25 * var(--is-dark));
 	}
 </style>
 
@@ -38,10 +53,19 @@
 	<h2>Featured Apps</h2>
 
 	<section class="row featured">
-		{#each featuredBlockchainApps as {name, slug, colors}, i}
-			<div class="card" transition:scale={{delay: i * 10}} style={cardStyle(colors)}>
-				<h3><a href="/apps/{slug}">{name}</a></h3>
-			</div>
+		{#each featuredBlockchainApps as {name, slug, colors, views}, i}
+			<a href="/apps/{slug}" class="app card" transition:scale={{delay: i * 10}} style={cardStyle(colors)}>
+				<h3 class="row">
+					{#each views?.flatMap(view => view.erc20Tokens ?? []).slice(0, 1) as {logoURI, address, name, symbol}}
+						<TokenIcon
+							token={symbol}
+							tokenIcon={logoURI}
+							tokenAddress={address}
+						/>
+					{/each}
+					<span>{name}</span>
+				</h3>
+			</a>
 		{/each}
 	</section>
 
@@ -50,10 +74,19 @@
 	<h2>Other Apps</h2>
 
 	<section class="row">
-		{#each notFeaturedBlockchainApps as {name, slug, colors}, i}
-			<div class="card" transition:scale={{delay: i * 10}}><!-- style={cardStyle(colors)} -->
-				<h4><a href="/apps/{slug}">{name}</a></h4>
-			</div>
+		{#each notFeaturedBlockchainApps as {name, slug, colors, views}, i}
+			<a href="/apps/{slug}" class="app card" transition:scale={{delay: i * 10}} style={cardStyle(colors)}>
+				<h3 class="row">
+					{#each views?.flatMap(view => view.erc20Tokens ?? []).slice(0, 1) as {logoURI, address, name, symbol}}
+						<TokenIcon
+							token={symbol}
+							tokenIcon={logoURI}
+							tokenAddress={address}
+						/>
+					{/each}
+					<span>{name}</span>
+				</h3>
+			</a>
 		{/each}
 	</section>
 </div>
