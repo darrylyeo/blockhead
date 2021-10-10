9b534cd919025385722a3a6e3bf997013a2af656
Darryl Yeo <dev@darryl-yeo.com>
2021-10-10 01:42:48 -0700

    Add Moralis to <EthereumTransactionsERC20Loader>

--- src/components/EthereumTransactionsERC20Loader.svelte
+++ src/components/EthereumTransactionsERC20Loader.svelte
@@ -11,12 +11,14 @@
 
 	export let quoteCurrency: TickerSymbol
 	export let transactionProvider
+	export let includeLogs = true
 
 	$: quoteCurrency = $$props.quoteCurrency || $preferences.quoteCurrency
 	$: transactionProvider = $$props.transactionProvider || $preferences.transactionProvider
 
 
 	import { getERC20TokenTransfers } from '../data/analytics/covalent'
+	import { chainCodeFromNetwork, MoralisWeb3Api } from '../data/moralis/moralis-web3-api'
 
 
 	import Loader from './Loader.svelte'
@@ -38,6 +40,38 @@
 			}))
 				.items
 		}
+		let:then={transactions}
+		let:status
+	>
+		<slot name="header" slot="header" {status} {transactions} />
+		<slot {transactions} />
+	</Loader>
+{:else if transactionProvider === 'Moralis'}
+	<Loader
+		loadingIcon={'/logos/moralis-logo.svg'}
+		loadingIconName={transactionProvider}
+		loadingMessage="Retrieving ERC-20 transactions from {transactionProvider}..."
+		errorMessage="Error retrieving ERC-20 transactions from {transactionProvider}"
+		fromPromise={async () => {
+			const { result: transactions, page, page_size, total } = (await MoralisWeb3Api.address.getTokenTransfers({
+				chain: chainCodeFromNetwork(network),
+				from_block: 0,
+				// to_block: ,
+				// offset,
+				// limit,
+				address
+			}))
+
+			return includeLogs
+				? await Promise.all(transactions.map(async transaction => ({
+					value_: transaction.value,
+					...(await MoralisWeb3Api.transaction.getTransaction({
+						chain: chainCodeFromNetwork(network),
+						transactionHash: transaction.hash
+					}))
+				})))
+				: transactions
+		}}
 		let:then={transactions}
 		let:status
 	>
