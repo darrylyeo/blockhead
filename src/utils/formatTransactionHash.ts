b8bc00671f26e9f91874f72e67a7e7721f05219f
Darryl Yeo <dev@darryl-yeo.com>
2021-10-12 00:23:53 -0700

    Show less digits for formatTransactionHash

--- src/utils/formatTransactionHash.ts
+++ src/utils/formatTransactionHash.ts
@@ -1,7 +1,7 @@
 export const formatTransactionHash = (transactionHash = '', format: 'full' | 'middle-truncated' = 'full') =>
 	format === 'middle-truncated' ?
-		transactionHash?.slice(0, 2 + 8) + '⸱⸱⸱' + transactionHash?.slice(-8)
-		// transactionHash?.slice(0, 2 + 8) + '⋯' + transactionHash?.slice(-8)
-		// transactionHash?.slice(0, 2 + 8) + '…' + transactionHash?.slice(-8)
+		transactionHash?.slice(0, 2 + 6) + '⸱⸱⸱' + transactionHash?.slice(-6)
+		// transactionHash?.slice(0, 2 + 6) + '⋯' + transactionHash?.slice(-6)
+		// transactionHash?.slice(0, 2 + 6) + '…' + transactionHash?.slice(-6)
 	:
 		transactionHash
