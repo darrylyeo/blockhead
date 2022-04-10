1852666727ec828e94bfea98c6e2fc20895d1345
Darryl Yeo <dev@darryl-yeo.com>
2022-04-09 17:26:34 -0700

    Create <WidthContainer> as a shortcut to <SizeContainer transitionWidth>

---
+++ src/components/WidthContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import SizeContainer from './SizeContainer.svelte'
+</script>
+
+
+<SizeContainer
+	transitionWidth
+	transitionHeight={false}
+    {...$$props}
+>
+    <slot />
+</SizeContainer>
