6d02186e56ec06e7956754896c0954293f33578b
Darryl Yeo <dev@darryl-yeo.com>
2022-04-18 23:09:55 -0700

    Simplify global layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,9 +17,10 @@
 	globalThis.process = process
 </script>
 
+
 <script lang="ts">
 	import Nav from '../components/Nav.svelte'
-	
+
 	export let segment: string
 	
 	
@@ -60,21 +61,10 @@
 		globalThis.document.documentElement.className = `color-scheme-${$preferences.theme}`
 </script>
 
-<style>
-	/* Grid-based */
-	/* :global(#sapper) {
-		display: grid;
-		grid:
-			"nav" auto
-			"main" 1fr
-			"preferences" auto;
-		place-items: start;
-		min-height: 100vh;
-	}
 
-	:global(nav) {
-		grid-area: nav;
-		position: sticky;
+<style>
+	:global(body > nav) {
+		position: fixed;
 		width: 100%;
 		top: 0;
 		z-index: 1;
@@ -85,55 +75,18 @@
 		padding: 0 var(--padding-outer);
 	}
 
-	:global(main) {
-		grid-area: main;
-	}
-
-	:global(.preferences) {
-		grid-area: preferences;
-		position: sticky;
-		width: 100%;
-		bottom: 0;
-		z-index: 1;
-
-		-webkit-backdrop-filter: var(--overlay-backdrop-filter);
-		backdrop-filter: var(--overlay-backdrop-filter);
-		border-top: 1px solid rgba(0, 0, 0, 0.2);
-		--padding-outer: 1em;
-		padding: var(--padding-outer);
-	} */
-
-
-	/* Experimental: make <main> the scrolling element to enable inner position: sticky elements */
-	/* :global(#sapper) {
-		height: 100vh;
-	} */
-	/* :global(#sapper main) {
-		overflow-y: auto;
-		height: 100vh;
-		grid-area: 1 / 1 / 3 / 1;
-	} */
-	/* :global(#sapper main) {
-		overflow-y: auto;
-		--bleed: 3rem;
-		margin: calc(-1 * var(--bleed)) 0;
-		padding: calc(var(--bleed) + var(--padding-outer)) var(--padding-outer);
-	} */
-
-
-	/* Fixed layout (supports inner position: sticky elements) */
-	:global(#svelte > nav) {
-		position: fixed;
-		width: 100%;
-		top: 0;
-		z-index: 1;
-	}
-
-	:global(main) {
+	.stack > :global(main) {
 		--bleed-top: 3.5rem;
 		--bleed-bottom: 3.25rem;
-		align-content: start;
+
+		margin: 0 auto;
+		width: 100%;
+		min-height: 100vh;
+
+		display: grid;
+		gap: var(--padding-inner);
 		padding: calc(var(--bleed-top) + var(--padding-outer)) var(--padding-outer) calc(var(--bleed-bottom) + var(--padding-outer));
+		align-content: start;
 	}
 
 	:global(.preferences) {
@@ -141,22 +94,13 @@
 		width: 100%;
 		bottom: 0;
 		z-index: 1;
-	}
 
-
-
-	:global(#svelte > nav) {
-		-webkit-backdrop-filter: var(--overlay-backdrop-filter);
-		backdrop-filter: var(--overlay-backdrop-filter);
-		border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-		padding: 0 var(--padding-outer);
-	}
-	:global(.preferences) {
 		-webkit-backdrop-filter: var(--overlay-backdrop-filter);
 		backdrop-filter: var(--overlay-backdrop-filter);
 		border-top: 1px solid rgba(0, 0, 0, 0.2);
 	}
 </style>
+
 
 <Nav {segment}/>
 
