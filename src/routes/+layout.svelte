5e6cee5ee1981cf63956515778a37b4d6eeef825
Darryl Yeo <dev@darryl-yeo.com>
2022-09-12 10:27:26 -0700

    @migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,52 +1,6 @@
-<script context="module">
-	import '../fonts.css'
-	import '../app.css'
-
-
-	import houdiniEnvironment from '../data/ens/houdini-environment'
-	import { setEnvironment } from '$houdini'
-
-	setEnvironment(houdiniEnvironment)
-
-
-	// Svelte Query
-	import { QueryClient, QueryClientProvider, persistQueryClient, broadcastQueryClient, createWebStoragePersistor } from '@sveltestack/svelte-query'
-
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				keepPreviousData: true,
-				// staleTime: Infinity,
-				cacheTime: Infinity,
-			},
-		}
-	})
-
-	const localStoragePersistor = createWebStoragePersistor({
-		storage: globalThis.localStorage
-	})
-	persistQueryClient({
-		queryClient,
-		persistor: localStoragePersistor,
-	})
-
-	// broadcastQueryClient({
-	// 	queryClient,
-	// 	broadcastChannel: globalThis.location?.origin,
-	// })
-
-
-
-	import process from 'process'
-	import { Buffer } from 'buffer'
-
-	globalThis.global = globalThis
-	globalThis.Buffer = Buffer
-	globalThis.process = process
-</script>
-
-
 <script lang="ts">
+	throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
+
 	import Nav from '../components/Nav.svelte'
 
 	export let segment: string
