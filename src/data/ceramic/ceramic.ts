dfd3902cc03143aebe5de28aca26a230c69fe574
Darryl Yeo <dev@darryl-yeo.com>
2022-09-24 22:37:05 -0700

    Model available Ceramic nodes

--- src/data/ceramic/ceramic.ts
+++ src/data/ceramic/ceramic.ts
@@ -1,9 +1,44 @@
 import Ceramic from '@ceramicnetwork/http-client'
 
-const CERAMIC_TESTNET_NODES = ['https://gateway-clay.ceramic.network', 'https://ceramic-clay.3boxlabs.com']
-
-
-export function getCeramic(apiHost = CERAMIC_TESTNET_NODES[0]) {
-	const ceramic = new Ceramic(apiHost)
-	return ceramic
+type CeramicNode = {
+	id: string,
+	url: string,
+	network: CeramicNetwork,
+	hasReadAccess: boolean,
+	hasWriteAccess: boolean,
 }
+
+enum CeramicNetwork {
+	Mainnet = 'mainnet',
+	ClayTestnet = 'clay-testnet',
+}
+
+// https://developers.ceramic.network/run/nodes/community-nodes/
+// https://developers.ceramic.network/tools/self-id/configuration/
+const ceramicNodes: CeramicNode[] = [
+	{
+		id: 'mainnet-gateway',
+		url: 'https://gateway.ceramic.network',
+		network: CeramicNetwork.Mainnet,
+		hasReadAccess: true,
+		hasWriteAccess: false,
+	},
+	{
+		id: 'testnet-clay',
+		url: 'https://gateway-clay.ceramic.network',
+		network: CeramicNetwork.ClayTestnet,
+		hasReadAccess: true,
+		hasWriteAccess: false,
+	},
+	{
+		id: 'testnet-clay-gateway',
+		url: 'https://ceramic-clay.3boxlabs.com',
+		network: CeramicNetwork.ClayTestnet,
+		hasReadAccess: true,
+		hasWriteAccess: true,
+	},
+]
+
+export const getCeramic = (ceramicNode = ceramicNodes.find(node => node.hasWriteAccess)) => (
+	new Ceramic(ceramicNode.url)
+)
