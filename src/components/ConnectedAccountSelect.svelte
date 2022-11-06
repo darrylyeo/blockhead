89375412a4443e63290bee5e09d7bf2c65714dd5
Darryl Yeo <dev@darryl-yeo.com>
2022-11-05 23:39:09 -0700

    Add <ConnectedAccountSelect> component

---
+++ src/components/ConnectedAccountSelect.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { formatAddress } from '../utils/formatAddress'
+	import { connectedAccounts, type ConnectedAccount } from '../state/account'
+
+
+	export let required: boolean
+
+
+	export let selectedAccount: ConnectedAccount
+</script>
+
+
+<select class="connected-account-select" bind:value={selectedAccount} {required}>
+	<option value={undefined} disabled>Choose account...</option>
+
+	<!-- <optgroup label="Connected"> -->
+		{#each $connectedAccounts as connectedAccount}
+			<option value={connectedAccount}>{connectedAccount.walletConnection.walletType}: {formatAddress(connectedAccount.address)}</option>
+		{/each}
+	<!-- </optgroup> -->
+</select>
