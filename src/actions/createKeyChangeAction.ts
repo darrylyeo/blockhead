e2ebf401622fe37dff4e89955ecddb7475ae0ab5
Darryl Yeo <dev@darryl-yeo.com>
2023-01-27 00:26:49 -0800

    createKeyChangeAction(): use Action type

--- src/actions/createKeyChangeAction.ts
+++ src/actions/createKeyChangeAction.ts
@@ -1,9 +1,13 @@
-export const createKeyChangeAction = <T>(onKeyChange: (element: HTMLElement, key: T) => void) =>
-	(element: HTMLElement, key: T) => {
+import type { Action } from 'svelte/action'
+
+export const createKeyChangeAction = <Element = HTMLElement, Parameter = any>(
+	onKeyChange: (element: Element, key: Parameter) => void
+): Action<Element, Parameter> =>
+	(element, key) => {
 		let previousKey = key
 
 		return {
-			update(key: T) {
+			update(key) {
 				if(previousKey !== key){
 					onKeyChange(element, key)
 					previousKey = key
