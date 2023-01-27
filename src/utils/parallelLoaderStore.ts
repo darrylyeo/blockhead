75dd5322e6ca5545e503da7a14926a07674fdf97
Darryl Yeo <dev@darryl-yeo.com>
2023-01-27 00:02:27 -0800

    Add parallelLoaderStore() function for mapping and updating async request results in real time

---
+++ src/utils/parallelLoaderStore.ts
@@ -0,0 +1,30 @@
+import { readable } from 'svelte/store'
+
+import { promiseAllFulfilled } from './promiseAllFulfilled'
+
+export const parallelLoaderStore = <T, Result>(items: T[], asyncMap: (item: T) => Promise<Result>) =>
+	readable({ loading: true }, set => {
+		const promiseMap = new Map(
+			items.map(item => [
+				item,
+				asyncMap(item)
+			])
+		)
+
+		let results = new Map
+
+		for (const [item, promise] of promiseMap.entries())
+			promise.then(result => {
+				results.set(item, result)
+				set({
+					loading: true,
+					data: results
+				})
+			})
+			.catch(console.error)
+
+		promiseAllFulfilled([...promiseMap.values()]).then(() => set({
+			loading: false,
+			data: results
+		}))
+	})
