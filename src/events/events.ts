714563e735bfc1690624b0e3837bfdad4cf4bae4
Darryl Yeo <dev@darryl-yeo.com>
2023-02-01 19:02:05 -0800

    Add triggerEvent function

---
+++ src/events/events.ts
@@ -0,0 +1,5 @@
+export type Events = {
+	
+}
+
+export type EventType = keyof Events
---
+++ src/events/triggerEvent.ts
@@ -0,0 +1,11 @@
+import type { EventType, Events } from './events'
+
+export const triggerEvent = <T extends EventType>(type: EventType, data: Events[T]) =>
+	globalThis.dispatchEvent(
+		new CustomEvent('blockhead/event', {
+			detail: {
+				type,
+				data,
+			}
+		})
+	)
