a9367c111d2e26582bb7451fb74882400d732f95
Darryl Yeo <dev@darryl-yeo.com>
2023-02-21 10:02:27 -0800

    <Collapsible>: support <summary> / <details> layout

--- src/components/Collapsible.svelte
+++ src/components/Collapsible.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	export let type: 'label' | 'details' = 'label'
+
 	export let title: string
 
 	export let isOpen = false
+
 
 	const ariaId = crypto.randomUUID()
 
@@ -11,7 +14,7 @@
 
 
 <style>
-	.collapsible {
+	.container {
 		display: grid;
 
 		perspective: 1000px;
@@ -19,21 +22,24 @@
 		transition: 0.5s var(--ease-out-expo);
 		/* transition-property: grid-template-rows, gap; */
 	}
-	.collapsible[data-state="open"] {
+	.container:is(details) {
+		display: contents;
+	}
+	.container[data-state="open"] {
 		grid-template-rows: max-content 1fr;
 	}
-	.collapsible[data-state="closed"] {
+	.container[data-state="closed"] {
 		grid-template-rows: max-content 0fr;
 		gap: 0;
 	}
 
-	.collapsible > div {
+	.collapsible {
 		min-height: 0;
 		overflow: clip;
 		overflow-clip-margin: var(--padding-outer);
 		transform-origin: top;
 	}
-	.collapsible[data-state="closed"] > div {
+	.container[data-state="closed"] > .collapsible {
 		/* transform: rotateX(-20deg); */
 		translate: 0 var(--padding-inner) -100px;
 
@@ -44,20 +50,22 @@
 		pointer-events: none;
 	}
 
-	label {
+	label, summary {
 		margin: calc(-1 * var(--padding-inner));
 		padding: var(--padding-inner);
 	}
-	label[data-state="open"] {
+	label[data-state="open"],
+	details[open] summary {
 		cursor: zoom-out;
 	}
-	label[data-state="closed"] {
+	label[data-state="closed"],
+	details:not([open]) summary {
 		cursor: zoom-in;
 	}
-	label:hover {
+	:is(label, summary):hover {
 		filter: brightness(1.1);
 	}
-	label:active {
+	:is(label, summary):active {
 		scale: 0.992;
 		opacity: 0.75;
 		transition-duration: 0.15s;
@@ -65,39 +73,96 @@
 </style>
 
 
-<div
-	class="collapsible {$$props.class}"
-	id={ariaId}
-	data-state={isOpen ? 'open' : 'closed'}
-	aria-labelledby="collapsible-header"
-	role="region"
->
-	<slot name="header" {isOpen} {toggle}>
-		<label class="bar" data-state={isOpen ? 'open' : 'closed'}>
-			<slot name="title">
-				<h4>{title}</h4>
-			</slot>
+{#if type === 'label'}
+	<div
+		class="container {$$props.class}"
+		id={ariaId}
+		data-state={isOpen ? 'open' : 'closed'}
+	>
+		<slot name="header" {isOpen} {toggle}>
+			<label class="bar" data-state={isOpen ? 'open' : 'closed'}>
+				<slot name="title">
+					<h4>{title}</h4>
+				</slot>
 
-			<button
-				class="small"
-				data-after={isOpen ? '⏶' : '⏷'}
-				aria-controls={ariaId}
-				aria-expanded={isOpen ? 'true' : 'false'}
-				on:click={toggle}
-			>{isOpen ? 'Hide' : 'Show'}</button>
-			<!-- <button
-				class="small"
-				data-after={isOpen ? '▲' : '▼'}
-				aria-controls={ariaId}
-				aria-expanded={isOpen ? 'true' : 'false'}
-				on:click={toggle}
-			/> -->
-		</label>
-	</slot>
+				<div class="row">
+					<slot name="toolbar" {isOpen} {toggle}>
+						{#if $$slots['toolbar-items']}
+							<div role="toolbar">
+								<slot name="toolbar-items" {isOpen} {toggle} />
+							</div>
+						{/if}
+					</slot>
 
-	<div>
-		<slot {isOpen} {toggle} />
+					<slot name="header-right" {isOpen} {toggle} />
+
+					<button
+						class="small"
+						data-after={isOpen ? '⏶' : '⏷'}
+						aria-controls={ariaId}
+						aria-expanded={isOpen ? 'true' : 'false'}
+						on:click={toggle}
+					>{isOpen ? 'Hide' : 'Show'}</button>
+					<!-- <button
+						class="small"
+						data-after={isOpen ? '▲' : '▼'}
+						aria-controls={ariaId}
+						aria-expanded={isOpen ? 'true' : 'false'}
+						on:click={toggle}
+					/> -->
+				</div>
+			</label>
+		</slot>
+
+		<div class="collapsible">
+			<slot {isOpen} {toggle} />
+		</div>
+
+		<slot name="footer" {isOpen} {toggle} />
 	</div>
 
-	<slot name="footer" {isOpen} {toggle} />
-</div>
+{:else if type === 'details'}
+	<details
+		class="container {$$props.class}"
+		bind:open={isOpen}
+	>
+		<slot name="header" {isOpen} {toggle}>
+			<summary class="bar" data-state={isOpen ? 'open' : 'closed'} >
+				<slot name="title">
+					<h4>{title}</h4>
+				</slot>
+
+				<div class="row">
+					<slot name="toolbar" {isOpen} {toggle}>
+						{#if $$slots['toolbar-items']}
+							<div role="toolbar">
+								<slot name="toolbar-items" {isOpen} {toggle} />
+							</div>
+						{/if}
+					</slot>
+
+					<slot name="header-right" {isOpen} {toggle} />
+
+					<button
+						class="small"
+						data-after={isOpen ? '⏶' : '⏷'}
+						on:click={toggle}
+					>{isOpen ? 'Hide' : 'Show'}</button>
+					<!-- <button
+						class="small"
+						data-after={isOpen ? '▲' : '▼'}
+						aria-controls={ariaId}
+						aria-expanded={isOpen ? 'true' : 'false'}
+						on:click={toggle}
+					/> -->
+				</div>
+			</summary>
+		</slot>
+
+		<div class="collapsible">
+			<slot {isOpen} {toggle} />
+		</div>
+
+		<slot name="footer" {isOpen} {toggle} />
+	</details>
+{/if}
