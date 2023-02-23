e08bb454aad62149da1cf1354763aa6f64c91ada
Darryl Yeo <dev@darryl-yeo.com>
2023-02-22 16:43:42 -0800

    <Collapsible>: support both <label> and <details> elements

--- src/components/Collapsible.svelte
+++ src/components/Collapsible.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let type: 'label' | 'details' = 'label'
+	export let type: 'label' | 'details' = 'details'
 
 	export let title: string
 
@@ -33,13 +33,35 @@
 		gap: 0;
 	}
 
+	label, summary {
+		margin: calc(-1 * var(--padding-inner));
+		padding: var(--padding-inner);
+	}
+	:is(label, summary):hover {
+		filter: brightness(1.1);
+	}
+	:is(label, summary):active {
+		scale: 0.992;
+		opacity: 0.75;
+		transition-duration: 0.15s;
+	}
+	.container[data-state="open"] label,
+	details[open] summary {
+		cursor: zoom-out;
+	}
+	.container[data-state="closed"] label,
+	details:not([open]) summary {
+		cursor: zoom-in;
+	}
+
 	.collapsible {
 		min-height: 0;
 		overflow: clip;
 		overflow-clip-margin: var(--padding-outer);
 		transform-origin: top;
 	}
-	.container[data-state="closed"] > .collapsible {
+	.container:is([data-state="closed"],
+	details:not([open])) > .collapsible {
 		/* transform: rotateX(-20deg); */
 		translate: 0 var(--padding-inner) -100px;
 
@@ -50,119 +72,80 @@
 		pointer-events: none;
 	}
 
-	label, summary {
-		margin: calc(-1 * var(--padding-inner));
-		padding: var(--padding-inner);
-	}
-	.container[data-state="open"] label,
-	details[open] summary {
-		cursor: zoom-out;
-	}
-	.container[data-state="closed"] label,
-	details:not([open]) summary {
-		cursor: zoom-in;
-	}
-	:is(label, summary):hover {
-		filter: brightness(1.1);
-	}
-	:is(label, summary):active {
-		scale: 0.992;
-		opacity: 0.75;
-		transition-duration: 0.15s;
+	::-webkit-details-marker {
+		display: none;
 	}
 </style>
 
 
-{#if type === 'label'}
-	<div
-		class="container {$$props.class}"
-		id={ariaId}
-		data-state={isOpen ? 'open' : 'closed'}
-	>
-		<slot name="header" {isOpen} {toggle}>
-			<label class="bar">
-				<slot name="title">
-					<h4>{title}</h4>
+<svelte:element
+	this={{
+		'label': 'div',
+		'details': 'details'
+	}[type]}
+	class="container {$$props.class}"
+	{...{
+		'label': {
+			'data-state': isOpen ? 'open' : 'closed',
+		},
+		'details': {
+			open: isOpen,
+		},
+	}[type]}
+	on:toggle={e => isOpen = e.target.open}
+>
+<!-- bind:open={isOpen} -->
+	<slot name="header" {isOpen} {toggle}>
+		<svelte:element
+			this={{
+				'label': 'label',
+				'details': 'summary'
+			}[type]}
+			class="bar"
+		>
+			<slot name="title">
+				<h4>{title}</h4>
+			</slot>
+
+			<div class="row">
+				<slot name="toolbar" {isOpen} {toggle}>
+					{#if $$slots['toolbar-items']}
+						<div role="toolbar">
+							<slot name="toolbar-items" {isOpen} {toggle} />
+						</div>
+					{/if}
 				</slot>
 
-				<div class="row">
-					<slot name="toolbar" {isOpen} {toggle}>
-						{#if $$slots['toolbar-items']}
-							<div role="toolbar">
-								<slot name="toolbar-items" {isOpen} {toggle} />
-							</div>
-						{/if}
-					</slot>
+				<slot name="header-right" {isOpen} {toggle} />
 
-					<slot name="header-right" {isOpen} {toggle} />
+				<button
+					class="small"
+					data-after={isOpen ? '⏶' : '⏷'}
+					{...type === 'label' ? {
+						'aria-controls': ariaId,
+						'aria-expanded': isOpen ? 'true' : 'false',
+					} : {}}
+					on:click={toggle}
+				>{isOpen ? 'Hide' : 'Show'}</button>
+				<!-- <button
+					class="small"
+					data-after={isOpen ? '▲' : '▼'}
+					aria-controls={ariaId}
+					aria-expanded={isOpen ? 'true' : 'false'}
+					on:click={toggle}
+				/> -->
+			</div>
+		</svelte:element>
+	</slot>
 
-					<button
-						class="small"
-						data-after={isOpen ? '⏶' : '⏷'}
-						aria-controls={ariaId}
-						aria-expanded={isOpen ? 'true' : 'false'}
-						on:click={toggle}
-					>{isOpen ? 'Hide' : 'Show'}</button>
-					<!-- <button
-						class="small"
-						data-after={isOpen ? '▲' : '▼'}
-						aria-controls={ariaId}
-						aria-expanded={isOpen ? 'true' : 'false'}
-						on:click={toggle}
-					/> -->
-				</div>
-			</label>
-		</slot>
-
-		<div class="collapsible">
-			<slot {isOpen} {toggle} />
-		</div>
-
-		<slot name="footer" {isOpen} {toggle} />
+	<div
+		class="collapsible"
+		{...type === 'label' ? {
+			id: ariaId,
+		} : {}}
+	>
+		<slot {isOpen} {toggle} />
 	</div>
 
-{:else if type === 'details'}
-	<details
-		class="container {$$props.class}"
-		bind:open={isOpen}
-	>
-		<slot name="header" {isOpen} {toggle}>
-			<summary class="bar">
-				<slot name="title">
-					<h4>{title}</h4>
-				</slot>
-
-				<div class="row">
-					<slot name="toolbar" {isOpen} {toggle}>
-						{#if $$slots['toolbar-items']}
-							<div role="toolbar">
-								<slot name="toolbar-items" {isOpen} {toggle} />
-							</div>
-						{/if}
-					</slot>
-
-					<slot name="header-right" {isOpen} {toggle} />
-
-					<button
-						class="small"
-						data-after={isOpen ? '⏶' : '⏷'}
-						on:click={toggle}
-					>{isOpen ? 'Hide' : 'Show'}</button>
-					<!-- <button
-						class="small"
-						data-after={isOpen ? '▲' : '▼'}
-						aria-controls={ariaId}
-						aria-expanded={isOpen ? 'true' : 'false'}
-						on:click={toggle}
-					/> -->
-				</div>
-			</summary>
-		</slot>
-
-		<div class="collapsible">
-			<slot {isOpen} {toggle} />
-		</div>
-
-		<slot name="footer" {isOpen} {toggle} />
-	</details>
-{/if}
+	<slot name="footer" {isOpen} {toggle} />
+</svelte:element>
