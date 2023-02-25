a931b39cfdf725e235f71e3eef13e937ebc41516
Darryl Yeo <dev@darryl-yeo.com>
2023-02-24 17:33:12 -0800

    <PortfolioAccount>: show loading status provider icons in section headers

--- src/components/PortfolioAccount.svelte
+++ src/components/PortfolioAccount.svelte
@@ -120,11 +120,14 @@
 	import EthereumNFTs from './EthereumNFTs.svelte'
 	import HeightContainer from './HeightContainer.svelte'
 	import InlineContainer from './InlineContainer.svelte'
+	import Loading from './Loading.svelte'
 	import NetworkIcon from './NetworkIcon.svelte'
 	import SizeContainer from './SizeContainer.svelte'
 	import TokenBalance from './TokenBalance.svelte'
 	import TweenedNumber from './TweenedNumber.svelte'
 
+
+	import { CovalentIcon, MoralisIcon, NFTPortIcon, QuickNodeIcon, ZapperIcon, ZerionIcon } from '../assets/icons'
 
 	import { tokenColors } from '../data/token-colors'
 
@@ -239,6 +242,26 @@
 
 	.is-zero {
 		opacity: 0.55;
+	}
+
+	.error-icon-container {
+		aspect-ratio: 1;
+	}
+	.error-icon-container > :first-child {
+		/* opacity: 0.75; */
+		filter: contrast(0.95) grayscale(0.75);
+	}
+	.error-icon-container > :last-child {
+		color: var(--primary-color);
+		font-size: 0.75em;
+		place-self: end;
+		justify-content: center;
+		align-items: center;
+		z-index: 1;
+		display: flex;
+		line-height: 0;
+		width: 0;
+		height: 0;
 	}
 
 	label {
@@ -401,7 +424,7 @@
 								isScrollable={!isGridLayout} isHorizontal={!isGridLayout}
 								bind:summary={balancesSummaries[i]}
 							>
-								<svelte:fragment slot="header" let:summary>
+								<svelte:fragment slot="header" let:summary let:status let:loadingMessage let:errorMessage>
 									<!-- {#if balances.length || isGridLayout} -->
 										<hr>
 
@@ -409,15 +432,55 @@
 											<!-- <span class="card-background"><NetworkIcon {network} /></span> -->
 											<h4 class="row">
 												<NetworkIcon {network} />
-												<Address {network} {address}><InlineContainer isOpen={!isEditing} clip><mark>{network.name}</mark>&nbsp;</InlineContainer>Balances</Address>
+												<Address {network} {address}><InlineContainer isOpen={!isEditing} clip containerClass="align-end"><mark>{network.name}</mark>&nbsp;</InlineContainer>Balances</Address>
 											</h4>
-											{#if summary}
-												<span class="summary" class:is-zero={!summary.filteredBalancesCount}>
-													<TokenBalance symbol={summary.quoteCurrency} balance={summary.quoteTotal} showPlainFiat={true} />
-													│
-													<strong><TweenedNumber value={summary.filteredBalancesCount} /></strong> tokens
-												</span>
-											{/if}
+											<InlineContainer containerClass="align-end" class="stack align-end">
+												{#if status === 'loading'}
+													<Loading
+														layout="icon"
+														iconAnimation="hover"
+														title={loadingMessage}
+													>
+														<img
+															slot="icon"
+															src={{
+																'Covalent': CovalentIcon,
+																'Moralis': MoralisIcon,
+																'QuickNode': QuickNodeIcon,
+																'Zapper': ZapperIcon,
+															}[tokenBalancesProvider]}
+															alt={tokenBalancesProvider}
+															width={20}
+														/>
+													</Loading>
+													<!-- <span transition:scale>Loading...</span> -->
+												{:else if status === 'error'}
+													<div class="error-icon-container stack" transition:scale>
+														<img
+															title={errorMessage}
+															src={{
+																'Covalent': CovalentIcon,
+																'Moralis': MoralisIcon,
+																'QuickNode': QuickNodeIcon,
+																'Zapper': ZapperIcon,
+															}[tokenBalancesProvider]}
+															alt={tokenBalancesProvider}
+															width={20}
+														/>
+														<span>⚠︎</span>
+													</div>
+												{:else if summary}
+													<span
+														class="summary align-end"
+														class:is-zero={!summary.filteredBalancesCount}
+														transition:scale
+													>
+														<TokenBalance symbol={summary.quoteCurrency} balance={summary.quoteTotal} showPlainFiat={true} />
+														│
+														<strong><TweenedNumber value={summary.filteredBalancesCount} /></strong> tokens
+													</span>
+												{/if}
+											</InlineContainer>
 											<InlineContainer containerClass="align-end" class="stack align-end">
 												{#if isEditing}
 													<button class="small" on:click={() => toggleSection('Balances')} transition:scale>Hide</button>
@@ -462,7 +525,7 @@
 							isScrollable={!isGridLayout}
 							bind:summary={defiAppsSummaries[i]}
 						>
-							<svelte:fragment slot="header" let:status let:summary>
+							<svelte:fragment slot="header" let:status let:summary let:loadingMessage let:errorMessage>
 								<!-- {#if (status === 'resolved' && summary?.defiAppsCount) || status === 'error' || isGridLayout} -->
 									<hr>
 
@@ -470,19 +533,53 @@
 										<!-- <span class="card-background"><NetworkIcon {network} /></span> -->
 										<h4 class="row">
 											<NetworkIcon {network} />
-											<span><InlineContainer isOpen={!isEditing} clip><mark>{network.name}</mark>&nbsp;</InlineContainer>DeFi</span>
+											<span><InlineContainer isOpen={!isEditing} clip containerClass="align-end"><mark>{network.name}</mark>&nbsp;</InlineContainer>DeFi</span>
 										</h4>
-										{#if summary}
-											<span class="summary" class:is-zero={!summary.defiAppsCount}>
-												<TokenBalance
-													symbol={summary.quoteTotalCurrency || quoteCurrency}
-													balance={summary.quoteTotal}
-													showPlainFiat={true}
-												/>
-												│
-												<strong><TweenedNumber value={summary.defiAppsCount} /></strong> app{summary.defiAppsCount === 1 ? '' : 's'}
-											</span>
-										{/if}
+										<InlineContainer containerClass="align-end" class="stack align-end">
+											{#if status === 'loading'}
+												<Loading
+													layout="icon"
+													iconAnimation="hover"
+													title={loadingMessage}
+												>
+													<img
+														slot="icon"
+														src={{
+															'Zerion DeFi SDK': ZerionIcon,
+															'Zapper': ZapperIcon,
+															'Covalent': CovalentIcon,
+														}[defiProvider]}
+														alt={defiProvider}
+														width={20}
+													/>
+												</Loading>
+												<!-- <span transition:scale>Loading...</span> -->
+											{:else if status === 'error'}
+												<div class="error-icon-container stack" transition:scale>
+													<img
+														title={errorMessage}
+														src={{
+															'Zerion DeFi SDK': ZerionIcon,
+															'Zapper': ZapperIcon,
+															'Covalent': CovalentIcon,
+														}[defiProvider]}
+														alt={defiProvider}
+														width={20}
+													/>
+													<span>⚠︎</span>
+												</div>
+											{:else if summary}
+												<span class="summary" class:is-zero={!summary.defiAppsCount}>
+													<TokenBalance
+														symbol={summary.quoteTotalCurrency || quoteCurrency}
+														balance={summary.quoteTotal}
+														showPlainFiat={true}
+													/>
+													│
+													<strong><TweenedNumber value={summary.defiAppsCount} /></strong> app{summary.defiAppsCount === 1 ? '' : 's'}
+												</span>
+											{/if}
+										</InlineContainer>
 										<InlineContainer containerClass="align-end" class="stack align-end">
 											{#if isEditing}
 												<button class="small" on:click={() => toggleSection('DeFi')} transition:scale>Hide</button>
@@ -516,7 +613,7 @@
 							isScrollable={!isGridLayout}
 							bind:summary={nftsSummaries[i]}
 						>
-							<svelte:fragment slot="header" let:summary>
+							<svelte:fragment slot="header" let:summary let:status let:loadingMessage let:errorMessage>
 								<!-- {#if balances?.length || isGridLayout} -->
 									<hr>
 
@@ -524,16 +621,48 @@
 										<h4 class="row">
 											<!-- <span class="card-background"><NetworkIcon {network} /></span> -->
 											<NetworkIcon {network} />
-											<span><InlineContainer isOpen={!isEditing} clip><mark>{network.name}</mark>&nbsp;</InlineContainer>NFTs</span>
+											<span><InlineContainer isOpen={!isEditing} clip containerClass="align-end"><mark>{network.name}</mark>&nbsp;</InlineContainer>NFTs</span>
 										</h4>
-										{#if summary}
-											<span class="summary" class:is-zero={!summary.nftsCount}>
-												<strong><TweenedNumber value={summary.nftsCount} /></strong> NFT{summary.nftsCount === 1 ? '' : 's'}
-												│
-												<!-- across -->
-												<strong><TweenedNumber value={summary.nftContractsCount} /></strong> collection{summary.nftContractsCount === 1 ? '' : 's'}
-											</span>
-										{/if}
+										<InlineContainer containerClass="align-end" class="stack align-end">
+											{#if status === 'loading'}
+												<Loading
+													layout="icon"
+													iconAnimation="hover"
+													title={loadingMessage}
+												>
+													<img
+														slot="icon"
+														src={{
+															'Covalent': CovalentIcon,
+															'NFTPort': NFTPortIcon,
+														}[nftProvider]}
+														alt={nftProvider}
+														width={20}
+													/>
+												</Loading>
+												<!-- <span transition:scale>Loading...</span> -->
+											{:else if status === 'error'}
+												<div class="error-icon-container stack" transition:scale>
+													<img
+														title={errorMessage}
+														src={{
+															'Covalent': CovalentIcon,
+															'NFTPort': NFTPortIcon,
+														}[nftProvider]}
+														alt={nftProvider}
+														width={20}
+													/>
+													<span>⚠︎</span>
+												</div>
+											{:else if summary}
+												<span class="summary" class:is-zero={!summary.nftsCount}>
+													<strong><TweenedNumber value={summary.nftsCount} /></strong> NFT{summary.nftsCount === 1 ? '' : 's'}
+													│
+													<!-- across -->
+													<strong><TweenedNumber value={summary.nftContractsCount} /></strong> collection{summary.nftContractsCount === 1 ? '' : 's'}
+												</span>
+											{/if}
+										</InlineContainer>
 										<InlineContainer containerClass="align-end" class="stack align-end">
 											{#if isEditing}
 												<button class="small" on:click={() => toggleSection('NFTs')} transition:scale>Hide</button>
