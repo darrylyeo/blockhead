ba7366906019e0df7e5aaad50266447e0f20fb61
Darryl Yeo <dev@darryl-yeo.com>
2023-02-25 18:35:39 -0800

    Introduce <GraphiqlExplorer> for exploring GraphQL queries

---
+++ src/components/GraphiqlExplorer.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let title = 'GraphiQL Explorer'
+
+	export let endpointUrl: string
+	export let query: string
+	export let variables: Record<string, string>
+</script>
+
+
+<style>
+	iframe {
+		border-radius: 0.5em;
+	}
+</style>
+
+
+<iframe
+	class="graphiql-explorer"
+	{title}
+	src="{endpointUrl}?{new URLSearchParams({
+		query,
+		variables: JSON.stringify(variables),
+		history: String(false),
+		prettify: String(true),
+		docs: String(true),
+	})}"
+/>
