f80fb5135dbb69abdce7039775458b630caede42
Darryl Yeo <dev@darryl-yeo.com>
2023-02-28 11:45:28 -0800

    chainsBySlip44: refactor

--- src/data/slip44.ts
+++ src/data/slip44.ts
@@ -1,7 +1,7 @@
 import slip44Constants from 'bip44-constants'
 
 export const chainsBySlip44 = Object.fromEntries(
-    slip44Constants.map(([slip44, symbol, name]) =>
-        [slip44 - slip44Constants[0][0], {symbol, name}]
-    )
+	slip44Constants.map(([slip44, symbol, name]) =>
+		[slip44 - slip44Constants[0][0], {symbol, name}]
+	)
 )
