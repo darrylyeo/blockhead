80fbf8178841df8965ad9814b191793a7fa2cfa0
Darryl Yeo <dev@darryl-yeo.com>
2023-03-26 04:31:00 -0700

    <Notifications>: improve layout

--- src/components/Notifications.svelte
+++ src/components/Notifications.svelte
@@ -21,7 +21,6 @@
 		for(const notification of notifications ?? [])
 			(notificationsByChannel[notification.app] ??= []).push(notification)
 	}
-	$: notifications
 
 	export let summary: {
 		notificationsCount: number,
@@ -42,8 +41,30 @@
 
 
 <style>
-	footer {
+	img {
+		border-radius: 0.33rem;
+		object-fit: contain;
+	}
+
+	/* .message {
+		font-size: 0.66em;
+	} */
+	.message {
 		font-size: 0.85em;
+		opacity: 0.75;
+		white-space: pre-line;
+		line-height: 1.2;
+	}
+	.card:not(:hover) .message {
+		line-clamp: 2;
+		display: -webkit-box;
+		-webkit-line-clamp: 3;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
+
+	footer {
+		font-size: 0.66em;
 		opacity: 0.8;
 	}
 </style>
@@ -57,50 +78,54 @@
 >
 	<slot slot="header" name="header" let:status let:loadingMessage let:errorMessage {summary} {status} {loadingMessage} {errorMessage} />
 
-	{#each console.log({notifications})||notifications as notification}<!-- (`${notification.blockchain}-${notification.sid}`) -->
+	{#each notifications as notification}<!-- (`${notification.blockchain}-${notification.sid}`) -->
 		{@const network = networksBySlug[pushSupportedChainIds[notification.blockchain] ?? 1]}
 
 		<article class="card">
 			<header class="bar">
 				<h4 class="row">
-					<img
-						src={notification.icon} 
-						height="24"
-						style="width: fit-content"
-					/>
-					<NetworkIcon {network} />
+					{#if notification.image}
+						<img
+							src={notification.image} 
+							height="24"
+							style="width: fit-content"
+							on:error={e => e.target.hidden = true}
+						/>
+					{/if}
+					<!-- <NetworkIcon {network} /> -->
+
 					<span>{notification.title}</span>
 				</h4>
-
-				<span class="card-annotation">
-					<a href={notification.url} target="_blank" rel="noreferrer">{notification.app}</a>
-				</span>
-			</header>
-
-			<hr>
-
-			{#if notification.image}
-				<img
-					src={notification.image} 
-					height="24"
-					style="width: fit-content"
-				/>
-			{/if}
-
-			<p>{notification.notification.body}</p>
-
-			{#if notification.message !== notification.notification.body}
-				<p><small>{notification.message}</small></p>
-			{/if}
-
-			<hr>
-
-			<footer>
-				<span>{notification.notification.title}</span>
 
 				{#if notification.cta}
 					<a href={notification.cta} target="_blank" rel="noreferrer"><button class="small">Go ›</button></a>
 				{/if}
+			</header>
+
+			<hr>
+
+			<p>{notification.notification.body}</p>
+
+			{#if notification.message !== notification.notification.body}
+				<p class="message">{notification.message}</p>
+			{/if}
+
+			<hr>
+
+			<footer class="bar">
+				<div class="row-inline">
+					<img
+						src={notification.icon} 
+						height="16"
+						style="width: fit-content"
+						on:error={e => e.target.hidden = true}
+					/>
+					<span>{notification.notification.title}</span>
+				</div>
+
+				<span class="card-annotation">
+					<a href={notification.url} target="_blank" rel="noreferrer">{notification.app}</a>
+				</span>
 			</footer>
 		</article>
 	{/each}
