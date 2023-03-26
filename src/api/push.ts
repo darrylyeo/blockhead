a0950f569a4adaa1c1e838db79680966036c8cb1
Darryl Yeo <dev@darryl-yeo.com>
2023-03-26 03:41:59 -0700

    Push: add API method to retrieve notifications by address; model types and constanrs

--- src/api/push.ts
+++ src/api/push.ts
@@ -1,23 +1,108 @@
 import type { Ethereum } from '../data/networks/types'
 
+export type AppName = string
+
+export type BlockchainName = 'ETH_MAINNET' | 'POLYGON_MAINNET' | string
+
+export type Notification = {
+	cta: string;
+	title: string;
+	message: string;
+	icon: string;
+	url: string;
+	sid: string;
+	app: AppName;
+	image: string;
+	blockchain: BlockchainName;
+	notification: {
+		body: string;
+		title: string;
+	};
+	secret: string;
+}
+
+export type NotificationRawPayload = {
+	payload_id: number;
+	sender: Ethereum.Address;
+	epoch: Date;
+	payload: {
+		data: {
+			app: AppName;
+			sid?: string;
+			url: string;
+			acta: string;
+			aimg: string;
+			amsg: string;
+			asub: string;
+			icon: string;
+			type: number | string;
+			epoch?: string;
+			etime?: null;
+			hidden: string;
+			sectype?: null;
+			additionalMeta?: null;
+			appbot?: string;
+			secret?: string;
+		};
+		recipients?: Record<Ethereum.Address | `eip155:${Ethereum.Address}`, object | null>;
+		notification: {
+			body: string;
+			title: string;
+		};
+		verificationProof: string;
+	};
+	source: BlockchainName;
+	etime: null;
+}
+
+
+import Constants, { ENV } from '@pushprotocol/restapi/src/lib/constants'
+
+export const pushSupportedChainIds = [...Constants.ETH_CHAINS, ...Constants.NON_ETH_CHAINS] as Ethereum.ChainID[]
+
+export const pushBlockchainNames: Record<typeof pushSupportedChainIds[number], BlockchainName> = {
+	1: 'ETH_MAINNET',
+	5: '',
+	137: 'POLYGON_MAINNET',
+	80001: '',
+	56: '',
+	97: '',
+	10: '',
+	420: '',
+} as const
+
 
 import * as PushAPI from '@pushprotocol/restapi'
-import { ENV } from '@pushprotocol/restapi/src/lib/constants'
+import { memoizedAsync } from '../utils/memoized'
 
+const getFeeds = memoizedAsync(PushAPI.user.getFeeds)
 
-export const getNotifications = async ({
+export const getNotifications = async <IsRaw extends boolean>({
 	network,
 	address,
+	raw = false,
+	page = 1,
+	limit = 100,
 }: {
 	network: Ethereum.Network,
 	address: Ethereum.Address,
+	raw?: IsRaw,
+	page?: number,
+	limit?: number,
 }) => {
-	const notifications = await PushAPI.user.getFeeds({
-		user: `eip155:${network.chainId}:${address}`, // user address in CAIP-10
+	if(!pushSupportedChainIds.includes(network.chainId))
+		throw new Error(`Push does not yet support ${network.name}.`)
+
+	const notifications = await getFeeds({
+		// user: `eip155:${network.chainId}:${address}`,
+		user: `eip155:1:${address}`, // currently returns notifications from all networks regardless of chainId specified in CAIP10 address
 		env: ENV.PROD,
-	});
+		spam: true,
+		page,
+		limit,
+		raw,
+	}) as IsRaw extends true ? NotificationRawPayload[] : Notification[]
 
-	console.log('Notifications', notifications)
-
-	return notifications
+	// Filter manually by specified network
+	return notifications.filter(notification => notification.blockchain === pushBlockchainNames[network.chainId])
 }
