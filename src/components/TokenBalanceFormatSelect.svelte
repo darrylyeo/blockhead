7cb1d686cad407f965f876680baee1d619875170
Darryl Yeo <dev@darryl-yeo.com>
2023-03-26 05:28:52 -0700

    <TokenBalanceFormatSelect>: add a "checkboxes" type that uses <input type="checkbox">

--- src/components/TokenBalanceFormatSelect.svelte
+++ src/components/TokenBalanceFormatSelect.svelte
@@ -5,19 +5,56 @@
 	import { fiatQuoteCurrencies } from '../data/currencies'
 
 
+	export let type: 'select' | 'checkboxes' = 'select'
+	export let quoteCurrency: QuoteCurrency	
+
+
 	export let tokenBalanceFormat: 'original' | 'converted' | 'both' = 'original'
-	export let quoteCurrency: QuoteCurrency
 </script>
 
 
-<select bind:value={tokenBalanceFormat}>
-	<option value="original">Amount</option>
-	<option value="converted">Value ({fiatQuoteCurrencies[quoteCurrency].symbol})</option>
-	<option value="both">Amount + Value ({fiatQuoteCurrencies[quoteCurrency].symbol})</option>
-	<!-- <option value="original">Balances</option>
-	<option value="converted">Quotes</option>
-	<option value="both">Balances + Quotes</option> -->
-	<!-- <option value="original">Token Amounts</option>
-	<option value="converted">Quote Values</option>
-	<option value="both">Amounts and Values</option> -->
-</select>
+{#if type ==='select'}
+	<select bind:value={tokenBalanceFormat}>
+		<option value="original">Amount</option>
+		<option value="converted">Value ({fiatQuoteCurrencies[quoteCurrency].symbol})</option>
+		<option value="both">Amount + Value ({fiatQuoteCurrencies[quoteCurrency].symbol})</option>
+		<!-- <option value="original">Balances</option>
+		<option value="converted">Quotes</option>
+		<option value="both">Balances + Quotes</option> -->
+		<!-- <option value="original">Token Amounts</option>
+		<option value="converted">Quote Values</option>
+		<option value="both">Amounts and Values</option> -->
+	</select>
+{:else if type === 'checkboxes'}
+	<label>
+		<input
+			type="checkbox"
+			checked={tokenBalanceFormat === 'original' || tokenBalanceFormat === 'both'}
+			on:change={e =>
+				tokenBalanceFormat = e.target.checked
+					? {
+						'converted': 'both',
+						'both': 'original',
+					}[tokenBalanceFormat]
+					: 'converted'
+			}
+		/>
+		<span>Amounts</span>
+	</label>
+
+	<label>
+		<input
+			type="checkbox"
+			checked={tokenBalanceFormat === 'converted' || tokenBalanceFormat === 'both'}
+			on:change={e =>
+				tokenBalanceFormat = e.target.checked
+					? {
+						'original': 'both',
+						'both': 'converted',
+					}[tokenBalanceFormat]
+					: 'original'
+			}
+		/>
+		<span>{fiatQuoteCurrencies[quoteCurrency].symbol}</span>
+	</label>
+{/if}
