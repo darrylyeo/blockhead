db81365618b57a8c831529903b752196ba93c886
Darryl Yeo <dev@darryl-yeo.com>
2023-04-02 22:29:36 -0700

    ⌐◨-◨

--- src/routes/apps/[web3AppSlug=isWeb3AppSlug]/+layout.svelte
+++ src/routes/apps/[web3AppSlug=isWeb3AppSlug]/+layout.svelte
@@ -39,10 +39,77 @@
 		gap: var(--padding-inner);
 		align-items: center;
 	}
+
+
+	/* Nouns DAO */
+	[data-app="nouns"] :global(:is(.card, select, button, input)) {
+		--nouns-border: var(--primary-color) 0.25rem solid;
+		border-radius: 0;
+	}
+	[data-app="nouns"] :global(.card) {
+		position: relative;
+		border: var(--nouns-border);
+	}
+	[data-app="nouns"] :global(.card:nth-last-child(n + 2):nth-child(2n + 1):before) {
+		content: '';
+		position: absolute;
+		top: calc(50% - 0.5rem * 0.75);
+		right: calc(100% + 0.25rem - 1px);
+		width: 0.5rem;
+		height: 0.5rem;
+		border-top: var(--nouns-border);
+		border-left: var(--nouns-border);
+		pointer-events: none;
+	}
+	[data-app="nouns"] :global(.card):nth-child(2n):before {
+		content: '';
+		position: absolute;
+		top: calc(50% - var(--padding-inner) * 0.75);
+		right: 100%;
+		width: calc(var(--padding-inner) + 0.25rem);
+		height: 0.5rem;
+		border-top: var(--nouns-border);
+		pointer-events: none;
+	}
+	[data-app="nouns"] :global(.card):is(:nth-last-child(2), :nth-child(2)):after {
+		content: '';
+		z-index: -1;
+		position: absolute;
+		inset: 0;
+		background: linear-gradient(
+			to right,
+			rgba(255, 255, 255, calc(0.08 + var(--is-light) * 0.2)) 49.5%,
+			rgba(0, 0, 0, calc(0.08 + var(--is-light) * 0.2)) 50.5%
+		);
+		pointer-events: none;
+		animation: Blink 10s infinite;
+	}
+	@keyframes Blink {
+		0%, 20%, 85% {
+			translate: 0;
+		}
+		35%, 45% {
+			translate: -1rem;
+		}
+		55%, 75% {
+			translate: 1rem;
+		}
+		98%, 100% {
+			scale: 1;
+		}
+		99% {
+			scale: 1 0;
+		}
+	}
 </style>
 
 
-<section class="column" in:fly={{x: 100}} out:fly={{x: -100}}>
+<section
+	class="column"
+	data-app={$web3AppConfig.slug}
+	in:fly={{x: 100}}
+	out:fly={{x: -100}}
+>
 	<form on:submit|preventDefault={() => $addressOrEnsName = currentAddressOrEnsName}>
 		<ExplorerInput bind:value={currentAddressOrEnsName} />
 		<button type="submit">Go</button>
