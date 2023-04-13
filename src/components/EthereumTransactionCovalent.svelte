ae396d37bef758bc3473459bbe70daaad785c882
Darryl Yeo <dev@darryl-yeo.com>
2023-04-12 18:34:28 -0700

    <EthereumTransactionCovalent>: update type imports

--- src/components/EthereumTransactionCovalent.svelte
+++ src/components/EthereumTransactionCovalent.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import type { Ethereum } from '../data/ethereum/types'
+	import type { Ethereum } from '../data/networks/types'
 	import type { Covalent } from '../api/covalent'
-	import type { QuoteCurrency } from '../data/currency/currency'
+	import type { QuoteCurrency } from '../data/currencies'
 
 
 	export let network: Ethereum.Network
