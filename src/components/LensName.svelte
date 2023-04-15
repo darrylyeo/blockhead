15bac11414d02b96ccefe06628a5521c175a48a4
Darryl Yeo <dev@darryl-yeo.com>
2023-04-16 06:47:32 +0900

    Lens: introduce <LensName> to format Lens handle and deep-link to account page

---
+++ src/components/LensName.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { LensInstance, LensName } from '../api/lens'
+
+
+	export let instance: LensInstance
+	export let lensName: LensName
+	export let linked = true
+</script>
+
+
+<style>
+	.lens-name {
+		font-family: var(--monospace-fonts), var(--base-fonts);
+	}
+</style>
+
+
+<svelte:element
+	this={linked ? 'a' : 'span'}
+	class="lens-name"
+	{...linked ? {
+		href: `/apps/lens/account/${lensName}`
+	} : {}}
+	title={lensName}
+>
+	<slot {lensName}>{lensName}</slot>
+</svelte:element>
