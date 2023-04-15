ddc75dbae706923d3db4bb0f1b62afb11afdb5a7
Darryl Yeo <dev@darryl-yeo.com>
2023-04-15 17:49:54 +0900

    <ConnectedAccountSelect>: select wallet type to trigger addAccountConnection() on the fly

--- src/components/ConnectedAccountSelect.svelte
+++ src/components/ConnectedAccountSelect.svelte
@@ -2,15 +2,15 @@
 	// Constants/types
 	import type { Ethereum } from '../data/networks/types'
 	import type { AccountConnectionState } from '../state/account'
+	import { wallets } from '../data/wallets'
+
+
+	// Context
+	import { accountConnections } from '../state/account'
 
 
 	// External state
 	export let required = false
-
-
-	// Functions
-	import { formatAddress } from '../utils/formatAddress'
-	import { accountConnections } from '../state/account'
 
 
 	// Shared state
@@ -21,6 +21,11 @@
 	// Computed
 	$: if(address) selectedAccount = $accountConnections.find(accountConnection => accountConnection.state?.address?.toLowerCase() === address?.toLowerCase())?.state || selectedAccount
 	// $: address = selectedAccount?.address
+
+
+	// Functions
+	import { formatAddress } from '../utils/formatAddress'
+	import { onEvent } from '../events/onEvent'
 </script>
 
 
@@ -28,12 +33,46 @@
 	class="connected-account-select"
 	bind:value={selectedAccount}
 	{required}
->
-	<option value={undefined} selected disabled>Choose account...</option>
+	on:input={e => {
+		const selectedOption = e.target.selectedOptions[0]
 
-	<!-- <optgroup label="Connected"> -->
-		{#each $accountConnections as accountConnection}
-			<option value={accountConnection.state}>{accountConnection.walletType}: {formatAddress(accountConnection.state?.address)}</option>
+		if(selectedOption?.value === 'blockhead_addAccountConnection'){
+			e.preventDefault()
+
+			const { walletType } = selectedOption.dataset
+
+			globalThis.dispatchEvent(new CustomEvent('blockhead_addAccountConnection', { detail: { walletType }, bubbles: true }))
+
+			// TODO: pass the actual accountConnection via event data
+			onEvent(
+				'Portfolio/AddAccount',
+				data => {
+					selectedAccount = $accountConnections.find(accountConnection => accountConnection.walletType === walletType)
+				},
+				{ once: true }
+			)
+		}
+	}}
+>
+	{#if $accountConnections.length}
+		<option value={undefined} selected disabled>Choose account...</option>
+
+		<!-- <optgroup label="Connected"> -->
+			{#each $accountConnections as accountConnection}
+				<option value={accountConnection.state}>{accountConnection.walletType}: {formatAddress(accountConnection.state?.address)}</option>
+			{/each}
+		<!-- </optgroup> -->
+
+		<optgroup label="+ Connect Wallet">
+			{#each wallets as {type, name, icon, colors}}
+				<option value="blockhead_addAccountConnection" data-wallet-type={type}>{name}</option>
+			{/each}
+		</optgroup>
+	{:else}
+		<option value={undefined} selected disabled>Connect Wallet...</option>
+
+		{#each wallets as {type, name, icon, colors}}
+			<option value="blockhead_addAccountConnection" data-wallet-type={type}>{name}</option>
 		{/each}
-	<!-- </optgroup> -->
+	{/if}
 </select>
