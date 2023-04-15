1e26f6f504a1b16d8bc2dc9cbb04ad1051efdacd
Darryl Yeo <dev@darryl-yeo.com>
2023-04-16 06:45:25 +0900

    Lens: create account page that resolves address or Lens handle from URL and shows all Lens profiles owned

---
+++ src/routes/apps/lens/account/[accountId]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	// Constants/types
+	import { type LensProfile, LensInstance } from '../../../../../api/lens'
+
+
+	// Params
+	import { accountId } from '../../../_appsParams'
+
+
+	// Internal state
+	let profiles: LensProfile[]
+	let instance = LensInstance.Polygon
+
+
+	// Components
+	import LensProfiles from '../../../../../components/LensProfiles.svelte'
+	import AccountIdResolver from '../../../../../components/AccountIdResolver.svelte';
+</script>
+
+
+<AccountIdResolver
+	accountId={$accountId || undefined}
+	let:address
+	let:lensName
+>
+	{#if address || lensName}
+		<LensProfiles {instance} {address} {lensName} bind:profiles />
+	{/if}
+</AccountIdResolver>
