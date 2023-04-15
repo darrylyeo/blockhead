61d017bd67d32b5016816012f283b676829bf505
Darryl Yeo <dev@darryl-yeo.com>
2023-04-15 10:35:45 +0900

    Lens: switch GraphQL client from Apollo to urql

--- src/api/lens.ts
+++ src/api/lens.ts
@@ -3,107 +3,110 @@ import type { BrandedString } from '../utils/branded'
 export type LensName = BrandedString<`${string}.${'lens' | 'test'}`>
 
 
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { Client, cacheExchange, fetchExchange, gql } from '@urql/svelte'
 
-const API_URL = 'https://api.lens.dev'
+let client: Client
 
-
-export const client = new ApolloClient({
-	uri: API_URL,
-	cache: new InMemoryCache()
-})
+export const getClient = () => client ||= (
+	new Client({
+		url: 'https://api.lens.dev',
+		exchanges: [
+			cacheExchange,
+			fetchExchange,
+		],
+	})
+)
 
 
 export const getProfile = async ({
-	lensName
+	lensName,
 }: {
-	lensName: LensName
+	lensName: LensName,
 }) => (
-	await client.query({
-		query: gql`
-			query Profile($handle: Handle!) {
-				profile(request: { handle: $handle }) {
-					id
-					name
-					bio
-					attributes {
-						displayType
-						traitType
-						key
-						value
+	await getClient().query(gql`
+		query Profile($handle: Handle!) {
+			profile(request: { handle: $handle }) {
+				id
+				name
+				bio
+				attributes {
+					displayType
+					traitType
+					key
+					value
+				}
+				followNftAddress
+				metadata
+				isDefault
+				picture {
+					... on NftImage {
+						contractAddress
+						tokenId
+						uri
+						verified
 					}
-					followNftAddress
-					metadata
-					isDefault
-					picture {
-						... on NftImage {
-							contractAddress
-							tokenId
-							uri
-							verified
+					... on MediaSet {
+						original {
+							url
+							mimeType
 						}
-						... on MediaSet {
-							original {
-								url
-								mimeType
+					}
+					__typename
+				}
+				handle
+				coverPicture {
+					... on NftImage {
+						contractAddress
+						tokenId
+						uri
+						verified
+					}
+					... on MediaSet {
+						original {
+							url
+							mimeType
+						}
+					}
+					__typename
+				}
+				ownedBy
+				dispatcher {
+					address
+					canUseRelay
+				}
+				stats {
+					totalFollowers
+					totalFollowing
+					totalPosts
+					totalComments
+					totalMirrors
+					totalPublications
+					totalCollects
+				}
+				followModule {
+					... on FeeFollowModuleSettings {
+						type
+						amount {
+							asset {
+								symbol
+								name
+								decimals
+								address
 							}
+							value
 						}
-						__typename
+						recipient
 					}
-					handle
-					coverPicture {
-						... on NftImage {
-							contractAddress
-							tokenId
-							uri
-							verified
-						}
-						... on MediaSet {
-							original {
-								url
-								mimeType
-							}
-						}
-						__typename
+					... on ProfileFollowModuleSettings {
+						type
 					}
-					ownedBy
-					dispatcher {
-						address
-						canUseRelay
-					}
-					stats {
-						totalFollowers
-						totalFollowing
-						totalPosts
-						totalComments
-						totalMirrors
-						totalPublications
-						totalCollects
-					}
-					followModule {
-						... on FeeFollowModuleSettings {
-							type
-							amount {
-								asset {
-									symbol
-									name
-									decimals
-									address
-								}
-								value
-							}
-							recipient
-						}
-						... on ProfileFollowModuleSettings {
-							type
-						}
-						... on RevertFollowModuleSettings {
-							type
-						}
+					... on RevertFollowModuleSettings {
+						type
 					}
 				}
 			}
-		`,
-		variables: { handle: lensName }
+		}
+	`, {
+		handle: lensName
 	})
 )
