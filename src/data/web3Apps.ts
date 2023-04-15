a4ee74f98f1877aa49df2eb70380ef746724fae4
Darryl Yeo <dev@darryl-yeo.com>
2023-04-15 21:55:43 +0900

    web3Apps: add Lens Protocol contracts

--- src/data/web3Apps.ts
+++ src/data/web3Apps.ts
@@ -4088,9 +4088,277 @@ export const web3Apps: Web3AppConfig[] = [
 		views: [
 			{
 				chainId: 137,
-				nfts: [
+				contracts: [
+					{
+						name: 'LensHub Proxy',
+						address: '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d',
+					},
+					{
+						name: 'LensHub Implementation',
+						address: '0xeCd1d11B69FFd9794d45269247f9Fc139462bC12',
+					},
+					{
+						name: 'PublishingLogic',
+						address: '0x931519D41797C73b9CE993B52c1af900373b5b43',
+					},
+					{
+						name: 'InteractionLogic',
+						address: '0xe71291f49C4b572EF6e864Ba54E50C13A6D407e7',
+					},
+					{
+						name: 'ProfileTokenURILogic',
+						address: '0x3FA902A571E941dCAc6081d57917994DDB0F9A9d',
+					},
+					{
+						name: 'FeeCollectModule',
+						address: '0x1292E6dF9a4697DAAfDDBD61D5a7545A634af33d',
+					},
+					{
+						name: 'LimitedFeeCollectModule',
+						address: '0xEF13EFa565FB29Cd55ECf3De2beb6c69bD988212',
+					},
+					{
+						name: 'TimedFeeCollectModule',
+						address: '0xbf4E6C28d7f37C867CE62cf6ccb9efa4C7676F7F',
+					},
+					{
+						name: 'LimitedTimedFeeCollectModule',
+						address: '0x7B94f57652cC1e5631532904A4A038435694636b',
+					},
+					{
+						name: 'RevertCollectModule',
+						address: '0xa31FF85E840ED117E172BC9Ad89E55128A999205',
+					},
+					{
+						name: 'FreeCollectModule',
+						address: '0x23b9467334bEb345aAa6fd1545538F3d54436e96',
+					},
+					{
+						name: 'FeeFollowModule',
+						address: '0x80ae0e6048d6e295Ee6520b07Eb6EC4485193FD6',
+					},
+					{
+						name: 'ProfileFollowModule',
+						address: '0x057ccDf5153bE1081830a6C3D507C9dfE1ac8e4E',
+					},
+					{
+						name: 'RevertFollowModule',
+						address: '0x6640e4Fb3fd56a6d7DfF3C351dFd9Ab7E57fb769',
+					},
+					{
+						name: 'FollowerOnlyReferenceModule',
+						address: '0x17317F96f0C7a845FFe78c60B10aB15789b57Aaa',
+					},
+					{
+						name: 'FollowNFT',
+						address: '0xb0298c5540f4cfb3840c25d290be3ef3fe09fa8c',
+					},
+					{
+						name: 'CollectNFT',
+						address: '0x2172758ebb894c43e0be01e37d065118317d7eec',
+					},
+					{
+						name: 'LensPeriphery',
+						address: '0xeff187b4190E551FC25a7fA4dFC6cf7fDeF7194f',
+					},
+					{
+						name: 'ModuleGlobals',
+						address: '0x3Df697FF746a60CBe9ee8D47555c88CB66f03BB9',
+					},
+					{
+						name: 'ProfileCreationProxy',
+						address: '0x1eeC6ecCaA4625da3Fa6Cd6339DBcc2418710E8a',
+					},
+					{
+						name: 'UIDataProvider',
+						address: '0x8b0A28a8DE1de77668260A876c6DCF0330183742',
+					},
 				],
-			}
+			},
+			{
+				chainId: 80001,
+				contracts: [
+					{
+						name: 'LensHub Proxy',
+						address: '0x60Ae865ee4C725cd04353b5AAb364553f56ceF82',
+					},
+					{
+						name: 'LensHub Implementation',
+						address: '0x45cf9Ba12b43F6c8B7148E06A6f84c5B9ad3Dd44',
+					},
+					{
+						name: 'PublishingLogic',
+						address: '0x7f9bfF8493F821111741b93429A6A6F79DC546F0',
+					},
+					{
+						name: 'InteractionLogic',
+						address: '0x845242e2Cd249af8D4f0D7085DefEAc3381815E3',
+					},
+					{
+						name: 'ProfileTokenURILogic',
+						address: '0xf62c27B7B70A33739A7C088097fc20609A80eE58',
+					},
+					{
+						name: 'FeeCollectModule',
+						address: '0xeb4f3EC9d01856Cec2413bA5338bF35CeF932D82',
+					},
+					{
+						name: 'LimitedFeeCollectModule',
+						address: '0xFCDA2801a31ba70dfe542793020a934F880D54aB',
+					},
+					{
+						name: 'TimedFeeCollectModule',
+						address: '0x36447b496ebc97DDA6d8c8113Fe30A30dC0126Db',
+					},
+					{
+						name: 'LimitedTimedFeeCollectModule',
+						address: '0xDa76E44775C441eF53B9c769d175fB2948F15e1C',
+					},
+					{
+						name: 'RevertCollectModule',
+						address: '0x5E70fFD2C6D04d65C3abeBa64E93082cfA348dF8',
+					},
+					{
+						name: 'FreeCollectModule',
+						address: '0x0BE6bD7092ee83D44a6eC1D949626FeE48caB30c',
+					},
+					{
+						name: 'FeeFollowModule',
+						address: '0xe7AB9BA11b97EAC820DbCc861869092b52B65C06',
+					},
+					{
+						name: 'ProfileFollowModule',
+						address: '0x8c32203df6b1A04E25145346e2DaAD0B4712C20D',
+					},
+					{
+						name: 'RevertFollowModule',
+						address: '0x8c822Fc029EBdE62Da1Ed1072534c5e112dAE48c',
+					},
+					{
+						name: 'FollowerOnlyReferenceModule',
+						address: '0x7Ea109eC988a0200A1F79Ae9b78590F92D357a16',
+					},
+					{
+						name: 'FollowNFT',
+						address: '0x1a2bb1bc90aa5716f5eb85fd1823338bd1b6f772',
+					},
+					{
+						name: 'CollectNFT',
+						address: '0x39dcB881eBdB0DF708412754468c99B4EbD2E370',
+					},
+					{
+						name: 'LensPeriphery',
+						address: '0xD5037d72877808cdE7F669563e9389930AF404E8',
+					},
+					{
+						name: 'ModuleGlobals',
+						address: '0x1353aAdfE5FeD85382826757A95DE908bd21C4f9',
+					},
+					{
+						name: 'MockProfileCreationProxy',
+						address: '0x420f0257D43145bb002E69B14FF2Eb9630Fc4736',
+					},
+					{
+						name: 'UIDataProvider',
+						address: '0x4fF8EB275b2817fB2e7893bFF7ae7994e54e0730',
+					},
+				]
+			},
+			{
+				name: 'Sandbox Mumbai',
+				slug: 'mumbai-sandbox',
+				chainId: 80001,
+				contracts: [
+					{
+						name: 'LensHub Proxy',
+						address: '0x7582177F9E536aB0b6c721e11f383C326F2Ad1D5',
+					},
+					{
+						name: 'LensHub Implementation',
+						address: '0x7836c7cb79b7f3d53e92c95bf43798ada212fe4e',
+					},
+					{
+						name: 'PublishingLogic',
+						address: '0xDaDB107743e91249929C55132A238a3E735eA7eC',
+					},
+					{
+						name: 'InteractionLogic',
+						address: '0xB91F340BEab90580d557F1332704bF5DB9c35A9D',
+					},
+					{
+						name: 'ProfileTokenURILogic',
+						address: '0x5A39B7A9507044509Ef97D4EA306Dc8bA9DB818e',
+					},
+					{
+						name: 'FeeCollectModule',
+						address: '0xD57C675b542bFC24242a1088514306fFaeaFc6Fb',
+					},
+					{
+						name: 'LimitedFeeCollectModule',
+						address: '0x27742f7e82356e1c1e4177C11f627B5Dc18E4686',
+					},
+					{
+						name: 'TimedFeeCollectModule',
+						address: '0x6Bf382f52398DAdD3969Fb9C1AB3a1Bab8268F4E',
+					},
+					{
+						name: 'LimitedTimedFeeCollectModule',
+						address: '0x75Df383C587B0de2EB00FeA95B9b8e3AF35EbCe6',
+					},
+					{
+						name: 'RevertCollectModule',
+						address: '0x75fe7513709E6Ca573d017cE979F7d35192CE0d5',
+					},
+					{
+						name: 'FreeCollectModule',
+						address: '0x11C45Cbc6fDa2dbe435C0079a2ccF9c4c7051595',
+					},
+					{
+						name: 'FeeFollowModule',
+						address: '0xE98a40DB1170B3A46ffa7bA84335A0A0e9A65C2d',
+					},
+					{
+						name: 'ProfileFollowModule',
+						address: '0x62D0EcAB45428087d91a94EE33E141B7496Ad7c3',
+					},
+					{
+						name: 'RevertFollowModule',
+						address: '0x65004331Ae1c3D35DF81aEC6E8C259167Bbe64E2',
+					},
+					{
+						name: 'FollowerOnlyReferenceModule',
+						address: '0xB080AAc00E53FBeb04917F22096721d602c70759',
+					},
+					{
+						name: 'FollowNFT',
+						address: '0xf51b134ca8f54fdf19eb49001fe337b1e93cf707',
+					},
+					{
+						name: 'CollectNFT',
+						address: '0x57F4040B6a38C26e8a92527e3495EB936496CCc9',
+					},
+					{
+						name: 'LensPeriphery',
+						address: '0xa6bcF4398824A199965f89094796DFCcEa81b1a6',
+					},
+					{
+						name: 'ModuleGlobals',
+						address: '0xcbCC5b9611d22d11403373432642Df9Ef7Dd81AD',
+					},
+					{
+						name: 'MockProfileCreationProxy',
+						address: '0x4fe8deB1cf6068060dE50aA584C3adf00fbDB87f',
+					},
+					{
+						name: 'MockSandboxGovernance',
+						address: '0x1677d9cc4861f1c85ac7009d5f06f49c928ca2ad',
+					},
+					{
+						name: 'UIDataProvider',
+						address: '0x5dDD1d6c04E805D830574A31Bf7979D416c6d7c5',
+					},
+				]
+			},
 		]
 	},
 	{
