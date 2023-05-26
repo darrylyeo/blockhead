443c4f66fd49693ba0b3572de21ec06d9cc494c9
Darryl Yeo <dev@darryl-yeo.com>
2023-05-25 22:04:33 -0700

    triggerEvent: check for undefined dispatchEvent (SSR)

--- src/events/triggerEvent.ts
+++ src/events/triggerEvent.ts
@@ -1,7 +1,7 @@
 import type { EventType, Events } from './events'
 
 export const triggerEvent = <T extends EventType>(type: EventType, data?: Events[T]) =>
-	globalThis.CustomEvent && globalThis.dispatchEvent(
+	globalThis.CustomEvent && globalThis.dispatchEvent?.(
 		new globalThis.CustomEvent('blockhead/event', {
 			detail: {
 				type,
