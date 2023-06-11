fd7e2f86289edc7d1e0f7d6181466c4f47305f16
Darryl Yeo <dev@darryl-yeo.com>
2023-06-10 23:01:26 -0700

    houdini: regenerate

--- $houdini/types/src/routes/apps/ipfs/$houdini.d.ts
+++ $houdini/types/src/routes/apps/ipfs/$houdini.d.ts
@@ -2,17 +2,17 @@ import type * as Kit from '@sveltejs/kit';
 
 type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
 type RouteParams = {  }
-type RouteId = '/apps';
+type RouteId = '/apps/ipfs';
 type MaybeWithVoid<T> = {} extends T ? T | void : T;
 export type RequiredKeys<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } ? never : K; }[keyof T];
 type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Partial<Pick<App.PageData, keyof T & keyof App.PageData>> & Record<string, any>>
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
 export type Snapshot<T = any> = Kit.Snapshot<T>;
-type PageParentData = Omit<EnsureDefined<import('../$houdini').LayoutData>, keyof LayoutData> & EnsureDefined<LayoutData>;
-type LayoutRouteId = RouteId | "/apps" | "/apps/[web3AppSlug=isWeb3AppSlug]" | "/apps/[web3AppSlug=isWeb3AppSlug]/account/[accountId]" | "/apps/[web3AppSlug=isWeb3AppSlug]/address/[accountId]" | "/apps/audius" | "/apps/audius/playlist/[audiusPlaylistId]" | "/apps/audius/search/[audiusQuery]" | "/apps/audius/track/[audiusTrackId]" | "/apps/audius/user/[audiusUserId]" | "/apps/ens/account/[accountId]" | "/apps/ipfs" | "/apps/ipfs/content/[ipfsContentId]" | "/apps/ipfs/content/[ipfsContentId]/[...ipfsContentPath]" | "/apps/lens" | "/apps/lens/account/[accountId]"
-type LayoutParams = RouteParams & { web3AppSlug?: string,accountId?: string,audiusPlaylistId?: string,audiusQuery?: string,audiusTrackId?: string,audiusUserId?: string,ipfsContentId?: string,ipfsContentPath?: string }
-type LayoutParentData = EnsureDefined<import('../$houdini').LayoutData>;
+type PageParentData = Omit<Omit<EnsureDefined<import('../../$houdini').LayoutData>, keyof import('../$houdini').LayoutData> & EnsureDefined<import('../$houdini').LayoutData>, keyof LayoutData> & EnsureDefined<LayoutData>;
+type LayoutRouteId = RouteId | "/apps/ipfs" | "/apps/ipfs/content/[ipfsContentId]" | "/apps/ipfs/content/[ipfsContentId]/[...ipfsContentPath]"
+type LayoutParams = RouteParams & { ipfsContentId?: string,ipfsContentPath?: string }
+type LayoutParentData = Omit<EnsureDefined<import('../../$houdini').LayoutData>, keyof import('../$houdini').LayoutData> & EnsureDefined<import('../$houdini').LayoutData>;
 						type MakeOptional<Target, Keys extends keyof Target> = Omit<Target, Keys> & {
 							[Key in Keys]?: Target[Key] | undefined | null
 						}
