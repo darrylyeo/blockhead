91cb810906e8c5bc7f94b371211f8e5718549fe1
Darryl Yeo <dev@darryl-yeo.com>
2023-06-11 16:21:54 -0700

    <BlockTransition>: apply transition to #key'ed content within an <SizeContainer> stack

---
+++ src/components/BlockTransition.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { type EasingFunction, type TransitionConfig, scale } from 'svelte/transition'
+
+
+    type Key = $$Generic<any>
+    type Value = $$Generic<any>
+    type TransitionParams = $$Generic<{delay?: number; duration?: number; easing?: EasingFunction}>
+
+
+    export let key: Key | undefined
+    export let value: Value | undefined
+
+
+    export let align: 'top' | 'center' | 'bottom' = 'top'
+    export let transition: (node: Element, _?: TransitionParams) => TransitionConfig = scale
+    export let transitionParams: TransitionParams | undefined
+
+
+    import SizeContainer from './SizeContainer.svelte'
+</script>
+
+
+<SizeContainer class="stack align-{align}" {...$$restProps}>{#key key ?? value}<span class="column align-{align}" transition:transition|local={transitionParams}><slot {key} {value}>{value}</slot></span>{/key}</SizeContainer>
