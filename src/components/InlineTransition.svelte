188648b1d2398991de138fbfb02a07690c7414f5
Darryl Yeo <dev@darryl-yeo.com>
2023-06-11 00:47:40 -0700

    <InlineTransition>: apply transition to #key'ed content within an <InlineContainer> stack

---
+++ src/components/InlineTransition.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { type EasingFunction, type TransitionConfig, scale } from 'svelte/transition'
+
+
+    type Key = $$Generic<any>
+    type Value = $$Generic<any>
+    type TransitionParams = $$Generic<{delay?: number; duration?: number; easing?: EasingFunction}>
+
+
+    export let key: Key | undefined
+    export let value: Value | undefined
+
+
+    export let align: 'start' | 'center' | 'end' = 'start'
+    export let transition: (node: Element, _?: TransitionParams) => TransitionConfig = scale
+    export let transitionParams: TransitionParams | undefined
+
+
+    import InlineContainer from './InlineContainer.svelte'
+</script>
+
+
+<InlineContainer class="stack-inline align-{align}" {...$$restProps}>{#key key ?? value}<span class="row-inline align-{align}" transition:transition|local={transitionParams}><slot {key} {value}>{value}</slot></span>{/key}</InlineContainer>
