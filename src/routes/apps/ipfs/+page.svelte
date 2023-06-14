7a2ebe995d20b289d9dbf08a3dda1f0af2b84c0c
Darryl Yeo <dev@darryl-yeo.com>
2023-06-13 23:37:28 -0700

    IPFS app: make <IpfsLocalNode> accessible from main app page

---
+++ src/routes/apps/ipfs/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	// Context
+	import { ipfsGateway } from './_context'
+
+
+	// Components
+	import IpfsLocalNode from '../../../components/IpfsLocalNode.svelte'
+</script>
+
+
+<IpfsLocalNode
+	ipfsGateway={$ipfsGateway}
+/>
