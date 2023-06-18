5df0f9c8072012144fbf52f5eae55686ee68a3d5
Darryl Yeo <dev@darryl-yeo.com>
2023-06-18 05:58:06 -0700

    <FileList>: display files from a FileList object with <FileDetails>

---
+++ src/components/FileList.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	// External state
+	export let fileList: FileList | File[]
+
+
+	// Components
+	// import Date from './Date.svelte'
+	import FileDetails from './FileDetails.svelte'
+</script>
+
+
+{#if fileList}
+	{#each Array.from(fileList) as file}
+		<FileDetails
+			fileName={file.name}
+			contentType={file.type}
+			contentSize={file.size}
+			lastModified={file.lastModified}
+		/>
+		<!-- <article class="card">
+			<header class="bar wrap">
+				<span>{file.name}</span>
+
+				<span class="card-annotation">
+					{file.type || 'file'}
+					| {file.size} bytes
+					{#if file.lastModified}| last modified <Date date={file.lastModified} />{/if}
+				</span>
+			</header>
+		</article> -->
+	{/each}
+{/if}
