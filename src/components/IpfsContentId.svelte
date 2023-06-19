0d289d97e5cab2eac7f1410438f4a976965d8706
Darryl Yeo <dev@darryl-yeo.com>
2023-06-19 12:11:24 -0700

    <IpfsContentId>: implement drag events / data transfer

--- src/components/IpfsContentId.svelte
+++ src/components/IpfsContentId.svelte
@@ -18,6 +18,14 @@
 		ipfsContentId,
 		...ipfsContentPath ? ipfsContentPath.split('/') : [],
 	]
+	$: link = `/apps/ipfs/content/${parts.map(encodeURIComponent).join('/')}`
+
+
+	// Actions
+	const onDragStart = (e: DragEvent) => {
+		e.dataTransfer.setData('text/plain', ipfsContentId)
+		if(linked) e.dataTransfer.setData('text/uri-list', link)
+	}
 </script>
 
 
@@ -35,7 +43,11 @@
 	>
 		<slot {ipfsContentId} {ipfsContentPath}>
 			{#each parts as part, i}
-				{#if i > 0}/{/if}<a href={`/apps/ipfs/content/${parts.slice(0, i + 1).map(encodeURIComponent).join('/')}`}>{part}</a>
+				{#if i > 0}/{/if}<a
+					href={`/apps/ipfs/content/${parts.slice(0, i + 1).map(encodeURIComponent).join('/')}`}
+					draggable={true}
+					on:dragstart={onDragStart}
+				>{part}</a>
 			{/each}
 		</slot>
 	</span>
@@ -44,9 +56,11 @@
 		this={linked ? 'a' : 'span'}
 		class="ipfs-content-id"
 		{...linked && {
-			href: `/apps/ipfs/content/${parts.map(encodeURIComponent).join('/')}`
+			href: link
 		}}
 		title={ipfsContentId}
+		draggable={true}
+		on:dragstart={onDragStart}
 	>
 		<slot {ipfsContentId} {ipfsContentPath}>{fullPath}</slot>
 	</svelte:element>
