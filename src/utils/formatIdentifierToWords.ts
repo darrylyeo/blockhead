a5852557d974416dd2c4b5422429d23752a12c6f
Darryl Yeo <dev@darryl-yeo.com>
2023-08-29 12:26:53 -0700

    formatIdentifierToWords: improve regex to account for numbers, all-capital words

--- src/utils/formatIdentifierToWords.ts
+++ src/utils/formatIdentifierToWords.ts
@@ -1,2 +1,6 @@
 export const formatIdentifierToWords = (str: string, titlecase = false) =>
-	str.replace(/^.|[A-Z]+/g, m => ` ${titlecase ? m.toUpperCase() : m.toLowerCase()}`).replace(/_/g, ' ').trim()
+	[...str.matchAll(/(?:[0-9]+|[A-Z]+(?=[A-Z][a-z]|[^a-z]|$)|[A-Z][a-z]+|[a-z]+)/g)]
+		.map(([word]) =>
+			titlecase ? `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}` : word.toLowerCase()
+		)
+		.join(' ')
