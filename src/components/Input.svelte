f23eaaf3056e5f9bb122a4fff11be0d428d1f39c
Darryl Yeo <dev@darryl-yeo.com>
2023-09-15 19:56:28 -0700

    <Input>: render <input> and associated <datalist>

---
+++ src/components/Input.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	// External state
+	export let value: string = ''
+
+	export let suggestedValues: Datalist<typeof value>['$$prop_def']['list'] | undefined
+
+	
+	// Internal state
+	let datalistId: Datalist<typeof value>['$$prop_def']['datalistId']
+
+
+	// Components
+	import Datalist from './Datalist.svelte'
+</script>
+
+
+<input
+	bind:value
+	list={datalistId}
+	on:input
+	on:change
+	{...$$restProps}
+/>
+
+<Datalist
+	bind:datalistId
+	list={suggestedValues}
+/>
