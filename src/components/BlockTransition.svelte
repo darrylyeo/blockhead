705cf7de30bb66e3da052d7ba4e13e997a870111
Darryl Yeo <dev@darryl-yeo.com>
2023-09-23 08:47:58 -0700

    <BlockTransition>: support in/out transitions

--- src/components/BlockTransition.svelte
+++ src/components/BlockTransition.svelte
@@ -15,6 +15,10 @@
 	export let align: 'top' | 'center' | 'bottom' = 'top'
 	export let transition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined
 	export let transitionParams: TransitionParams | undefined
+	export let inTransition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined
+	export let inTransitionParams: TransitionParams | undefined
+	export let outTransition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined
+	export let outTransitionParams: TransitionParams | undefined
 
 
 	import SizeContainer from './SizeContainer.svelte'
@@ -22,4 +26,4 @@
 
 
 <!-- <SizeContainer class="stack align-{align}" {...$$restProps}>{#key key ?? value}<svelte:element this={element} class="column align-{align}" transition:transition={transitionParams}><slot {key} {value}>{value}</slot></svelte:element>{/key}</SizeContainer> -->
-<SizeContainer class="stack align-{align}" {...$$restProps}>{#key key ?? value}{#if transition}<span class="column align-{align}" transition:transition={transitionParams}><slot {key} {value}>{value}</slot></span>{/if}{/key}</SizeContainer>
+<SizeContainer class="stack align-{align}" {...$$restProps}>{#key key ?? value}{#if transition}<span class="column align-{align}" transition:transition={transitionParams}><slot {key} {value}>{value}</slot></span>{:else if inTransition && outTransition}<span class="column align-{align}" in:inTransition={inTransitionParams} out:outTransition={outTransitionParams}><slot {key} {value}>{value}</slot></span>{/if}{/key}</SizeContainer>
