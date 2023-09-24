27c679466726028ad78ec70d79d78cf45b721596
Darryl Yeo <dev@darryl-yeo.com>
2023-09-24 01:39:34 -0700

    Uniswap V3: update layout for showing account's existing positions

--- src/routes/apps/(withDashboard)/uniswap/+page.svelte
+++ src/routes/apps/(withDashboard)/uniswap/+page.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	// Constants/types
+	import { UniswapV3 } from '../../../../api/uniswap-v3'
+
+
 	// Params
 	import { accountId } from '../../_appsParams'
 
@@ -7,13 +11,18 @@
 	import { network } from '../../_appsContext'
 
 
+	// Internal state
+	let dataProvider = UniswapV3.DataProvider.TheGraph
+
+
 	// Components
 	import AccountIdResolver from '../../../../components/AccountIdResolver.svelte'
 	import UniswapV3SwapForm from '../../../../components/UniswapV3SwapForm.svelte'
-	import UniswapV3PositionManageForm from '../../../../components/UniswapV3PositionManageForm.svelte'
+	import UniswapV3Position from '../../../../components/UniswapV3Position.svelte'
 	import UniswapV3PositionCreateForm from '../../../../components/UniswapV3PositionCreateForm.svelte'
 	import UniswapV3PositionsLoader from '../../../../components/UniswapV3PositionsLoader.svelte'
 	import TokenListLoader from '../../../../components/TokenListLoader.svelte'
+	import TweenedNumber from '../../../../components/TweenedNumber.svelte'
 </script>
 
 
@@ -41,16 +50,45 @@
 
 		{#if address}
 			<UniswapV3PositionsLoader
-				network={$network}
+				layout="collapsible"
+				collapsibleType="label"
+				containerClass="card"
 				{address}
+				{dataProvider}
+				network={$network}
 				let:positions
 			>
-				{#each positions as position}
-					<UniswapV3PositionManageForm
-						network={$network}
-						{position}
-					/>
-				{/each}
+				<svelte:fragment slot="header"
+					let:positions
+					let:isOpen let:toggle
+				>
+					<header class="bar">
+						<h4>Positions</h4>
+
+						<div class="row">
+							<div class="summary">
+								{#if positions?.length}
+									<span><TweenedNumber value={positions.length} /> positions</span>
+								{/if}
+							</div>
+
+							<button
+								class="small"
+								data-after={isOpen ? '⏶' : '⏷'}
+								on:click={toggle}
+							>{isOpen ? 'Hide' : 'Show'}</button>
+						</div>
+					</header>
+				</svelte:fragment>
+
+				<div class="scrollable-list column">
+					{#each positions as position}
+						<UniswapV3Position
+							network={$network}
+							{position}
+						/>
+					{/each}
+				</div>
 			</UniswapV3PositionsLoader>
 		{/if}
 	</TokenListLoader>
