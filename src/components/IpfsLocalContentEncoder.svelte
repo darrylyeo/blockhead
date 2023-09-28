96a8acbe6ced3fa17fc200c51540905f39c86c3c
Darryl Yeo <dev@darryl-yeo.com>
2023-09-27 18:23:51 -0700

    <IpfsLocalContentEncoder>: remove dependency on selected IPFS gateway

--- src/components/IpfsLocalContentEncoder.svelte
+++ src/components/IpfsLocalContentEncoder.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	// Constants
-	import { type IpfsGatewayConfig, IpfsGatewayProvider } from '../data/ipfsGateways'
-
-
 	// External state
-	export let ipfsGateway: IpfsGatewayConfig
 	export let content: string | Blob | File | FileList
 
 
@@ -18,53 +13,51 @@
 </script>
 
 
-{#if ipfsGateway.gatewayProvider === IpfsGatewayProvider.Helia}
-	{#if content instanceof FileList}
-		<Loader
-			fromPromise={async () => (
-				await encodeFiles(content)
-			)}
-			loadingIcon={IpfsIcon}
-			loadingIconName='IPFS'
-			loadingMessage={`Encoding ${content.length} files using local IPFS node...`}
-			errorMessage={`Couldn't encode files using local IPFS node.`}
-			{...$$restProps}
-			let:result={addedFiles}
-		>
-			<slot slot='header' name='header' {addedFiles} />
-			<slot {addedFiles} />
-		</Loader>
+{#if content instanceof FileList}
+	<Loader
+		fromPromise={async () => (
+			await encodeFiles(content)
+		)}
+		loadingIcon={IpfsIcon}
+		loadingIconName='IPFS'
+		loadingMessage={`Encoding ${content.length} files using local IPFS node...`}
+		errorMessage={`Couldn't encode files using local IPFS node.`}
+		{...$$restProps}
+		let:result={addedFiles}
+	>
+		<slot slot='header' name='header' {addedFiles} />
+		<slot {addedFiles} />
+	</Loader>
 
-	{:else if content instanceof File}
-		<Loader
-			fromPromise={async () => (
-				await encodeFile(content)
-			)}
-			loadingIcon={IpfsIcon}
-			loadingIconName='IPFS'
-			loadingMessage={`Encoding file using local IPFS node...`}
-			errorMessage={`Couldn't encode file using local IPFS node.`}
-			{...$$restProps}
-			let:result={cid}
-		>
-			<slot slot='header' name='header' {cid} />
-			<slot {cid} />
-		</Loader>
+{:else if content instanceof File}
+	<Loader
+		fromPromise={async () => (
+			await encodeFile(content)
+		)}
+		loadingIcon={IpfsIcon}
+		loadingIconName='IPFS'
+		loadingMessage={`Encoding file using local IPFS node...`}
+		errorMessage={`Couldn't encode file using local IPFS node.`}
+		{...$$restProps}
+		let:result={cid}
+	>
+		<slot slot='header' name='header' {cid} />
+		<slot {cid} />
+	</Loader>
 
-	{:else}
-		<Loader
-			fromPromise={async () => (
-				await encodeBytes(content)
-			)}
-			loadingIcon={IpfsIcon}
-			loadingIconName='IPFS'
-			loadingMessage={`Encoding content using local IPFS node...`}
-			errorMessage={`Couldn't encode content using local IPFS node.`}
-			{...$$restProps}
-			let:result={cid}
-		>
-			<slot slot='header' name='header' {cid} />
-			<slot {cid} />
-		</Loader>
-	{/if}
+{:else}
+	<Loader
+		fromPromise={async () => (
+			await encodeBytes(content)
+		)}
+		loadingIcon={IpfsIcon}
+		loadingIconName='IPFS'
+		loadingMessage={`Encoding content using local IPFS node...`}
+		errorMessage={`Couldn't encode content using local IPFS node.`}
+		{...$$restProps}
+		let:result={cid}
+	>
+		<slot slot='header' name='header' {cid} />
+		<slot {cid} />
+	</Loader>
 {/if}
