fad45ab6a67d2dc9ea698d448fb2b7fca49ccb28
Darryl Yeo <dev@darryl-yeo.com>
2023-10-17 19:53:25 -0700

    DID Provider: keep track of DID providers including Ceramic

---
+++ src/data/didProvider.ts
@@ -0,0 +1,10 @@
+export enum DidProvider {
+	Ceramic = 'Ceramic Network',
+}
+
+
+import { CeramicIcon } from '../assets/icons'
+
+export const didProviderIcons = {
+	[DidProvider.Ceramic]: CeramicIcon,
+} as const satisfies Record<DidProvider, string>
