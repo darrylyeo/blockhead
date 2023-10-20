8a1a3924a93f762f0313eabe94c3b7e28b6bf181
Darryl Yeo <dev@darryl-yeo.com>
2023-10-20 14:37:23 -0700

    <EthereumTransactionsLoader>: refactor to use conditional attribute spread syntax

--- src/components/EthereumTransactionsLoader.svelte
+++ src/components/EthereumTransactionsLoader.svelte
@@ -69,193 +69,125 @@
 </script>
 
 
-{#if transactionProvider === TransactionProvider.Covalent}
-	<Loader
-		layout="collapsible"
-		loadingIcon={transactionProviderIcons[transactionProvider]}
-		loadingIconName={transactionProvider}
-		{loadingMessage}
-		{errorMessage}
-		fromInfiniteQuery={createInfiniteQuery({
-			queryKey: ['Transactions', {
-				transactionProvider,
-				chainID: network.chainId,
-				address,
-				quoteCurrency,
-			}],
-			queryFn: async ({ pageParam: pageNumber }) => {
-				const result = await getTransactionsByAddress({
+<Loader
+	layout="collapsible"
+	loadingIcon={transactionProviderIcons[transactionProvider]}
+	{loadingMessage}
+	{errorMessage}
+	{...{
+		[TransactionProvider.Covalent]: {
+			fromInfiniteQuery: createInfiniteQuery({
+				queryKey: ['Transactions', {
+					transactionProvider,
 					chainID: network.chainId,
 					address,
-					includeLogs,
 					quoteCurrency,
-					pageNumber: pageNumber ?? 0,
-					pageSize: 100,
-				})
+				}],
+				queryFn: async ({ pageParam: pageNumber }) => {
+					const result = await getTransactionsByAddress({
+						chainID: network.chainId,
+						address,
+						includeLogs,
+						quoteCurrency,
+						pageNumber: pageNumber ?? 0,
+						pageSize: 100,
+					})
 
-				return result
-			},
-			getPreviousPageParam: (firstPage, allPages) => firstPage.pagination?.page_number > 0 ? firstPage.pagination.page_number - 1 : undefined,
-			getNextPageParam: (lastPage, allPages) => lastPage.pagination?.has_more ? lastPage.pagination.page_number + 1 : undefined
-		})}
-		then={result => result?.pages?.flatMap(page => page.items) ?? []}
-		{...$$restProps}
-		bind:result={transactions}
-		let:result={transactions}
-		let:pagination
-	>
-		<svelte:fragment slot="header"
-			let:result={transactions}
-			let:status
-			let:pagination
-		>
-			<slot name="header" {transactions} {status} {pagination} />
-		</svelte:fragment>
-		<slot {transactions} {pagination} />
-	</Loader>
+					return result
+				},
+				getPreviousPageParam: (firstPage, allPages) => firstPage.pagination?.page_number > 0 ? firstPage.pagination.page_number - 1 : undefined,
+				getNextPageParam: (lastPage, allPages) => lastPage.pagination?.has_more ? lastPage.pagination.page_number + 1 : undefined
+			}),
+			then: result => result?.pages?.flatMap(page => page.items) ?? [],
+		},
 
-{:else if transactionProvider === TransactionProvider.Etherscan}
-	<Loader
-		layout="collapsible"
-		loadingIcon={transactionProviderIcons[transactionProvider]}
-		{loadingMessage}
-		{errorMessage}
-		fromQuery={createQuery({
-			queryKey: ['Transactions', {
-				transactionProvider,
-				chainID: network.chainId,
-				address,
-			}],
-			queryFn: async () => (
-				await Etherscan.Accounts.getTransactions({
-					chainId: network.chainId,
+		[TransactionProvider.Etherscan]: {
+			fromQuery: createQuery({
+				queryKey: ['Transactions', {
+					transactionProvider,
+					chainID: network.chainId,
 					address,
-				})
-			)
-		})}
-		then={transactions => transactions.map(transaction => normalizeEtherscanTransaction(network, transaction))}
-		{...$$restProps}
-		bind:result={transactions}
+				}],
+				queryFn: async () => (
+					await Etherscan.Accounts.getTransactions({
+						chainId: network.chainId,
+						address,
+					})
+				)
+			}),
+			then: transactions => transactions.map(transaction => normalizeEtherscanTransaction(network, transaction)),
+		},
+
+		[TransactionProvider.Moralis]: {
+			fromInfiniteQuery: createInfiniteQuery({
+				queryKey: ['Transactions', {
+					transactionProvider,
+					chainID: network.chainId,
+					address,
+					quoteCurrency,
+				}],
+				queryFn: async ({ pageParam }) => (
+					await MoralisWeb3Api.address.getTransactions({
+						chain: chainCodeFromNetwork(network),
+						from_block: 0,
+						// to_block: ,
+						offset: pageParam?.offset ?? 0,
+						limit: pageParam?.limit ?? 100,
+						address
+					})
+				),
+				// queryFn: async ({ pageParam: offset = 0 }) => {
+				// 	const {result: transactions, total, page_size, page} = await MoralisWeb3Api.address.getTransactions({
+				// 		chain: chainCodeFromNetwork(network),
+				// 		from_block: 0,
+				// 		// to_block: ,
+				// 		offset: offset ?? 0,
+				// 		limit: 100,
+				// 		address
+				// 	})
+
+				// 	// const logs = (await MoralisWeb3Api.address.getLogsByAddress({
+				// 	// 	chain: chainCodeFromNetwork(network),
+				// 	// 	from_block: 0,
+				// 	// 	address: transactions[0].hash
+				// 	// }))
+
+				// 	// console.log(transactions[0], logs)
+
+				// 	return (
+				// 		includeLogs
+				// 			? await Promise.all(transactions.map(transaction => (
+				// 				MoralisWeb3Api.transaction.getTransaction({
+				// 					chain: chainCodeFromNetwork(network),
+				// 					transactionHash: transaction.hash
+				// 				})
+				// 			)))
+				// 			: transactions
+				// 	).reverse()
+				// },
+				getPreviousPageParam: (firstPage, allPages) => {
+					const offset = (firstPage.page - 1) * firstPage.page_size
+					return offset > 0 ? { offset, limit: firstPage.page_size } : undefined
+				},
+				getNextPageParam: (lastPage, allPages) => {
+					const offset = (lastPage.page + 1) * lastPage.page_size
+					return offset < lastPage.total ? { offset, limit: lastPage.page_size } : undefined
+				}
+			}),
+			then: result => result?.pages?.flatMap(page => page.result) ?? [],
+		},
+	}[transactionProvider]}
+	{...$$restProps}
+	bind:result={transactions}
+	let:result={transactions}
+	let:pagination
+>
+	<svelte:fragment slot="header"
 		let:result={transactions}
+		let:status
 		let:pagination
 	>
-		<svelte:fragment slot="header"
-			let:result={transactions}
-			let:status
-			let:pagination
-		>
-			<slot name="header" {transactions} {status} {pagination} />
-		</svelte:fragment>
-		<slot {transactions} {pagination} />
-	</Loader>
-
-<!-- {:else if transactionProvider === TransactionProvider.Etherspot}
-	<Loader
-		layout="collapsible"
-		loadingIcon={transactionProviderIcons[transactionProvider]}
-		{loadingMessage}
-		{errorMessage}
-		fromQuery={createQuery({
-			queryKey: ['Transactions', {
-				transactionProvider,
-				chainID: network.chainId,
-				address,
-				quoteCurrency,
-			}],
-			queryFn: async () => (
-				await getTransactionsEtherspot({network, address})
-			)
-		})}
-		{...$$restProps}
-		bind:result={transactions}
-		let:result={transactions}
-		let:pagination
-	>
-		<svelte:fragment slot="header"
-			let:result={transactions}
-			let:status
-			let:pagination
-		>
-			<slot name="header" {transactions} {status} {pagination} />
-		</svelte:fragment>
-		<slot {transactions} {pagination} />
-	</Loader> -->
-
-{:else if transactionProvider === TransactionProvider.Moralis}
-	<Loader
-		layout="collapsible"
-		loadingIcon={transactionProviderIcons[transactionProvider]}
-		loadingIconName={transactionProvider}
-		{loadingMessage}
-		{errorMessage}
-		fromInfiniteQuery={createInfiniteQuery({
-			queryKey: ['Transactions', {
-				transactionProvider,
-				chainID: network.chainId,
-				address,
-				quoteCurrency,
-			}],
-			queryFn: async ({ pageParam }) => (
-				await MoralisWeb3Api.address.getTransactions({
-					chain: chainCodeFromNetwork(network),
-					from_block: 0,
-					// to_block: ,
-					offset: pageParam?.offset ?? 0,
-					limit: pageParam?.limit ?? 100,
-					address
-				})
-			),
-			// queryFn: async ({ pageParam: offset = 0 }) => {
-			// 	const {result: transactions, total, page_size, page} = await MoralisWeb3Api.address.getTransactions({
-			// 		chain: chainCodeFromNetwork(network),
-			// 		from_block: 0,
-			// 		// to_block: ,
-			// 		offset: offset ?? 0,
-			// 		limit: 100,
-			// 		address
-			// 	})
-
-			// 	// const logs = (await MoralisWeb3Api.address.getLogsByAddress({
-			// 	// 	chain: chainCodeFromNetwork(network),
-			// 	// 	from_block: 0,
-			// 	// 	address: transactions[0].hash
-			// 	// }))
-
-			// 	// console.log(transactions[0], logs)
-
-			// 	return (
-			// 		includeLogs
-			// 			? await Promise.all(transactions.map(transaction => (
-			// 				MoralisWeb3Api.transaction.getTransaction({
-			// 					chain: chainCodeFromNetwork(network),
-			// 					transactionHash: transaction.hash
-			// 				})
-			// 			)))
-			// 			: transactions
-			// 	).reverse()
-			// },
-			getPreviousPageParam: (firstPage, allPages) => {
-				const offset = (firstPage.page - 1) * firstPage.page_size
-				return offset > 0 ? { offset, limit: firstPage.page_size } : undefined
-			},
-			getNextPageParam: (lastPage, allPages) => {
-				const offset = (lastPage.page + 1) * lastPage.page_size
-				return offset < lastPage.total ? { offset, limit: lastPage.page_size } : undefined
-			}
-		})}
-		then={result => result?.pages?.flatMap(page => page.result) ?? []}
-		{...$$restProps}
-		bind:result={transactions}
-		let:result={transactions}
-		let:pagination
-	>
-		<svelte:fragment slot="header"
-			let:result={transactions}
-			let:status
-			let:pagination
-		>
-			<slot name="header" {transactions} {status} {pagination} />
-		</svelte:fragment>
-		<slot {transactions} {pagination} />
-	</Loader>
-{/if}
+		<slot name="header" {transactions} {status} {pagination} />
+	</svelte:fragment>
+	<slot {transactions} {pagination} />
+</Loader>
