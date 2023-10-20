6d469f71d08895029e99f579ae7bc942cfc2d0d8
Darryl Yeo <dev@darryl-yeo.com>
2023-10-20 14:23:55 -0700

    isEvmAddress: add type predicate

--- src/utils/isEvmAddress.ts
+++ src/utils/isEvmAddress.ts
@@ -1,2 +1,4 @@
-export const isEvmAddress = (string: string) =>
+import type { Ethereum } from '../data/networks/types'
+
+export const isEvmAddress = (string: string): string is Ethereum.Address =>
 	/^0x[0-9a-f]{40}$/i.test(string)
