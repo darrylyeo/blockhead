c680a9390bfd107fd7ee7046a0440e41e68cf9b9
Darryl Yeo <dev@darryl-yeo.com>
2023-11-10 14:18:25 -0800

    cardStyle: check for undefined

--- src/utils/card-background.ts
+++ src/utils/card-background.ts
@@ -1,8 +1,12 @@
-export const makeCardGradient = (colors = []) =>
+export const makeCardGradient = (colors: string[] = []) => (
 	`linear-gradient(155deg, ${colors.length == 1 ? [`${colors[0]}20`, `${colors[0]}55`] : colors.map(color => `${color}50`)}`
+)
 
-export const cardStyle = (colors: string[]) =>
-	colors ? `
-		--card-background-image: ${makeCardGradient(colors)});
-		--primary-color: ${colors[colors.length / 2 | 0] ?? 'inherit'}
-	` : ''
+export const cardStyle = (colors: string[] | undefined) => (
+	colors?.length
+		? `
+			--card-background-image: ${makeCardGradient(colors)});
+			--primary-color: ${colors[colors.length / 2 | 0] ?? 'inherit'}
+		`
+		: ''
+)
