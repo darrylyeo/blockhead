7052ad00ae6b2f32e6003e9d2b6a37518fd7a061
Darryl Yeo <dev@darryl-yeo.com>
2023-11-11 21:29:21 -0800

    Apps: add Farcaster

--- src/data/web3Apps.ts
+++ src/data/web3Apps.ts
@@ -9,6 +9,7 @@ import type { DeepReadonly } from '../utils/DeepReadonly'
 import {
 	CeramicIcon,
 	DiscoIcon,
+	FarcasterIcon,
 	IpfsIcon,
 	LensIcon,
 	NounsIcon,
@@ -2842,6 +2843,52 @@ export const web3Apps = [
 				}
 			}
 		]
+	},
+	{
+		name: 'Farcaster',
+		slug: 'farcaster',
+		links: ['https://www.farcaster.xyz'],
+		colors: ['#8a63d2'],
+		icon: FarcasterIcon,
+		views: [
+			{
+				chainId: 10,
+				contracts: [
+					{
+						name: 'IdRegistry',
+						address: '0x00000000fc6c5f01fc30151999387bb99a9f489b',
+					},
+					{
+						name: 'StorageRegistry',
+						address: '0x00000000fcce7f938e7ae6d3c335bd6a1a7c593d',
+					},
+					{
+						name: 'KeyRegistry',
+						address: '0x00000000fc1237824fb747abde0ff18990e59b7e',
+					},
+					{
+						name: 'IdGateway',
+						address: '0x00000000fc25870c6ed6b6c7e41fb078b7656f69',
+					},
+					{
+						name: 'KeyGateway',
+						address: '0x00000000fc56947c7e7183f8ca4b62398caadf0b',
+					},
+					{
+						name: 'Bundler',
+						address: '0x00000000fc04c910a0b5fea33b03e0447ad0b0aa',
+					},
+					{
+						name: 'SignedKeyRequestValidator',
+						address: '0x00000000fc700472606ed4fa22623acf62c60553',
+					},
+					{
+						name: 'RecoveryProxy',
+						address: '0x00000000fcb080a4d6c39a9354da9eb9bc104cd7',
+					},
+				],		
+			},
+		],
 	},
 	{
 		name: 'Fei Protocol',
