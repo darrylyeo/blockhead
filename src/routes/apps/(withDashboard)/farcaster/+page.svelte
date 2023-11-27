beb08d42f567d67a2bc842609ba10e1e72af5616
Darryl Yeo <dev@darryl-yeo.com>
2023-11-27 11:15:11 -0800

    Farcaster: show Trending feed on front page

---
+++ src/routes/apps/(withDashboard)/farcaster/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	// Types
+	import type { FarcasterProvider } from '../../../../data/farcasterProviders'
+
+
+	// Context
+	import { farcasterUserId, farcasterUserName, farcasterCastId } from '../../_appsParams'
+	import { preferences } from '../../../../state/preferences'
+
+
+	// Internal state
+	let farcasterProvider: FarcasterProvider
+	$: farcasterProvider = $preferences.farcasterProvider
+
+
+	// Components
+	import FarcasterCasts from '../../../../components/FarcasterCasts.svelte'
+	import FarcasterCastsLoader from '../../../../components/FarcasterCastsLoader.svelte'
+</script>
+
+
+{#if $farcasterUserId || $farcasterUserName}
+
+
+{:else if $farcasterCastId}
+
+
+{:else}
+	<FarcasterCastsLoader
+		{farcasterProvider}
+		let:casts
+	>
+		{#if casts}
+			<FarcasterCasts
+				title="Trending"
+				{casts}
+			/>
+		{/if}
+	</FarcasterCastsLoader>
+{/if}
