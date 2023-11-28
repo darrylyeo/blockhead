ce9b4a06e570511987595be5211faa8375a32322
Darryl Yeo <dev@darryl-yeo.com>
2023-11-27 17:14:44 -0800

    Farcaster: model user type

--- src/api/farcaster/index.ts
+++ src/api/farcaster/index.ts
@@ -1,2 +1,36 @@
 export type FarcasterUserId = number
 export type FarcasterUserName = string
+
+export type FarcasterUser = {
+	id: FarcasterUserId;
+	name: FarcasterUserName;
+	displayName: string;
+
+	avatar?: {
+		url: string;
+	};
+
+	bio: {
+		text: string;
+		mentions: string[];
+	};
+
+	isActive: boolean;
+
+	verifications: string[];
+
+	summary: {
+		followerCount: number;
+		followingCount: number;
+	};
+
+	context?: Record<
+		FarcasterUserId,
+		{
+			following: boolean;
+			followedBy: boolean;
+			liked?: boolean;
+			recasted?: boolean;
+		}
+	>;
+}
