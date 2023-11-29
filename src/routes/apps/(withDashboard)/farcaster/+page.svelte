08ccd1896bcc6dd04b65b4c9fb154bd2be338ebb
Darryl Yeo <dev@darryl-yeo.com>
2023-11-28 18:21:24 -0800

    Farcaster: implement routes showing individual Farcaster profile feeds by user ID or username

--- src/routes/apps/(withDashboard)/farcaster/+page.svelte
+++ src/routes/apps/(withDashboard)/farcaster/+page.svelte
@@ -18,11 +18,36 @@
 	import FarcasterCastsLoader from '../../../../components/FarcasterCastsLoader.svelte'
 	import FarcasterCastLoader from '../../../../components/FarcasterCastLoader.svelte'
 	import FarcasterCast from '../../../../components/FarcasterCast.svelte'
+	import FarcasterUserProfileLoader from '../../../../components/FarcasterUserProfileLoader.svelte'
 </script>
 
 
 {#if $farcasterUserId || $farcasterUserName}
-
+	<FarcasterUserProfileLoader
+		{farcasterProvider}
+		userId={$farcasterUserId || undefined}
+		userName={$farcasterUserName}
+		let:user
+	>
+		{#if user}
+			<section>
+				{#if user?.id}
+					<FarcasterCastsLoader
+						{farcasterProvider}
+						userId={user.id}
+						let:casts
+					>
+						{#if casts}
+							<FarcasterCasts
+								title={`${user?.name || user?.id} › Casts`}
+								{casts}
+							/>
+						{/if}
+					</FarcasterCastsLoader>
+				{/if}
+			</section>
+		{/if}
+	</FarcasterUserProfileLoader>
 
 {:else if $farcasterCastId}
 	<FarcasterCastLoader
