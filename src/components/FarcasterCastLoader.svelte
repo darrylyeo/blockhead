56ec81e0d50ba5bb5ad2aab65a83ab10301f321e
Darryl Yeo <dev@darryl-yeo.com>
2023-11-28 18:15:03 -0800

    <FarcasterCastLoader>: load an individual cast by cast ID (hash) or client-specific URL

---
+++ src/components/FarcasterCastLoader.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	// Constants/types
+	import { CastParamType, type Cast as CastNeynar } from '../api/neynar/v2'
+	import type { FarcasterCastId, FarcasterUserId } from '../api/farcaster/index'
+	import { FarcasterProvider, farcasterProviderIcons } from '../data/farcasterProviders'
+
+
+	// Context
+	import { env } from '../env'
+
+
+	// Inputs
+	export let farcasterProvider: FarcasterProvider = FarcasterProvider.Neynar
+	export let userId: FarcasterUserId | undefined
+	export let castId: FarcasterCastId | undefined
+	export let clientUrl: string | undefined
+	export let viewerUserId: FarcasterUserId | undefined
+
+
+	// Outputs
+	export let cast: CastNeynar | undefined
+
+	type SharedSlotProps = {
+		cast: typeof cast,
+	}
+
+	type $$Slots = {
+		default: SharedSlotProps,
+		header: SharedSlotProps,
+	}
+
+
+	// Functions
+	import { createQuery } from '@tanstack/svelte-query'
+
+
+	// Components
+	import Loader from './Loader.svelte'
+</script>
+
+
+<Loader
+	loadingMessage="Loading cast from {farcasterProvider}..."
+	loadingIcon={farcasterProviderIcons[farcasterProvider]}
+	errorMessage="Couldn't load cast from {farcasterProvider}."
+	{...{
+		[FarcasterProvider.Hub]: () => {},
+
+		[FarcasterProvider.Neynar]: () => ({
+			fromQuery: (
+				createQuery({
+					queryKey: ['FarcasterCast', {
+						farcasterProvider,
+						castId,
+					}],
+					queryFn: async () => {
+						const { cast } = await import('../api/neynar/v2')
+
+						return (
+							castId ?
+								await cast(
+									env.NEYNAR_API_KEY,
+									castId,
+									CastParamType.Hash,
+								)
+							: clientUrl ? 
+								await cast(
+									env.NEYNAR_API_KEY,
+									clientUrl,
+									CastParamType.Url,
+								)
+							:
+								undefined
+						)
+					},
+					select: result => (
+						result?.cast
+					),
+				})
+			),
+		}),
+	}[farcasterProvider]?.()}
+	{...$$restProps}
+	bind:result={cast}
+	let:result={cast}
+	debug
+>
+	<svelte:fragment slot="header"
+		let:result={cast}
+	>
+		<slot name="header"
+			{cast}
+		/>
+	</svelte:fragment>
+
+	<slot {cast} />
+</Loader>
