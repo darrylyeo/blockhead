a5899c368a3fd0170c709667651b38a3c6c8bb96
Darryl Yeo <dev@darryl-yeo.com>
2023-11-29 23:40:41 -0800

    Neynar: normalize user V2

--- src/api/neynar/index.ts
+++ src/api/neynar/index.ts
@@ -1,5 +1,6 @@
 import type { FarcasterUser, FarcasterUserId, FarcasterChannel } from '../farcaster'
 import { ActiveStatus, type User as UserV1 } from './v1'
+import type { User as UserV2 } from './v2'
 
 type Channel = {
 	name: string;
@@ -43,6 +44,42 @@ export const normalizeUserV1 = (
 				followedBy: user.viewerContext.followedBy,
 				liked: user.viewerContext.liked,
 				recasted: user.viewerContext.recasted,
+			},
+		}
+		: undefined,
+})
+
+export const normalizeUserV2 = (
+	user: UserV2,
+	viewerUserId?: FarcasterUserId,
+): FarcasterUser => ({
+	id: user.fid,
+	name: user.username,
+	displayName: user.display_name,
+
+	avatar: {
+		url: user.pfp_url,
+	},
+
+	bio: {
+		text: user.profile.bio.text,
+		mentions: user.profile.bio.mentioned_profiles,
+	},
+
+	isActive: user.active_status === ActiveStatus.Active,
+
+	verifications: user.verifications,
+
+	summary: {
+		followerCount: user.follower_count,
+		followingCount: user.following_count,
+	},
+
+	context: user.viewer_context && viewerUserId !== undefined
+		? {
+			[viewerUserId]: {
+				following: user.viewer_context.following,
+				followedBy: user.viewer_context.followed_by,
 			},
 		}
 		: undefined,
