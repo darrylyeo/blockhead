5961e5d4c28a2d7abb9ccd5458fcf4eb2283d9cf
Darryl Yeo <dev@darryl-yeo.com>
2023-12-21 16:35:40 -0800

    <UrlMetadataLoader>: load OpenGraph / JSON-LD data for a given URL

---
+++ src/components/UrlMetadataLoader.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	// Constants/types
+	import { UrlMetadataProvider, urlMetadataProviderIcons } from '../data/urlMetadataProvider'
+	import type { UrlMetadata } from '../api/mod'
+
+
+	// Context
+	import { preferences } from '../state/preferences'
+
+
+	// Inputs
+	export let urlMetadataProvider: UrlMetadataProvider
+	export let url: string
+
+	// (Computed)
+	$: urlMetadataProvider = $$props.urlMetadataProvider || $preferences.urlMetadataProvider
+
+
+	// Outputs
+	export let urlMetadata: UrlMetadata | undefined
+
+	type SharedSlotProps = {
+		urlMetadata: typeof urlMetadata,
+	}
+
+	type $$Slots = {
+		default: SharedSlotProps,
+		header: SharedSlotProps,
+	}
+
+
+	// Functions
+	import { createQuery } from '@tanstack/svelte-query'
+	import { getMetadataForUrls } from '../api/mod'
+
+
+	// Components
+	import Loader from './Loader.svelte'
+</script>
+
+
+<Loader
+	loadingMessage="Loading metadata from {urlMetadataProvider}..."
+	loadingIcon={urlMetadataProviderIcons[urlMetadataProvider]}
+	errorMessage="Couldn't load metadata from {urlMetadataProvider}."
+	{...{
+		[UrlMetadataProvider.ModProtocol]: () => ({
+			fromQuery: (
+				createQuery({
+					queryKey: ['UrlMetadata', {
+						urlMetadataProvider,
+						url,
+					}],
+					queryFn: async () => (
+						await getMetadataForUrls([
+							url,
+						])
+					),
+					select: result => (
+						Object.values(result)[0]
+					),
+					staleTime: 10 * 1000,
+				})
+			),
+		}),
+	}[urlMetadataProvider]?.()}
+	{...$$restProps}
+	bind:result={urlMetadata}
+	let:result={urlMetadata}
+>
+	<svelte:fragment slot="header"
+		let:result={urlMetadata}
+	>
+		<slot name="header"
+			{urlMetadata}
+		/>
+	</svelte:fragment>
+
+	<slot {urlMetadata} />
+</Loader>
