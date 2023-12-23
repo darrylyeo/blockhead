23efee1e052604db2f13ba04641ba4441aeedf00
Darryl Yeo <dev@darryl-yeo.com>
2023-12-22 18:16:18 -0800

    <UrlMetadata>: display URL preview

---
+++ src/components/UrlMetadata.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+	// Inputs
+	export let url: string
+
+
+	// Components
+	import UrlMetadataLoader from './UrlMetadataLoader.svelte'
+	import Collapsible from './Collapsible.svelte'
+
+
+	// Functions
+	const formatContent = (text: string) => (
+		text
+			.split('\n\n')
+			.map((line, i) => (
+				`<p>${line.replaceAll('\n', '<br />')}</p>`
+			))
+			.join('')
+	)
+
+
+	// Styles
+	import { matchesMediaQuery } from '../utils/matchesMediaQuery'
+	const matchesLayoutBreakpoint = matchesMediaQuery('(max-width: 30rem)')
+</script>
+
+
+<UrlMetadataLoader
+	{url}
+	let:urlMetadata
+>
+	{#if urlMetadata}
+		<Collapsible
+			containerClass="card"
+			class="column"
+			showTriggerText={false}
+			isOpen
+			canToggle={false}
+		>
+			<svelte:fragment slot="title">
+				<a
+					href={url}
+					target="_blank"
+					class="row"
+				>
+					{#if urlMetadata.logo}
+						<img
+							src={urlMetadata.logo.url}
+							width="24"
+							height="24"
+						/>
+					{/if}
+
+					<h4>
+						{urlMetadata.title}
+					</h4>
+				</a>
+			</svelte:fragment>
+
+			<svelte:fragment slot="header-right">
+				<span class="card-annotation">
+					{#if urlMetadata.publisher}
+						<address>{urlMetadata.publisher}</address>
+					{/if}
+				</span>
+			</svelte:fragment>
+
+			{#if urlMetadata.description || urlMetadata.image}
+				<div class="content-and-images bar align-top" class:wrap={$matchesLayoutBreakpoint}>
+					{#if urlMetadata.description}
+						<div class="content column">
+							{@html formatContent(urlMetadata.description)}
+						</div>
+					{/if}
+
+					{#if urlMetadata.image}
+						<div class="image-embeds row">
+							<img
+								src={urlMetadata.image.url}
+								style={`
+									aspect-ratio: ${urlMetadata.image.width} / ${urlMetadata.image.height};
+								`}
+								alt={urlMetadata.alt}
+							/>
+						</div>
+					{/if}
+				</div>
+			{/if}
+		</Collapsible>
+	{/if}
+</UrlMetadataLoader>
+
+
+<style>
+	address {
+		font-style: normal;
+		display: inline;
+	}
+
+	.content-and-images > :global(:first-child:not(.image-embeds)) {
+		min-width: min(14rem, 100%);
+	}
+
+	.image-embeds {
+		justify-content: center;
+		margin-inline-end: auto;
+	}
+
+	.image-embeds img {
+		border-radius: 4px;
+		object-fit: contain;
+		max-width: 100%;
+		height: 100%;
+		max-height: 8rem;
+	}
+</style>
