457e07501c4fe31e5c2e7c3a13d8950c298d4271
Darryl Yeo <dev@darryl-yeo.com>
2024-01-21 19:55:46 -0800

    ESLint: allow TypeScript namespaces

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,28 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
+		{
+			files: ['*.ts'],
+			rules: {
+				'@typescript-eslint/no-namespace': 'off',
+			},
+		},
+	],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
-};
+		node: true,
+	},
+}
