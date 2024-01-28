fe3a1e0593bb74c8c409ee2ea64eed6eb4dcf16f
Darryl Yeo <dev@darryl-yeo.com>
2024-01-28 00:21:47 -0800

    Explorer: generate OpenGraph image for address page

---
+++ src/routes/explorer/(explorer)/[networkSlug=isNetworkSlug]/address/[address=isEvmAddress]/+server.ts
@@ -0,0 +1,105 @@
+// Constants
+import { getNetworkColor, networksBySlug } from '$/data/networks'
+import { NetworkProvider } from '$/data/networkProviders/types'
+
+
+// Functions
+import { getViemPublicClient } from '$/data/networkProviders'
+
+import { Etherscan } from '$/api/etherscan'
+import { normalizeContractSource as normalizeContractSourceEtherscan } from '$/api/etherscan/normalize'
+
+
+// OpenGraph
+import { renderPreviewSvg } from '$/opengraph/renderPreviewSvg'
+import { svgToImageResponse } from '$/opengraph/svgToImageResponse'
+
+
+// Request handlers
+import type { RequestHandler } from './$types'
+
+import type { OpenGraphImageGeneratorParams } from '$/opengraph/imageGenerator'
+
+/**
+ * OpenGraph Image generator
+ */
+export const GET: RequestHandler = async ({
+	request,
+	url,
+	params,
+}) => {
+	// Context
+	const { networkSlug, address } = params
+
+	const {
+		width,
+		height,
+	} = Object.fromEntries(url.searchParams.entries()) as unknown as OpenGraphImageGeneratorParams
+
+
+	// Internal state
+	const network = networksBySlug[networkSlug]
+
+	const publicClient = getViemPublicClient({
+		network: networksBySlug['ethereum'],
+		networkProvider: NetworkProvider.Alchemy,
+	})
+
+	const ensName = publicClient && (
+		await publicClient.getEnsName({
+			address,
+		})
+			.catch(() => undefined)
+	) || undefined
+	
+	const bytecode = publicClient && address && (
+		await publicClient.getBytecode({ address })
+			.then(contractBytecode => contractBytecode === undefined ? undefined : contractBytecode)
+	)
+
+	const contractMetadata = bytecode && await Etherscan.Contracts.getSource({
+		chainId: network.chainId,
+		contractAddress: address,
+	})
+		.then(normalizeContractSourceEtherscan)
+
+	const addressType = bytecode && contractMetadata ? 'Contract' : 'Account'
+
+	const contractName = contractMetadata && Object.values(contractMetadata.contractMetadata.settings.compilationTarget)?.[0]
+
+	const sourcePaths = contractMetadata ? Object.keys(contractMetadata.contractMetadata.sources) : undefined
+
+
+	// Render
+	const svg = renderPreviewSvg({
+		width,
+		height,
+		...(
+			contractName && address && ensName ?
+				{
+					title: `${contractName} (${address})`,
+					subtitle: ensName,
+				}
+			: contractName ?
+				{
+					title: contractName,
+					subtitle: address,
+				}
+			:
+				{
+					title: address,
+				}
+		),
+		annotation: `${network.name} ${addressType}`,
+		body: sourcePaths?.join('\n'),
+		url,
+		primaryColor: getNetworkColor(network) ?? getNetworkColor(networksBySlug['ethereum']),
+	})
+
+
+	// Response
+	return svgToImageResponse({
+		headers: request.headers,
+		svg,
+	})
+}
