96d40c6d02770defcd418d594f23327629fae6e5
Darryl Yeo <dev@darryl-yeo.com>
2024-01-29 22:20:13 -0800

    <Dialog>: show content in a top-layer popover

---
+++ src/components/Dialog.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	// Inputs
+	export let id: string = crypto.randomUUID()
+</script>
+
+
+<button
+	popovertarget={id}
+	class="small"
+>
+	<slot />
+</button>
+
+<div
+	popover
+	{id}
+>
+	<slot name="content" />
+</div>
+
+
+<style>
+	[popover] {
+		border: none;
+		background: none;
+		width: 40rem;
+		max-width: 100%;
+		margin: auto;
+		opacity: 0;
+		transition: 0.4s var(--ease-out-expo);
+	}
+	[popover]:popover-open {
+		opacity: 1;
+	}
+
+	::backdrop {
+		background-color: rgba(0, 0, 0, 0.25);
+		animation: Blur 0.2s forwards;
+		animation-timing-function: var(--ease-out-expo);
+	}
+	@keyframes Blur {
+		to {
+			backdrop-filter: blur(16px);
+		}
+	}
+</style>
