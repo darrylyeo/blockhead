0454eb385714ee0b05ae62c2fb8c78e5f22835dc
Darryl Yeo <dev@darryl-yeo.com>
2024-01-29 16:53:43 -0800

    OpenGraph: resolve OpenGraph metadata for image generator

--- src/opengraph/imageGenerator.ts
+++ src/opengraph/imageGenerator.ts
@@ -1,5 +1,37 @@
+// Types
+import type { OpenGraph } from 'svelte-meta-tags'
+
 export type OpenGraphImageGeneratorParams = {
 	timestamp: number,
 	width: number,
 	height: number,
+}
+
+
+// Functions
+export const openGraphImageGeneratorMeta = ({
+	pageUrl,
+	width = 1728,
+	height = 1080,
+	altText,
+}: {
+	pageUrl: URL,
+	width?: number,
+	height?: number
+	altText?: string,
+}) => {
+	const params: OpenGraphImageGeneratorParams = {
+		timestamp: Date.now(),
+		width,
+		height,
+	}
+
+	const imageGeneratorUrl = `${pageUrl.protocol}//${pageUrl.host}${pageUrl.pathname}?${new URLSearchParams(params)}`
+
+	return {
+		url: imageGeneratorUrl,
+		width,
+		height,
+		alt: altText,
+	} as NonNullable<OpenGraph['images']>[number]
 }
