9bbf563b7b8d58a13934fd3c8300a0103caff026
Darryl Yeo <dev@darryl-yeo.com>
2024-02-01 02:17:29 -0800

    OpenGraph Image Generator: fail silently if read() is unavailable for current server adapter

--- src/opengraph/fonts.ts
+++ src/opengraph/fonts.ts
@@ -1,4 +1,12 @@
-import { read } from '$app/server'
+import { read as _read } from '$app/server'
+const read = (...args: Parameters<typeof _read>) => {
+	try {
+		return _read(...args)
+	}catch (error) {
+		console.error(error)
+	}
+}
+
 
 import FiraCode400 from '@fontsource/fira-code/files/fira-code-latin-400-normal.woff'
 import FiraCode500 from '@fontsource/fira-code/files/fira-code-latin-500-normal.woff'
@@ -11,44 +19,45 @@ import Ubuntu700 from '@fontsource/ubuntu/files/ubuntu-latin-700-normal.woff'
 export const fonts = [
 	{
 		name: 'Fira Code',
-		data: await read(FiraCode400).arrayBuffer(),
+		data: await read(FiraCode400)?.arrayBuffer(),
 		style: 'normal',
 		weight: '400',
 	},
 	{
 		name: 'Fira Code',
-		data: await read(FiraCode500).arrayBuffer(),
+		data: await read(FiraCode500)?.arrayBuffer(),
 		style: 'normal',
 		weight: '500',
 	},
 	{
 		name: 'Fira Code',
-		data: await read(FiraCode600).arrayBuffer(),
+		data: await read(FiraCode600)?.arrayBuffer(),
 		style: 'normal',
 		weight: '600',
 	},
 	{
 		name: 'Fira Code',
-		data: await read(FiraCode700).arrayBuffer(),
+		data: await read(FiraCode700)?.arrayBuffer(),
 		style: 'normal',
 		weight: '700',
 	},
 	{
 		name: 'Ubuntu',
-		data: await read(Ubuntu400).arrayBuffer(),
+		data: await read(Ubuntu400)?.arrayBuffer(),
 		style: 'normal',
 		weight: '400',
 	},
 	{
 		name: 'Ubuntu',
-		data: await read(Ubuntu500).arrayBuffer(),
+		data: await read(Ubuntu500)?.arrayBuffer(),
 		style: 'normal',
 		weight: '500',
 	},
 	{
 		name: 'Ubuntu',
-		data: await read(Ubuntu700).arrayBuffer(),
+		data: await read(Ubuntu700)?.arrayBuffer(),
 		style: 'normal',
 		weight: '700',
 	},
 ]
+	.filter(font => font.data)
