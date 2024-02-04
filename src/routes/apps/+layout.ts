1a943d9f8e30193740f76e2a32fbdd561c0ad0e8
Darryl Yeo <dev@darryl-yeo.com>
2024-02-03 18:52:42 -0800

    Apps: update <title> to include network name

--- src/routes/apps/+layout.ts
+++ src/routes/apps/+layout.ts
@@ -1,3 +1,11 @@
+// Constants
+import { networksBySlug } from '$/data/networks'
+
+
+// Functions
+import { isTruthy } from '$/utils/isTruthy'
+
+
 // Context
 import type { MetaTagsProps } from 'svelte-meta-tags'
 
@@ -5,12 +13,29 @@ import type { LayoutLoad } from './$types'
 
 export const load: LayoutLoad = async ({
 	parent,
+	params,
 }) => {
 	const data = await parent()
 
 	const metaTags: MetaTagsProps = {
 		...data.metaTags,
-		title: `Apps | ${data.metaTags.title}`,
+		title: (
+			[
+				[
+					params.networkSlug && networksBySlug[params.networkSlug].name,
+					'Apps'
+				],
+				[
+					data.metaTags.title
+				],
+			]
+				.map((parts) => (
+					parts
+						.filter(isTruthy)
+						.join(' ')
+				))
+				.join(' | ')
+		),
 		description: 'Browse DeFi protocols and web3 apps on Blockhead.',
 	}
 
