4f8c3c69dc1c0bf0e217af5a4f0ed9e0882b77e7
Darryl Yeo <dev@darryl-yeo.com>
2024-02-05 10:32:26 -0800

    Vercel: add SvelteKit adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,13 @@ import preprocess from 'svelte-preprocess'
 import adapterAuto from '@sveltejs/adapter-auto'
 import adapterStatic from '@sveltejs/adapter-static'
 import adapterNetlify from '@sveltejs/adapter-netlify'
+import adapterVercel from '@sveltejs/adapter-vercel'
 
 const isStatic = !!process.env.SVELTE_BUILD_STATIC
 const isNetlify = !!process.env.NETLIFY
+const isVercel = !!process.env.VERCEL
+
+console.log({isStatic, isNetlify, isVercel})
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,6 +26,8 @@ const config = {
 				})
 			: isNetlify ?
 				adapterNetlify()
+			: isVercel ?
+				adapterVercel()
 			:
 				adapterAuto(),
 
