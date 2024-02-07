39f0288983b1cf56f21e20a6f1b301f215686f18
Darryl Yeo <dev@darryl-yeo.com>
2024-02-07 14:35:38 -0800

    Explorer › Address: display contract code in OpenGraph image

--- src/routes/explorer/(explorer)/[networkSlug=isNetworkSlug]/address/[address=isEvmAddress]/contract/page.opengraph.svelte
+++ src/routes/explorer/(explorer)/[networkSlug=isNetworkSlug]/address/[address=isEvmAddress]/contract/page.opengraph.svelte
@@ -11,3 +11,21 @@
 		data: Awaited<ReturnType<typeof import('./page.opengraph').load>>,
 	})
 </script>
+
+
+<div class="column">
+	{#each sourcePaths?.slice(1) ?? [] as sourcePath}
+		{@const source = contractMetadata && contractMetadata.contractMetadata.sources[sourcePath]}
+		<pre>
+			{source && source.content || ''}
+		</pre>
+	{/each}
+</div>
+
+
+<style>
+	pre {
+		font-size: 0.66em;
+		opacity: 0.5;
+	}
+</style>
