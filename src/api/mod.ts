fd627abc3f0cb80a7e1bb7e8cb352b23e86f8de6
Darryl Yeo <dev@darryl-yeo.com>
2024-02-10 21:09:08 -0800

    Mod Protocol: update to include custom OpenGraph data for Farcaster Frames

--- src/api/mod.ts
+++ src/api/mod.ts
@@ -37,6 +37,25 @@ export type UrlMetadata = {
 			creator?: Ethereum.Address;
 		};
 	};
+	customOpenGraph?: {
+		'fc:frame'?: string;
+		'fc:frame:image'?: string;
+		'fc:frame:image:aspect_ratio'?: string;
+		'fc:frame:input:text'?: string;
+		'fc:frame:button:1'?: string;
+		'fc:frame:button:1:action'?: string;
+		'fc:frame:button:1:target'?: string;
+		'fc:frame:button:2'?: string;
+		'fc:frame:button:2:action'?: string;
+		'fc:frame:button:2:target'?: string;
+		'fc:frame:button:3'?: string;
+		'fc:frame:button:3:action'?: string;
+		'fc:frame:button:3:target'?: string;
+		'fc:frame:button:4'?: string;
+		'fc:frame:button:4:action'?: string;
+		'fc:frame:button:4:target'?: string;
+		'fc:frame:post_url'?: string;
+	};
 }
 
 
