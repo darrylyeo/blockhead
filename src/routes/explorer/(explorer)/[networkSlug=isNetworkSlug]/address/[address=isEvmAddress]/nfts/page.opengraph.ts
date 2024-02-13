8f7ef57ea40e85d3eff9d552597b29d6ab32198c
Darryl Yeo <dev@darryl-yeo.com>
2024-02-13 00:19:53 -0800

    Explorer › Address › NFTs: query NFTs from Airstack

--- src/routes/explorer/(explorer)/[networkSlug=isNetworkSlug]/address/[address=isEvmAddress]/nfts/page.opengraph.ts
+++ src/routes/explorer/(explorer)/[networkSlug=isNetworkSlug]/address/[address=isEvmAddress]/nfts/page.opengraph.ts
@@ -1,15 +1,45 @@
-// Types
-
-
 // Functions
+import { getNftsByAddress as getNftsAirstack } from '$/api/airstack/index'
+import { normalizeNftContracts as normalizeNftContractsAirstack } from '$/api/airstack/normalize'
 
 
 // OpenGraph load
 export const load = async ({
-	
+	layoutData: {
+		address,
+		network,
+		quoteCurrency,
+	},
 }: {
-
+	layoutData: Awaited<ReturnType<typeof import('../layout.opengraph').load>>
 }) => {
+	const result = (
+		await getNftsAirstack({
+			network,
+			address,
+			cursor: '',
+		})
+			.catch(() => undefined)
+	)
+
+	const hasMore = result && Boolean(result.TokenBalances.pageInfo.nextCursor)
+
+	const nftContractsWithBalances = result && normalizeNftContractsAirstack(
+		result
+			.TokenBalances.TokenBalance		
+	)
+
+	const summary = nftContractsWithBalances && {
+		quoteTotal: nftContractsWithBalances.reduce((sum, item) => sum + (item.conversion?.value ?? 0) * (item.nftsCount ?? item.nfts?.length ?? 0), 0),
+		quoteCurrency,
+		nftContractsCount: nftContractsWithBalances.length,
+		nftsCount: nftContractsWithBalances.reduce((sum, item) => sum + (item.nfts?.length ?? 0), 0)
+	}
+
 	return {
+		network,
+		nftContractsWithBalances,
+		hasMore,
+		summary,
 	}
 }
