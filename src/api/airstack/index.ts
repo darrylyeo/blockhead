c59340987cb42b4859d218bf37d864bd8834cc6f
Darryl Yeo <dev@darryl-yeo.com>
2024-02-24 12:54:29 -0800

    Airstack: update queries/normalization functions to use TypeScript types generated by gql.tada

--- src/api/airstack/index.ts
+++ src/api/airstack/index.ts
@@ -1,26 +1,4 @@
-import { Client, cacheExchange, fetchExchange, gql } from '@urql/svelte'
-
-import { env } from '$/env'
-
-
-let client: Client
-
-export const getClient = () => client ||= (
-	new Client({
-		url: 'https://api.airstack.xyz/gql',
-		exchanges: [
-			// cacheExchange,
-			fetchExchange,
-		],
-		fetchOptions: () => ({
-			headers: {
-				authorization: env.AIRSTACK_API_KEY,
-			},
-		})
-	})
-)
-
-
+// Types/constants
 import type { Ethereum } from '$/data/networks/types'
 
 export const airstackNetworkNames = {
@@ -31,6 +9,32 @@ export const airstackNetworkNames = {
 } as Record<Ethereum.ChainID, string>
 
 
+// gql.tada
+import { initGraphQLTada } from 'gql.tada'
+import type { introspection } from './graphql-env'
+const graphql = initGraphQLTada<{
+	introspection: introspection,
+}>()
+
+
+// urql
+import { Client, fetchExchange } from '@urql/svelte'
+import { env } from '$/env'
+
+const client = new Client({
+	url: 'https://api.airstack.xyz/gql',
+	exchanges: [
+		fetchExchange,
+	],
+	fetchOptions: () => ({
+		headers: {
+			authorization: env.AIRSTACK_API_KEY,
+		},
+	})
+})
+
+
+// Queries
 export const getTokenBalances = async ({
 	address,
 	network,
@@ -45,87 +49,95 @@ export const getTokenBalances = async ({
 	if(!(network.chainId in airstackNetworkNames))
 		throw new Error(`Airstack doesn't yet support ${network.name}.`)
 
-	return await getClient().query(gql`
-		query TokenBalances(
-			$address: Identity!, 
-			$blockchain: TokenBlockchain!, 
-			$limit: Int!, 
-			$cursor: String!
-		) {
-			TokenBalances(
-				input: {
-					filter: {
-						owner: {_in: [$address]},
-						tokenType: { _in: [ERC20] }
-					},
-					blockchain: $blockchain,
-					limit: $limit,
-					cursor: $cursor
-				}
-			) {
-				TokenBalance {
-					tokenAddress
-					amount
-					tokenType
-					blockchain
-					chainId
-					formattedAmount
-					id
-					lastUpdatedBlock
-					lastUpdatedTimestamp
-					token {
-						address
-						baseURI
-						chainId
-						blockchain
-						contractMetaData {
-							description
-							externalLink
-							feeRecipient
-							image
-							name
-							sellerFeeBasisPoints
+	return await client
+		.query(
+			graphql(`
+				query TokenBalances(
+					$address: Identity!, 
+					$blockchain: TokenBlockchain!, 
+					$limit: Int!, 
+					$cursor: String!
+				) {
+					TokenBalances(
+						input: {
+							filter: {
+								owner: {
+									_in: [$address]
+								}
+								tokenType: {
+									_in: [ERC20]
+								}
+							}
+							blockchain: $blockchain
+							limit: $limit
+							cursor: $cursor
 						}
-						contractMetaDataURI
-						decimals
-						id
-						lastTransferBlock
-						lastTransferHash
-						lastTransferTimestamp
-						logo {
-							external
-							large
-							medium
-							original
-							small
+					) {
+						TokenBalance {
+							tokenAddress
+							amount
+							tokenType
+							blockchain
+							chainId
+							formattedAmount
+							id
+							lastUpdatedBlock
+							lastUpdatedTimestamp
+							token {
+								address
+								baseURI
+								chainId
+								blockchain
+								contractMetaData {
+									description
+									externalLink
+									feeRecipient
+									image
+									name
+									sellerFeeBasisPoints
+								}
+								contractMetaDataURI
+								decimals
+								id
+								lastTransferBlock
+								lastTransferHash
+								lastTransferTimestamp
+								logo {
+									external
+									large
+									medium
+									original
+									small
+								}
+								name
+								projectDetails {
+									collectionName
+									description
+									discordUrl
+									externalUrl
+									twitterUrl
+								}
+								rawContractMetaData
+								symbol
+								tokenTraits
+								totalSupply
+								type
+							}
 						}
-						name
-						projectDetails {
-							collectionName
-							description
-							discordUrl
-							externalUrl
-							twitterUrl
+						pageInfo {
+							nextCursor
+							prevCursor
 						}
-						rawContractMetaData
-						symbol
-						tokenTraits
-						totalSupply
-						type
 					}
 				}
-				pageInfo {
-					nextCursor
-					prevCursor
-				}
-			}
-		}
-	`, {
-		address,
-		blockchain: airstackNetworkNames[network.chainId],
-		limit,
-		cursor,
-	})
+			`),
+			{
+				address,
+				blockchain: airstackNetworkNames[network.chainId],
+				limit,
+				cursor,
+			},
+		)
 		.toPromise()
 		.then(result => {
 			if(result.error)
@@ -149,138 +161,145 @@ export const getNftsByAddress = async ({
 	if(!(network.chainId in airstackNetworkNames))
 		throw new Error(`Airstack doesn't yet support ${network.name}.`)
 
-	return await getClient().query(gql`
-		query NftBalances(
-			$address: Identity!, 
-			$blockchain: TokenBlockchain!, 
-			$limit: Int!, 
-			$cursor: String!
-		) {
-			TokenBalances(
-				input: {
-					filter: {
-						owner: {_in: [$address]},
-						tokenType: { _in: [ERC721, ERC1155] }
-					},
-					blockchain: $blockchain,
-					limit: $limit,
-					cursor: $cursor
-				}
-			) {
-				TokenBalance {
-					tokenAddress
-					tokenNfts {
-						id
-						address
-						tokenId
-						type
-						rawMetaData
-						chainId
-						contentType
-						blockchain
-						# contentValue {
-						# 	audio
-						# 	animation_url {
-						# 		original
-						# 	}
-						# 	image {
-						# 		extraSmall
-						# 		large
-						# 		medium
-						# 		original
-						# 		small
-						# 	}
-						# 	video
-						# }
-						lastTransferBlock
-						lastTransferHash
-						lastTransferTimestamp
-						metaData {
-							animationUrl
-							attributes {
-								displayType
-								maxValue
-								trait_type
-								value
+	return await client
+		.query(
+			graphql(`
+				query NftBalances(
+					$address: Identity!
+					$blockchain: TokenBlockchain!
+					$limit: Int!
+					$cursor: String!
+				) {
+					TokenBalances(
+						input: {
+							filter: {
+								owner: {
+									_in: [$address]
+								}
+								tokenType: {
+									_in: [ERC721, ERC1155]
+								}
+							},
+							blockchain: $blockchain,
+							limit: $limit,
+							cursor: $cursor
+						}
+					) {
+						TokenBalance {
+							tokenAddress
+							tokenNfts {
+								id
+								address
+								tokenId
+								type
+								rawMetaData
+								chainId
+								contentType
+								blockchain
+								# contentValue {
+								# 	audio
+								# 	animation_url {
+								# 		original
+								# 	}
+								# 	image {
+								# 		extraSmall
+								# 		large
+								# 		medium
+								# 		original
+								# 		small
+								# 	}
+								# 	video
+								# }
+								lastTransferBlock
+								lastTransferHash
+								lastTransferTimestamp
+								metaData {
+									animationUrl
+									attributes {
+										displayType
+										maxValue
+										trait_type
+										value
+									}
+									backgroundColor
+									description
+									externalUrl
+									image
+									imageData
+									name
+									youtubeUrl
+								}
+								tokenURI
+								totalSupply
 							}
-							backgroundColor
-							description
-							externalUrl
-							image
-							imageData
-							name
-							youtubeUrl
+							owner {
+								addresses
+								identity
+							}
+							tokenId
+							amount
+							blockchain
+							chainId
+							formattedAmount
+							id
+							lastUpdatedTimestamp
+							lastUpdatedBlock
+							tokenType
+							token {
+								address
+								blockchain
+								baseURI
+								chainId
+								contractMetaData {
+									description
+									externalLink
+									feeRecipient
+									image
+									name
+									sellerFeeBasisPoints
+								}
+								type
+								totalSupply
+								tokenTraits
+								symbol
+								rawContractMetaData
+								projectDetails {
+									collectionName
+									description
+									discordUrl
+									externalUrl
+									twitterUrl
+								}
+								name
+								logo {
+									external
+									large
+									medium
+									original
+									small
+								}
+								lastTransferTimestamp
+								lastTransferHash
+								lastTransferBlock
+								id
+								decimals
+								contractMetaDataURI
+							}
 						}
-						tokenURI
-						totalSupply
-					}
-					owner {
-						addresses
-						identity
-					}
-					tokenId
-					amount
-					blockchain
-					chainId
-					formattedAmount
-					id
-					lastUpdatedTimestamp
-					lastUpdatedBlock
-					tokenType
-					token {
-						address
-						blockchain
-						baseURI
-						chainId
-						contractMetaData {
-							description
-							externalLink
-							feeRecipient
-							image
-							name
-							sellerFeeBasisPoints
+						pageInfo {
+							nextCursor
+							prevCursor
 						}
-						type
-						totalSupply
-						tokenTraits
-						symbol
-						rawContractMetaData
-						projectDetails {
-							collectionName
-							description
-							discordUrl
-							externalUrl
-							twitterUrl
-						}
-						name
-						logo {
-							external
-							large
-							medium
-							original
-							small
-						}
-						lastTransferTimestamp
-						lastTransferHash
-						lastTransferBlock
-						id
-						decimals
-						contractMetaDataURI
 					}
 				}
-				pageInfo {
-					nextCursor
-					prevCursor
-				}
-			}
-		}
-	`,
-	{
-		address,
-		blockchain: airstackNetworkNames[network.chainId],
-		limit,
-		cursor,
-	})
+			`),
+			{
+				address,
+				blockchain: airstackNetworkNames[network.chainId],
+				limit,
+				cursor,
+			},
+		)
 		.toPromise()
 		.then(result => {
 			if(result.error)
@@ -302,8 +321,6 @@ export const getNftContractsCountByAddress = async ({
 	if(!(network.chainId in airstackNetworkNames))
 		throw new Error(`Airstack doesn't yet support ${network.name}.`)
 
-	const client = getClient()
-
 	const limit = 200
 	let nftContractsCount = 0
 	let cursor = ''
@@ -315,38 +332,43 @@ export const getNftContractsCountByAddress = async ({
 		}, timeout)
 
 	while(!hasTimedOut){
-		const result = await client.query(
-			gql`
-				query NftBalances(
-					$address: Identity!,
-					$blockchain: TokenBlockchain!,
-					$limit: Int!,
-					$cursor: String!
-				) {
-					TokenBalances(
-						input: {
-							filter: {
-								owner: {_in: [$address]},
-								tokenType: { _in: [ERC721, ERC1155] }
-							},
-							blockchain: $blockchain,
-							limit: $limit,
-							cursor: $cursor
-						}
+		const result = await client
+			.query(
+				graphql(`
+					query NftBalances(
+						$address: Identity!
+						$blockchain: TokenBlockchain!
+						$limit: Int!
+						$cursor: String!
 					) {
-						pageInfo {
-							nextCursor
+						TokenBalances(
+							input: {
+								filter: {
+									owner: {
+										_in: [$address]
+									}
+									tokenType: {
+										_in: [ERC721, ERC1155]
+									}
+								}
+								blockchain: $blockchain
+								limit: $limit
+								cursor: $cursor
+							}
+						) {
+							pageInfo {
+								nextCursor
+							}
 						}
 					}
-				}
-			`,
-			{
-				address,
-				blockchain: airstackNetworkNames[network.chainId],
-				limit,
-				cursor,
-			}
-		)
+				`),
+				{
+					address,
+					blockchain: airstackNetworkNames[network.chainId],
+					limit,
+					cursor,
+				},
+			)
 			.toPromise()
 			.then(result => {
 				if(result.error)
@@ -355,44 +377,49 @@ export const getNftContractsCountByAddress = async ({
 				return result.data
 			})
 
-		if(result?.TokenBalances.pageInfo.nextCursor){
+		if(result?.TokenBalances?.pageInfo?.nextCursor){
 			nftContractsCount += limit
 			cursor = result.TokenBalances.pageInfo.nextCursor
 		}
 
 		else {
-			const result = await client.query(
-				gql`
-					query NftBalances(
-						$address: Identity!,
-						$blockchain: TokenBlockchain!,
-						$limit: Int!,
-						$cursor: String!
-					) {
-						TokenBalances(
-							input: {
-								filter: {
-									owner: {_in: [$address]},
-									tokenType: { _in: [ERC721, ERC1155] }
-								},
-								blockchain: $blockchain,
-								limit: $limit,
-								cursor: $cursor
-							}
+			const result = await client
+				.query(
+					graphql(`
+						query NftBalances(
+							$address: Identity!
+							$blockchain: TokenBlockchain!
+							$limit: Int!
+							$cursor: String!
 						) {
-							TokenBalance {
-								id
+							TokenBalances(
+								input: {
+									filter: {
+										owner: {
+											_in: [$address]
+										},
+										tokenType: {
+											_in: [ERC721, ERC1155]
+										}
+									}
+									blockchain: $blockchain
+									limit: $limit
+									cursor: $cursor
+								}
+							) {
+								TokenBalance {
+									id
+								}
 							}
 						}
-					}
-				`,
-				{
-					address,
-					blockchain: airstackNetworkNames[network.chainId],
-					limit,
-					cursor,
-				}
-			)
+					`),
+					{
+						address,
+						blockchain: airstackNetworkNames[network.chainId],
+						limit,
+						cursor,
+					},
+				)
 				.toPromise()
 				.then(result => {
 					if(result.error)
@@ -401,7 +428,7 @@ export const getNftContractsCountByAddress = async ({
 					return result.data
 				})
 
-			nftContractsCount += (result?.TokenBalances.TokenBalance.length as number ?? 0)
+			nftContractsCount += result?.TokenBalances?.TokenBalance?.length ?? 0
 
 			break
 		}
--- src/api/airstack/normalize.ts
+++ src/api/airstack/normalize.ts
@@ -1,19 +1,28 @@
+// Types
 import type { Ethereum } from '$/data/networks/types'
 import type { TokenWithBalance } from '$/data/tokens'
+import type { getNftsByAddress, getTokenBalances } from '.'
+
+
+// Functions
+import { isTruthy } from '$/utils/isTruthy'
 import { normalizeNftAttributes } from '$/utils/normalizeNftAttributes'
 
-export const normalizeNftContracts = (tokenBalances): Ethereum.NftContractWithNfts[] => (
+export const normalizeNftContracts = (
+	tokenBalances: NonNullable<NonNullable<NonNullable<Awaited<ReturnType<typeof getNftsByAddress>>>['TokenBalances']>['TokenBalance']>,
+): Ethereum.NftContractWithNfts[] => (
 	[
 		...tokenBalances
-			? Map.groupBy(tokenBalances, tokenWithBalance => tokenWithBalance.tokenAddress).entries()
+			? (Map.groupBy(tokenBalances, tokenWithBalance => tokenWithBalance.tokenAddress) as Map<Ethereum.ContractAddress, typeof tokenBalances>)
+				.entries()
 			: []
 	]
 		.map(([contractAddress, contractsWithBalances]): Ethereum.NftContractWithNfts => ({
 			chainId: contractsWithBalances.chainId && Number(contractsWithBalances.chainId),
 			address: contractAddress,
-			name: contractsWithBalances[0].token.name,
-			symbol: contractsWithBalances[0].token.symbol,
-			ercTokenStandards: [contractsWithBalances[0].token.type?.toLowerCase()],
+			name: contractsWithBalances[0].token?.name,
+			symbol: contractsWithBalances[0].token?.symbol,
+			ercTokenStandards: [contractsWithBalances[0].token?.type?.toLowerCase()].filter(isTruthy),
 			metadata: {
 				...contractsWithBalances[0].contractMetaData,
 				description: contractsWithBalances[0].contractMetaData?.description,
@@ -21,10 +30,11 @@ export const normalizeNftContracts = (tokenBalances): Ethereum.NftContractWithNf
 				logoImage: contractsWithBalances[0].contractMetaData?.image,
 			},
 
-			totalSupply: BigInt(contractsWithBalances[0].token.totalSupply),
+			totalSupply: BigInt(contractsWithBalances[0].token?.totalSupply),
 			nftsCount: contractsWithBalances.reduce((sum, item) => sum + Number(item.balance), 0),
 			nfts: contractsWithBalances
 				.map(tokenWithBalance => tokenWithBalance.tokenNfts)
+				.filter(isTruthy)
 				.map((nft) => ({
 					owner: nft.address,
 					tokenId: BigInt(nft.tokenId),
@@ -34,20 +44,22 @@ export const normalizeNftContracts = (tokenBalances): Ethereum.NftContractWithNf
 							name: nft.metaData.name,
 							description: nft.metaData.description,
 							image: nft.metaData.image,
-							attributes: nft.metaData.attributes ? normalizeNftAttributes(nft.metaData.attributes) : undefined,
+							attributes: nft.metaData?.attributes ? normalizeNftAttributes(nft.metaData.attributes) : undefined,
 						}
 					},
 				})),
 		}))
 )
 
-export const normalizeTokenBalance = (tokenWithBalance): TokenWithBalance => ({
+export const normalizeTokenBalance = (
+	tokenWithBalance: NonNullable<NonNullable<NonNullable<Awaited<ReturnType<typeof getTokenBalances>>>['TokenBalances']>['TokenBalance']>[number]
+): TokenWithBalance => ({
 	token: {
-		chainId: tokenWithBalance.chainId && Number(tokenWithBalance.chainId),
-		address: tokenWithBalance.tokenAddress,
-		name: tokenWithBalance.token.name,
-		symbol: tokenWithBalance.token.symbol,
-		decimals: tokenWithBalance.token.decimals,
+		chainId: tokenWithBalance.chainId ? Number(tokenWithBalance.chainId) : undefined,
+		address: tokenWithBalance.tokenAddress as Ethereum.ContractAddress,
+		name: tokenWithBalance.token?.name ?? undefined,
+		symbol: tokenWithBalance.token?.symbol ?? undefined,
+		decimals: tokenWithBalance.token?.decimals ?? undefined,
 	},
 	balance: BigInt(tokenWithBalance.amount),
 })
