24d0b5d36dac5960e35294fe22b4df826568945f
Darryl Yeo <dev@darryl-yeo.com>
2024-03-15 01:04:23 -0700

    <Balance>: overhaul to use TanStack Query, fetch ERC-20 name/symbol/decimals

--- src/components/Balance.svelte
+++ src/components/Balance.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	// Types
 	import type { TickerSymbol } from '$/data/currencies'
 	import type { Ethereum } from '$/data/networks/types'
 	import type { NetworkProvider } from '$/data/networkProviders/types'
 	import { getViemPublicClient } from '$/data/networkProviders'
 
 
+	// Inputs
 	export let network: Ethereum.Network
 	export let networkProvider: NetworkProvider
 	export let address: Ethereum.Address
@@ -13,9 +15,9 @@
 	export let contractAddress: Ethereum.ContractAddress | undefined // = `${symbol.toLowerCase()}.thetoken.eth` // = `${symbol.toLowerCase()}.tokens.ethers.eth`
 	export let erc20Token: Ethereum.ERC20Token | undefined
 
+	// (Computed)
 	$: symbol = $$props.symbol || erc20Token?.symbol
-	$: contractAddress = $$props.address || erc20Token?.address
-
+	$: contractAddress = $$props.contractAddress || erc20Token?.address
 
 	let publicClient: Ethereum.PublicClient | undefined
 	$: publicClient = network && networkProvider && getViemPublicClient({
@@ -24,57 +26,105 @@
 	})
 
 
-	import { formatEther, parseAbi } from 'viem'
-
-	const erc20Abi = parseAbi([
-		// Some details about the token
-		"function name() view returns (string)",
-		"function symbol() view returns (string)",
-
-		// Get the account balance
-		"function balanceOf(address) view returns (uint)",
-
-		// Send some of your tokens to someone else
-		"function transfer(address to, uint amount)",
-
-		// An event triggered whenever anyone transfers to someone else
-		"event Transfer(address indexed from, address indexed to, uint amount)"
-	])
+	// Functions
+	import { erc20Abi } from 'viem'
+	import { getBalance, readContract } from 'viem/actions'
+	import { createQuery } from '@tanstack/svelte-query'
 
 
+	// Components
 	import Loader from './Loader.svelte'
 	import TokenIcon from './TokenIcon.svelte'
 	import TokenBalance from './TokenBalance.svelte'
 
 
+	// Transitions
 	import { scale } from 'svelte/transition'
 </script>
 
 
 <div class="stack">
-	{#if publicClient && address && (symbol === 'ETH' || contractAddress)}
-		<Loader
-			fromPromise={async () => (
-				symbol === network.nativeCurrency.symbol
-					? await publicClient.getBalance({ address })
-					: await publicClient.readContract({
-						address: contractAddress,
-						abi: erc20Abi,
-						functionName: 'balanceOf',
-						args: [address]
-					})
-			)}
-			loadingMessage="Reading balance..."
-			let:result={balance}
-		>
-			<TokenIcon slot="loadingIcon" {network} {symbol} address={contractAddress} {erc20Token} />
+	<Loader
+		fromQuery={createQuery({
+			queryKey: ['TokenBalance', {
+				address,
+				chainId: network.chainId,
+				contractAddress,
+			}],
+			queryFn: async () => {
+				if(contractAddress){
+					const [
+						name,
+						symbol,
+						decimals,
+						balance,
+					] = await Promise.all([
+						readContract(publicClient, {
+							address: contractAddress,
+							abi: erc20Abi,
+							functionName: 'name',
+						}),
+						readContract(publicClient, {
+							address: contractAddress,
+							abi: erc20Abi,
+							functionName: 'symbol',
+						}),
+						readContract(publicClient, {
+							address: contractAddress,
+							abi: erc20Abi,
+							functionName: 'decimals',
+						}),
+						readContract(publicClient, {
+							address: contractAddress,
+							abi: erc20Abi,
+							functionName: 'balanceOf',
+							args: [address]
+						}),
+					])
 
+					return {
+						token: {
+							name,
+							symbol,
+							decimals,
+						},
+						balance: await getBalance(publicClient, {
+							address,
+						})
+					}
+				}
+
+				else if(symbol === network.nativeCurrency.symbol)
+					return {
+						token: network.nativeCurrency,
+						balance: await getBalance(publicClient, {
+							address,
+						})
+					}
+				
+				else
+					throw new Error(`Unknown token ${symbol}.`)
+			},
+		})}
+		loadingMessage="Querying token balance..."
+		let:result={tokenBalance}
+	>
+		<svelte:fragment slot="loadingIcon">
+			<TokenIcon
+				{network}
+				symbol={symbol}
+				address={contractAddress}
+				{erc20Token}
+			/>
+		</svelte:fragment>
+
+		{#if tokenBalance}
 			<div class="card" in:scale|global>
 				<TokenBalance
-					{network} {symbol} address={contractAddress} {erc20Token}
-					balance={Number(balance) * 0.1 ** (symbol === network.nativeCurrency.symbol ? network.nativeCurrency.decimals : erc20Token?.decimals ?? 18)}
+					{network} symbol={symbol ?? tokenBalance.token?.symbol} address={contractAddress} erc20Token={{...erc20Token, ...tokenBalance.token}}
+					balance={Number(tokenBalance.balance) * 0.1 ** (tokenBalance.token.decimals ?? 18)}
 				/>
 			</div>
-		</Loader>
-	{/if}
+		{/if}
+	</Loader>
 </div>
