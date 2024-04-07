3d6c1a79924e491a0b0778734c87ebfea3ba655e
Darryl Yeo <dev@darryl-yeo.com>
2024-04-07 16:50:17 -0700

    <InlineTransition>: update to use <SizeContainer>'s content transitions

--- src/components/InlineTransition.svelte
+++ src/components/InlineTransition.svelte
@@ -10,14 +10,19 @@
 	export let key: Key | undefined
 	export let value: Value | undefined
 	export let isOpen: boolean = true
+	export let renderOnlyWhenOpen = false
 
 	export let contentProps: Record<string, any> | undefined
 
 	export let align: 'start' | 'center' | 'end' = 'start'
 	export let clip: boolean | undefined
 
-	export let transition: (node: Element, _?: TransitionParams) => TransitionConfig = scale
+	export let transition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined = scale
 	export let transitionParams: TransitionParams | undefined
+	export let inTransition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined
+	export let inTransitionParams: TransitionParams | undefined
+	export let outTransition: ((node: Element, _?: TransitionParams) => TransitionConfig) | undefined
+	export let outTransitionParams: TransitionParams | undefined
 
 
 	import InlineContainer from './InlineContainer.svelte'
@@ -26,25 +31,38 @@
 
 <InlineContainer
 	{isOpen}
-	renderOnlyWhenOpen={false}
+	{renderOnlyWhenOpen}
+
+	{clip}
+
 	containerProps={{
 		class: `stack inline align-${align}`,
 	}}
+
+	contentKey={key ?? value}
+	contentTransition={
+		transition ?
+			[transition, transitionParams]
+		: inTransition || outTransition ?
+			{
+				...inTransition && { in: [inTransition, inTransitionParams] },
+				...outTransition && { out: [outTransition, outTransitionParams] },
+			}
+		: undefined
+	}
 	contentProps={{
 		class: `stack inline align-${align}`,
+		...contentProps,
 	}}
-	contentKey={key ?? value}
-	{clip}
->{
-	#if isOpen
-		}<span
-			class={`row inline align-${align}`}
-			{...contentProps}
-			transition:transition|global={transitionParams}
-			><slot
-				{key}
-				{value}
-			>{value}</slot
-		></span>{
-	/if
-}</InlineContainer>
+>
+	<span
+		class={`row inline align-${align}`}
+	>
+		<slot
+			{key}
+			{value}
+		>
+			{value}
+		</slot>
+	</span>
+</InlineContainer>
