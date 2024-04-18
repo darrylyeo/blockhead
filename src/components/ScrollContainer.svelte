3075616ccbdc4c7974bf6d2c30456154b2c31c78
Darryl Yeo <dev@darryl-yeo.com>
2024-04-18 00:19:47 -0700

    <ScrollContainer>: enable "infinite scroll" pagination by detecting visibility of <Visible> children

---
+++ src/components/ScrollContainer.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	// Inputs
+	export let pagination: Loader<any, any, any, any>['$$slot_def']['default']['pagination']
+	export let margin: number | undefined
+
+
+	// Components	
+	import Loader from './Loader.svelte'
+	import Visible from './Visible.svelte'
+</script>
+
+
+<div
+	class="column scrollable-list"
+	{...$$restProps}
+>
+	{#if pagination?.hasPreviousPage}
+		<Visible
+			{margin}
+			onVisible={() => pagination?.fetchPreviousPage()}
+		>
+			<slot name="before" />
+		</Visible>
+	{/if}
+
+	<slot />
+
+	{#if pagination?.hasNextPage}
+		<Visible
+			{margin}
+			onVisible={() => pagination?.fetchNextPage()}
+		>
+			<slot name="after" />
+		</Visible>
+	{/if}
+</div>
