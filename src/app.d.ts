9c8fc59c2b2ee96518509432e81e504a1289d748
Darryl Yeo <dev@darryl-yeo.com>
2024-05-15 15:58:50 -0700

    SvelteKit: add types for meta tags in page data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { MetaTagsProps } from 'svelte-meta-tags'
+
 // and what to do when importing types
 declare namespace App {
 	// interface Locals {}
-	// interface PageData {}
+
+	interface PageData {
+		metaTags?: MetaTagsProps
+	}
+
 	// interface Error {}
 	// interface Platform {}
 }
