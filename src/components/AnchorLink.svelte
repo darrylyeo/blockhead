73376c9fecbc9469b71bef38946e37d24f49298c
Darryl Yeo <dev@darryl-yeo.com>
2024-05-17 23:55:51 -0700

    <AnchorLink>: wrap contents in an anchor link

---
+++ src/components/AnchorLink.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// Inputs
+	export let link: `/${string}`
+	export let isEnabled: boolean = true
+</script>
+
+
+{#if isEnabled}
+	<a
+		id={link}
+		href={`#${link}`}
+		{...$$restProps}
+		class={$$props.class ?? 'card scroll-snap-item'}
+	>
+		<slot />
+	</a>
+{:else}
+	<span
+		class={$$props.class ?? 'card'}
+	>
+		<slot />
+	</span>
+{/if}
