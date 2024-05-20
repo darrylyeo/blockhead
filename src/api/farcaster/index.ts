d9d48ddabe88404d8caeddbcc2a66c722f20d2af
Darryl Yeo <dev@darryl-yeo.com>
2024-05-20 14:54:09 -0700

    Farcaster: make channels optional in casts

--- src/api/farcaster/index.ts
+++ src/api/farcaster/index.ts
@@ -117,7 +117,7 @@ export type FarcasterCast = {
 	replies?: FarcasterCast[];
 	repliesCount?: number;
 
-	channel: FarcasterChannel | Pick<FarcasterChannel, 'id'> | Pick<FarcasterChannel, 'url'>;
+	channel?: FarcasterChannel | Pick<FarcasterChannel, 'id'> | Pick<FarcasterChannel, 'url'>;
 }
 
 
