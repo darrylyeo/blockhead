664cf02e68e73dc9f3e0bf9c68ece7d7e1b16efd
Darryl Yeo <dev@darryl-yeo.com>
2024-05-22 00:14:11 -0700

    Explorer / Apps: refactor to use consistent layout

--- src/routes/apps/(withNavigation)/+layout.svelte
+++ src/routes/apps/(withNavigation)/+layout.svelte
@@ -38,133 +38,143 @@
 </script>
 
 
-<style>
-	.grid {
-		gap: 1.5rem 2rem;
-		display: flex;
-		flex-wrap: wrap;
-	}
-	.grid > * {
-		flex: 1 auto;
-	}
-
-	.row {
-		align-content: start;
-		--padding-inner: 1rem;
-	}
-	.row > * {
-		flex: 1 auto;
-		text-align: center;
-		justify-content: center;
-	}
-
-	.item {
-		text-decoration: none;
-	}
-	.item:hover {
-		transform: scale(1.04);
-	}
-
-	.item h3 {
-		gap: 0.66em;
-	}
-
-	.item span {
-		color: rgba(var(--rgb-light-dark-inverse), calc(0.6 + 0.3 * var(--is-dark)));
-		text-shadow: 0 0 0 var(--primary-color);
-		opacity: calc(0.75 + 0.25 * var(--is-dark));
-	}
-
-	section {
-		padding-top: 1.5rem;
-		position: relative;
-		gap: 1rem;
-	}
-	section:before {
-		content: '';
-		position: absolute;
-		top: 0;
-		width: 100%;
-
-		background: none;
-		border: none;
-		border-top: currentColor 1px solid;
-		opacity: 0.2;
-	}
-
-	section.featured .content {
-		gap: 1.5rem;
-		font-size: 1.15em;
-	}
-	section:not(.featured) .content {
-		font-size: 0.9em;
-	}
-	section:not(.featured) .content:after {
-		flex: 10000;
-	}
-</style>
-
-
 <div
-	class="column"
+	class="layout"
 	in:fly|global={{x: 300}}
 	out:fly|global={{x: -300}}
 >
-	<div class="grid">
-		{#each displayedWeb3AppsBySection as {title, apps, isFeatured}, i(title)}
-			<section
-				class="column"
+	{#each displayedWeb3AppsBySection as {title, apps, isFeatured}, i(title)}
+		<section
+			class="column"
+			transition:scale={{ duration: 300, easing: expoOut }}
+			animate:flip={{ duration: 300, easing: expoOut }}
+		>
+			<h2>{title}</h2>
+
+			<div
+				class="content row wrap"
 				class:featured={isFeatured}
-				transition:scale={{ duration: 300, easing: expoOut }}
-				animate:flip={{ duration: 300, easing: expoOut }}
 			>
-				<h2>{title}</h2>
-
-				<div class="content row wrap">
-					{#each apps as app, i}
-						<a
-							href={
-								$network
-									? resolveRoute(`/apps/[web3AppSlug]/network/[networkSlug]`, { web3AppSlug: app.slug, networkSlug: $network.slug })
-									: resolveRoute(`/apps/[web3AppSlug]`, { web3AppSlug: app.slug })
-							}
-							class="item card"
-							in:scale={{ delay: i * 10, duration: 300 }}
-							out:scale
-							style={cardStyle(app.colors)}
-						>
-							<h3 class="row">
-								{#if app.icon}
-									<img src={app.icon} alt={app.name} width={24} />
-								{:else}
-									{#each app.views?.flatMap(view => view.erc20Tokens ?? []).filter(Boolean).slice(0, 1) as erc20Token}
-										<TokenIcon {erc20Token} />
-									{/each}
-								{/if}
-
-								<span>{app.name}</span>
-							</h3>
-						</a>
-					{/each}
-				</div>
-			</section>
-		{:else}
-			<section
-				class="column"
-				transition:scale={{ duration: 300, easing: expoOut }}
-			>
-				<div class="card bar wrap">
-					<p class="faded">No apps to show{$network ? ` for ${$network.name}` : ''} yet!</p>
-
+				{#each apps as app, i}
 					<a
-						href="/#contact"
-						target="_blank"
-						class="button medium"
+						href={
+							$network
+								? resolveRoute(`/apps/[web3AppSlug]/network/[networkSlug]`, { web3AppSlug: app.slug, networkSlug: $network.slug })
+								: resolveRoute(`/apps/[web3AppSlug]`, { web3AppSlug: app.slug })
+						}
+						class="item card row"
+						in:scale={{ delay: i * 10, duration: 300 }}
+						out:scale
+						style={cardStyle(app.colors)}
 					>
-						Feedback ›
+						{#if app.icon}
+							<img src={app.icon} alt={app.name} width={24} />
+						{:else}
+							{#each app.views?.flatMap(view => view.erc20Tokens ?? []).filter(Boolean).slice(0, 1) as erc20Token}
+								<TokenIcon {erc20Token} />
+							{/each}
+						{/if}
+
+						<span>{app.name}</span>
 					</a>
-				</div>
-			</section>
-		{/each}
-	</div>
+				{/each}
+			</div>
+		</section>
+	{:else}
+		<section
+			class="column"
+			transition:scale={{ duration: 300, easing: expoOut }}
+		>
+			<div class="card bar wrap">
+				<p class="faded">No apps to show{$network ? ` for ${$network.name}` : ''} yet!</p>
+
+				<a
+					href="/#contact"
+					target="_blank"
+					class="button medium"
+				>
+					Feedback ›
+				</a>
+			</div>
+		</section>
+	{/each}
 </div>
+
+
+<style>
+	.layout {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5rem 2rem;
+
+		> section {
+			flex: 1 auto;
+
+			position: relative;
+			align-content: start;
+			gap: 1rem;
+			padding-top: 1.5rem;
+
+			&:before {
+				content: '';
+				position: absolute;
+				top: 0;
+				width: 100%;
+
+				background: none;
+				border: none;
+				border-top: currentColor 1px solid;
+				opacity: 0.2;
+			}
+
+			.content {
+				gap: 1rem;
+
+				&.featured {
+					font-size: 1.15em;
+				}
+
+				&:not(.featured) {
+					font-size: 0.9em;
+
+					&:after {
+						content: '';
+						flex: 10000;
+					}
+				}
+
+				.item {
+					flex: 1 auto;
+
+					justify-content: center;
+					padding: 1rem 1.125rem;
+					gap: 0.66em;
+
+					font-size: 1.17em;
+					font-weight: 600;
+					line-height: 1.2;
+					text-align: center;
+
+					transition-duration: 0.2s;
+					transition-timing-function: ease-in-out;
+					transition-property:
+						background,
+						color,
+						opacity,
+						scale
+					;
+
+					&:hover {
+						scale: 1.04;
+					}
+
+					span {
+						color: rgba(var(--rgb-light-dark-inverse), calc(0.6 + 0.3 * var(--is-dark)));
+						text-shadow: 0 0 0 var(--primary-color);
+						opacity: calc(0.75 + 0.25 * var(--is-dark));
+					}
+				}
+			}
+		}
+	}
+</style>
--- src/routes/explorer/+page.svelte
+++ src/routes/explorer/+page.svelte
@@ -14,94 +14,139 @@
 
 	import { cardStyle } from '$/utils/card-background'
 	import { fly, scale } from 'svelte/transition'
+	import { flip } from 'svelte/animate'
 </script>
 
 
-<style>
-	.column {
-		--padding-inner: 1.5rem;
-	}
 
-	.row {
-		align-content: start;
-		--padding-inner: 1rem;
-	}
-	.row > * {
-		flex: 1 auto;
-		text-align: center;
-		justify-content: center;
-	}
+<div
+	class="layout"
+	in:fly|global={{x: 300}}
+	out:fly|global={{x: -300}}
+>
+	{#each networksBySection as {title, featuredNetworks, otherNetworks}, i (title)}
+		<section
+			class="column"
+			transition:scale={{ duration: 300, easing: expoOut }}
+			animate:flip={{ duration: 300, easing: expoOut }}
+		>
+			<h2>{title}</h2>
 
-	.item {
-		text-decoration: none;
-	}
-	.item:hover {
-		transform: scale(1.04);
-	}
-
-	.item h3 {
-		gap: 0.66em;
-	}
-
-	.item span {
-		color: rgba(var(--rgb-light-dark-inverse), calc(0.6 + 0.3 * var(--is-dark)));
-		text-shadow: 0 0 0 var(--primary-color);
-		opacity: calc(0.75 + 0.25 * var(--is-dark));
-	}
-
-	section.featured {
-		font-size: 1.15em;
-	}
-	section:not(.featured) {
-		font-size: 0.9em;
-	}
-	section:not(.featured):after {
-		content: '';
-		flex: 10000;
-	}
-</style>
-
-
-<div class="column" in:fly|global={{x: 300}} out:fly|global={{x: -300}}>
-	{#each networksBySection as {title, featuredNetworks, otherNetworks}}
-		<hr>
-
-		<h2>{title}</h2>
-
-		{#each
-			[
-				featuredNetworks && { networks: featuredNetworks, isFeatured: true },
-				otherNetworks && { networks: otherNetworks, isFeatured: false }
-			].filter(isTruthy)
-			as
-			{ networks, isFeatured }
-		}
-			<SizeContainer>
-				<section class="row wrap" class:featured={isFeatured}>
-					{#each
-						(
-							$showTestnets
-								? networks.flatMap(network => [network, ...testnetsForMainnets[network.slug] ?? []])
-								: networks.filter(network => !isTestnet(network))
-						).filter(network => network.chainId)
-						as
-						network, i (network.slug)
-					}
-						<a
-							href="/explorer/{network.slug}"
-							class="item card"
-							style={cardStyle([getNetworkColor(network)])}
-							in:scale|global={{ duration: 300, delay: i * 10 }}
-							out:scale={{ duration: 300 }}
+			{#each
+				[
+					featuredNetworks && { networks: featuredNetworks, isFeatured: true },
+					otherNetworks && { networks: otherNetworks, isFeatured: false }
+				].filter(isTruthy)
+				as
+				{ networks, isFeatured }
+			}
+				<SizeContainer>
+					<div
+						class="content row wrap"
+						class:featured={isFeatured}
+					>
+						{#each
+							(
+								$showTestnets
+									? networks.flatMap(network => [network, ...testnetsForMainnets[network.slug] ?? []])
+									: networks.filter(network => !isTestnet(network))
+							).filter(network => network.chainId)
+							as
+							network, i (network.slug)
+						}
+							<a
+								href="/explorer/{network.slug}"
+								class="item card row"
+								style={cardStyle([getNetworkColor(network)])}
+								in:scale|global={{ duration: 300, delay: i * 10 }}
+								out:scale={{ duration: 300 }}
 						>
-							<h3 class="row">
 								<NetworkIcon {network} />
 								<span>{network.name}</span>
-							</h3>
-						</a>
-					{/each}
-				</section>
-			</SizeContainer>
-		{/each}
+							</a>
+						{/each}
+					</div>
+				</SizeContainer>
+			{/each}
+		</section>
 	{/each}
 </div>
+
+
+<style>
+	.layout {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5rem 2rem;
+
+		> section {
+			flex: 1 auto;
+
+			position: relative;
+			align-content: start;
+			gap: 1rem;
+			padding-top: 1.5rem;
+
+			&:before {
+				content: '';
+				position: absolute;
+				top: 0;
+				width: 100%;
+
+				background: none;
+				border: none;
+				border-top: currentColor 1px solid;
+				opacity: 0.2;
+			}
+
+			.content {
+				gap: 1rem;
+
+				&.featured {
+					font-size: 1.15em;
+				}
+
+				&:not(.featured) {
+					font-size: 0.9em;
+
+					&:after {
+						content: '';
+						flex: 10000;
+					}
+				}
+
+				.item {
+					flex: 1 auto;
+
+					justify-content: center;
+					padding: 1rem 1.125rem;
+					gap: 0.66em;
+
+					font-size: 1.17em;
+					font-weight: 600;
+					line-height: 1.2;
+					text-align: center;
+
+					transition-duration: 0.2s;
+					transition-timing-function: ease-in-out;
+					transition-property:
+						background,
+						color,
+						opacity,
+						scale
+					;
+
+					&:hover {
+						scale: 1.04;
+					}
+
+					span {
+						color: rgba(var(--rgb-light-dark-inverse), calc(0.6 + 0.3 * var(--is-dark)));
+						text-shadow: 0 0 0 var(--primary-color);
+						opacity: calc(0.75 + 0.25 * var(--is-dark));
+					}
+				}
+			}
+		}
+	}
+</style>
