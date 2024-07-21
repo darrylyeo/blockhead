29267fd4b0ae20d7308feed7af395bfdb4bd9ec4
Darryl Yeo <dev@darryl-yeo.com>
2024-07-21 17:47:35 -0500

    Zapper: improve types

--- src/api/zapper/index.ts
+++ src/api/zapper/index.ts
@@ -25,6 +25,100 @@ export const networkNamesByChainId: Record<Ethereum.ChainID, ZapperSupportedNetw
 
 type ZapperSupportedApp = string
 
+type Address = `0x${string}`
+type Timestamp = `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`
+
+type Network = {
+	id: number,
+	slug: string,
+	name: string,
+	enabled: boolean,
+	evmCompatible: boolean,
+	vm: 'evm',
+	holdingsComparisonJobEnabled: boolean,
+	holdingsEnabled: boolean,
+	hasLiveFeedEtl: boolean,
+	nftBalancesEnabled: boolean,
+	onchainPricesEnabled: boolean,
+	activityFeedEnabled: boolean,
+	thirdPartyBaseTokensEnabled: boolean,
+	useHoldingsAsBalances: boolean,
+	chainId: number,
+	multicallContractAddress: Address,
+	wrappedGasTokenAddress: Address,
+	blockScannerBaseUrl: string,
+	blocksPerDayEstimate: number,
+	config: {
+		web3Provider: string,
+		coinGeckoPlatform: string,
+	}[],
+}
+
+type NativeToken = {
+	id: `${number}`,
+	address: Address,
+	name: string,
+	symbol: string,
+	decimals: number,
+	coingeckoId: string,
+	hide: boolean,
+	canExchange: boolean,
+	updatedAt: Timestamp,
+	createdAt: Timestamp,
+	price: number,
+	networkId: number,
+	status: 'approved',
+	totalSupply: `${bigint}`,
+	dailyVolume: number | null,
+	verified: boolean,
+	holdersEnabled: boolean,
+	marketCap: number | null,
+	priceUpdatedAt: Timestamp,
+	externallyVerified: boolean,
+	label: string | null,
+	balance: number,
+	balanceUSD: number,
+	balanceRaw: `${bigint}`,
+}
+
+type Token = {
+	id: `${string}:${number}`,
+	networkId: number,
+	blockNumber: number | null,
+	blockHash: string | null,
+	address: Address,
+	symbol: string,
+	name: string,
+	decimals: number,
+	totalSupply: `${bigint}`,
+	updatedAt: Timestamp,
+	createdAt: Timestamp,
+	ignoreHoldingsForBalance: null,
+	isSpam: null,
+	spamReason: null,
+	network: Network,
+	coingeckoId: string | null,
+	status: 'approved',
+	hide: boolean,
+	canExchange: boolean,
+	verified: boolean,
+	externallyVerified: boolean,
+	priceUpdatedAt: string,
+	dailyVolume: number | null,
+	holdersEnabled: boolean,
+	marketCap: number | null,
+	daos: string[],
+	historicData: {
+		[key: string]: {
+			[key: string]: number,
+		},
+	} | null,
+	price: number,
+	balance: number,
+	balanceUSD: number,
+	balanceRaw: `${bigint}`,
+}
+
 
 // API client
 import * as publicEnv from '$env/static/public'
@@ -72,32 +166,7 @@ export const getTokenBalances = async ({
 		address: Ethereum.Address,
 		network: ZapperSupportedNetwork,
 		updatedAt: `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`,
-		token: {
-			id: `${bigint}`,
-			address: Ethereum.Address,
-			name: string,
-			symbol: string,
-			decimals: number,
-			coingeckoId: string,
-			hide: boolean,
-			canExchange: boolean,
-			updatedAt: `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`,
-			createdAt: `${number}-${number}-${number}T${number}:${number}:${number}.${number}Z`,
-			price: number,
-			networkId: number,
-			status: 'approved',
-			totalSupply: `${bigint}`,
-			dailyVolume: number,
-			verified: boolean,
-			holdersEnabled: boolean,
-			marketCap: number,
-			priceUpdatedAt: string,
-			externallyVerified: boolean,
-			label: string | null,
-			balance: number,
-			balanceUSD: number,
-			balanceRaw: `${bigint}`,
-		},
+		token: NativeToken | Token,
 	}[]>
 
 	return Object.values(result)[0]
