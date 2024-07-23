112d7aad61de99621a2b44dabea09dd07dceaf18
Darryl Yeo <dev@darryl-yeo.com>
2024-07-23 00:29:43 -0500

    <HistoricalPriceChart>: organize; refactor to use `<Collapsible>`

--- src/components/HistoricalPriceChart.svelte
+++ src/components/HistoricalPriceChart.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
+	// Types/constants
 	import type { Ethereum } from '$/data/networks/types'
 	import type { QuoteCurrency, TickerSymbol } from '$/data/currencies'
+	import { PriceProvider } from '$/data/priceProviders'
 
-	export let historicalPriceProvider = 'Covalent'
+
+	// Inputs
+	export let historicalPriceProvider = PriceProvider.Covalent
 	export let currencies: (TickerSymbol | Ethereum.ContractAddress)[]
 	export let quoteCurrency: QuoteCurrency
 
 
+	// Internal state
 	import type { PriceScale } from './PriceChart.svelte'
 	let priceScale: PriceScale
 
 
+	// Components
+	import Collapsible from './Collapsible.svelte'
 	import CovalentPriceChart from './CovalentPriceChart.svelte'
 </script>
+
+
+<Collapsible
+	type="label"
+>
+	<svelte:fragment slot="title">
+		<slot name="title">
+			<h3>Historical Price</h3>
+		</slot>
+	</svelte:fragment>
+
+	<svelte:fragment slot="header-right">
+		<span class="card-annotation">{historicalPriceProvider}</span>
+	</svelte:fragment>
+
+	<CovalentPriceChart
+		{historicalPriceProvider}
+		{currencies}
+		{quoteCurrency}
+		{priceScale}
+	/>
+
+	<footer class="options bar wrap">
+		<h4>Show</h4>
+
+		<label>
+			<span>Price Scale</span>
+			<select bind:value={priceScale}>
+				<option value="logarithmic">Logarithmic</option>
+				<option value="linear">Linear</option>
+				<option value="linearFromZero">Linear From Zero</option>
+			</select>
+		</label>
+	</footer>
+</Collapsible>
 
 
 <style>
@@ -20,30 +62,3 @@
 		font-size: calc(var(--options-size, 1) * 1em);
 	}
 </style>
-
-
-<div class="bar wrap">
-	<slot name="title">
-		<h3>Historical Price</h3>
-	</slot>
-	<span class="card-annotation">{historicalPriceProvider}</span>
-</div>
-
-<CovalentPriceChart
-	{historicalPriceProvider}
-	{currencies}
-	{quoteCurrency}
-	{priceScale}
-/>
-
-<div class="options bar wrap">
-	<h4>Show</h4>
-	<label>
-		<span>Price Scale</span>
-		<select bind:value={priceScale}>
-			<option value="logarithmic">Logarithmic</option>
-			<option value="linear">Linear</option>
-			<option value="linearFromZero">Linear From Zero</option>
-		</select>
-	</label>
-</div>
