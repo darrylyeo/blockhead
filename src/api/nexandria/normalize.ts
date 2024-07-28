01da482f4cc53474cc04e660a0fe005cd4ad1f3d
Darryl Yeo <dev@darryl-yeo.com>
2024-07-28 15:17:24 -0500

    Nexandria: improve quote currency normalization logic

--- src/api/nexandria/normalize.ts
+++ src/api/nexandria/normalize.ts
@@ -1,12 +1,10 @@
 import type { Ethereum } from '$/data/networks/types'
 import type { TokenWithBalance } from '$/data/tokens'
-import type { QuoteCurrency } from '$/data/currencies'
 import type { getPointInTimeBalances } from '.'
 
 
 export const normalizeTokenBalance = (
 	tokenBalance: Awaited<ReturnType<typeof getPointInTimeBalances>>['tokens'][0],
-	quoteCurrency: QuoteCurrency = 'USD',
 	chainId: Ethereum.ChainID,
 ): TokenWithBalance => ({
 	token: {
@@ -20,10 +18,10 @@ export const normalizeTokenBalance = (
 	balance: tokenBalance.native_balance && BigInt(Number(tokenBalance.native_balance.replace(/,/g, '')) * 10 ** 100),
 	// balance: tokenBalance.native_balance && tokenBalance.decimals && BigInt(tokenBalance.native_balance.replace(/,/g, '') * 10 ** tokenBalance.decimals),
 
-	...('fiat_value' in tokenBalance && {
+	...'fiat_value' in tokenBalance && {
 		conversion: {
-			currency: quoteCurrency,
+			currency: 'USD',
 			value: Number(tokenBalance.fiat_value.replace(/,/g, '')),
 		},
-	}),
+	},
 })
