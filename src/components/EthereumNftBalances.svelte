9bae74e994728ac185bc4c82252c5700e4a88dfb
Darryl Yeo <dev@darryl-yeo.com>
2024-08-03 15:04:45 -0500

    <EthereumNftBalances>: use nested CSS rules

--- src/components/EthereumNftBalances.svelte
+++ src/components/EthereumNftBalances.svelte
@@ -84,104 +84,119 @@
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
 		grid-auto-flow: row dense;
-	}
-	.nft-contracts.scrollable-list {
-		--resizeVertical-defaultHeight: 39.5rem;
-	}
-	.nft-contract {
-		position: relative;
-	}
-	.nft-contract:has(> [data-state="open"]):not(.is-single) {
-		grid-column: 1 / -1;
-	}
-	.nft-contract:has(> [data-state="open"]) {
-		grid-row-end: span 6;
-	}
-	.nft-contract header > * {
-		margin: -0.25em 0;
-	}
-	.nft-contract-logo {
-		width: fit-content;
-		height: 1.75em;
-		border-radius: 0.3em;
-	}
-	.nft-contract header .nfts-preview {
-		display: flex;
-		flex-direction: row;
-	}
-	.nft-contract header .nfts-preview > :global(* + *) {
-		margin-inline-start: -1em;
-	}
-	.nft-contract header .nfts-preview > :global(.nft-image img) {
-		border-radius: 3px;
+
+		&.scrollable-list {
+			--resizeVertical-defaultHeight: 39.5rem;
+		}
+
+		.nft-contract {
+			position: relative;
+
+			&:has(> [data-state="open"]) {
+				&:not(.is-single) {
+					grid-column: 1 / -1;
+				}
+				grid-row-end: span 6;
+			}
+
+			header {
+				> * {
+					margin: -0.25em 0;
+				}
+
+				.nft-contract-logo {
+					width: fit-content;
+					height: 1.75em;
+					border-radius: 0.3em;
+				}
+
+				.nfts-preview {
+					display: flex;
+					flex-direction: row;
+
+					> :global(* + *) {
+						margin-inline-start: -1em;
+					}
+
+					> :global(.nft-image img) {
+						border-radius: 3px;
+					}
+				}
+			}
+
+			.nfts {
+				display: grid;
+				grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
+				grid-template-rows: masonry;
+				align-items: stretch;
+
+				--padding-inner: 1.5em;
+				gap: var(--padding-inner);
+
+				&.scrollable-list {
+					--resizeVertical-defaultHeight: 31rem;
+				}
+
+				.nft {
+					--padding-inner: 0.75em;
+					gap: var(--padding-inner);
+
+					> a {
+						display: grid;
+					}
+
+					header {
+						row-gap: 0.15em;
+					}
+
+					/* * {
+						overflow: hidden;
+						text-overflow: ellipsis;
+					} */
+
+					:global(.nft-image img) {
+						width: 100%;
+						border-radius: 0.33rem;
+						margin: auto;
+						background-color: rgba(0, 0, 0, 0.1);
+						object-fit: contain;
+						aspect-ratio: auto 1 / 1;
+					}
+
+					.nft-name {
+						gap: 0.5ch;
+						overflow: hidden;
+						text-overflow: ellipsis;
+
+						.nft-count {
+							font-size: 0.85em;
+							opacity: 0.8;
+						}
+					}
+
+					.description {
+						font-size: 0.85em;
+						line-height: 1.3;
+					}
+
+					.attributes {
+						font-size: 0.8em;
+						line-height: 1.25;
+						row-gap: 0.5em;
+						opacity: 0.8;
+					}
+
+					.token-id {
+						display: inline-block;
+						line-height: 1.25;
+						word-break: break-all;
+					}
+				}
+			}
+		}
 	}
 
-	.nfts {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(10rem, 1fr));
-		grid-template-rows: masonry;
-		align-items: stretch;
-
-		--padding-inner: 1.5em;
-		gap: var(--padding-inner);
-	}
-	.nfts.scrollable-list {
-		--resizeVertical-defaultHeight: 31rem;
-	}
-
-	.nft {
-		--padding-inner: 0.75em;
-		gap: var(--padding-inner);
-	}
-	.nft > a {
-		display: grid;
-	}
-	.nft header {
-		row-gap: 0.15em;
-	}
-	/* .nft * {
-		overflow: hidden;
-		text-overflow: ellipsis;
-	} */
-
-	.nft :global(.nft-image img) {
-		width: 100%;
-		border-radius: 0.33rem;
-		margin: auto;
-		background-color: rgba(0, 0, 0, 0.1);
-		object-fit: contain;
-		aspect-ratio: auto 1 / 1;
-	}
-
-	.nft .nft-name {
-		gap: 0.5ch;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-	.nft .nft-name .nft-count {
-		font-size: 0.85em;
-		opacity: 0.8;
-	}
-
-	.description {
-		font-size: 0.85em;
-		line-height: 1.3;
-	}
-	.attributes {
-		font-size: 0.8em;
-		line-height: 1.25;
-		row-gap: 0.5em;
-		opacity: 0.8;
-	}
-
-	.token-id {
-		display: inline-block;
-		line-height: 1.25;
-		word-break: break-all;
-	}
-
-
-	.showImagesOnly.nft-contracts {
+	.nft-contracts.showImagesOnly {
 		--grid-unit-size: 0.125rem;
 		--grid-item-width: 24;
 		--grid-item-height: 24;
@@ -191,144 +206,166 @@
 		grid-template-columns: repeat(auto-fit, minmax(var(--grid-unit-size), auto));
 		grid-auto-columns: min-content;
 		gap: 0.25em;
-	}
-	.showImagesOnly .nft-contract,
-	.showImagesOnly .nfts {
-		display: contents;
-	}
-	.showImagesOnly .nft {
-		overflow: hidden;
-		border-radius: 0.33rem;
-		grid-column-end: span var(--grid-item-width);
-		grid-row-end: span var(--grid-item-height);
 
-		scroll-snap-align: start;
-	}
-	.showImagesOnly .nft figcaption {
-		align-self: end;
-		padding: var(--padding-inner);
+		.nft-contract {
+			display: contents;
 
-		z-index: 1;
+			.nfts {
+				display: contents;
+			}
 
-		/* min-height: min(max-content, 100%);
-		height: min(max-content, 100%); */
+			.nft {
+				overflow: hidden;
+				border-radius: 0.33rem;
+				grid-column-end: span var(--grid-item-width);
+				grid-row-end: span var(--grid-item-height);
+	
+				scroll-snap-align: start;
+	
+				figcaption {
+					align-self: end;
+					padding: var(--padding-inner);
+	
+					z-index: 1;
+	
+					/* min-height: min(max-content, 100%);
+					height: min(max-content, 100%); */
+	
+					/* max-height: 0; */
+					min-height: 100%;
+					max-height: 0;
+	
+					opacity: 0;
+					/* transform: translateY(100%); */
+					transform: scale(0.95) translateZ(-1rem);
+					transition: 0.15s;
+				}
+	
+				&:hover figcaption {
+					/* backdrop-filter: blur(5px); */
+					background-color: rgba(var(--rgb-light-dark), 0.33);
+					opacity: 1;
+					transform: translateZ(1rem);
+					overflow-y: auto;
+				}
+	
+				header {
+					position: sticky;
+					top: 0;
+				}
+			}
+			
+			& > header {
+				display: none;
+			}
+			& > hr {
+				display: none;
+			}
+		}
+	}
 
-		/* max-height: 0; */
-		min-height: 100%;
-		max-height: 0;
-
-		opacity: 0;
-		/* transform: translateY(100%); */
-		transform: scale(0.95) translateZ(-1rem);
-		transition: 0.15s;
-	}
-	.showImagesOnly .nft:hover figcaption {
-		/* backdrop-filter: blur(5px); */
-		background-color: rgba(var(--rgb-light-dark), 0.33);
-		opacity: 1;
-		transform: translateZ(1rem);
-		overflow-y: auto;
-	}
-	.showImagesOnly .nft header {
-		position: sticky;
-		top: 0;
-	}
-	.showImagesOnly .nft-contract > header,
-	.showImagesOnly .nft-contract > hr {
-		display: none;
-	}
-
-	.show3D.nft-contracts {
+	.nft-contracts.show3D {
 		--perspective: 1000px;
 		--transition-duration: 0.3s;
 
 		perspective: var(--perspective);
 		transform-style: preserve-3d;
-	}
-	.show3D .nft-contract {
-		transition: var(--transition-duration), background-color var(--transition-duration) calc(var(--transition-duration) * 2/3);
-	}
-	.show3D .nft-contract:not(:focus-within) {
-		background-color: transparent;
-		box-shadow: 0 0 1px 1px var(--card-background-color) inset;
-		transition: var(--transition-duration);
-	}
-	.show3D:not(.showImagesOnly) .nft-contract:before {
-		--angle: -0.075turn;
-		content: '';
-		position: absolute;
-		inset: 0;
-		transform: rotateX(var(--angle));
-		transform-origin: top;
-		/* background-color: var(--card-background-color); */
-		border-radius: inherit;
-		transition: 2s;
+
+		.nft-contract {
+			transition: var(--transition-duration), background-color var(--transition-duration) calc(var(--transition-duration) * 2/3);
+
+			&:not(:focus-within) {
+				background-color: transparent;
+				box-shadow: 0 0 1px 1px var(--card-background-color) inset;
+				transition: var(--transition-duration);
+			}
+		}
+
+		&:not(.showImagesOnly) .nft-contract {
+			&:before {
+				--angle: -0.075turn;
+				content: '';
+				position: absolute;
+				inset: 0;
+				transform: rotateX(var(--angle));
+				transform-origin: top;
+				/* background-color: var(--card-background-color); */
+				border-radius: inherit;
+				transition: 2s;
 
 
-		/* --parallax-factor: 1.1;
-		transform: translateZ(calc(var(--perspective) * (1 - var(--parallax-factor)))) scale(var(--parallax-factor));
-		transition: transform 0.3s;
-		z-index: calc(100 * var(--parallax-factor)); */
-	}
-	.show3D:not(.showImagesOnly) .nft-contract:not(:focus-within):before {
-		transform-origin: top;
-		backdrop-filter: blur(10px);
-	}
-	.show3D .nft {
-		--angle: -0.075turn;
-		--grid-height: 8em;
+				/* --parallax-factor: 1.1;
+				transform: translateZ(calc(var(--perspective) * (1 - var(--parallax-factor)))) scale(var(--parallax-factor));
+				transition: transform 0.3s;
+				z-index: calc(100 * var(--parallax-factor)); */
+			}
 
-		overflow: visible;
-		height: var(--grid-height);
-		transform: rotateX(var(--angle));
-		transform-origin: top;
-		display: grid;
-		grid-auto-flow: row reverse;
+			&:not(:focus-within):before {
+				transform-origin: top;
+				backdrop-filter: blur(10px);
+			}
+		}
 
-		transition: var(--transition-duration);
-		will-change: transform;
-		animation-delay: var(--transition-duration);
-	}
-	.show3D .nft:focus-within,
-	.show3D:not(.showImagesOnly) .nft-contract:focus-within .nft,
-	.show3D.showImagesOnly .nft:focus {
-		height: auto;
-		--angle: 0;
-	}
-	.show3D .nft :global(.nft-image) {
-		/* mask: linear-gradient(white, transparent) no-repeat; */
-		/* mask-image: linear-gradient(white, transparent); */
-		/* mask-image: linear-gradient(white 20%, transparent 80%); */
+		.nft {
+			--angle: -0.075turn;
+			--grid-height: 8em;
 
-		--wipe-gradient-height: 0.333;
-		--total-height: calc(2 + var(--wipe-gradient-height));
+			overflow: visible;
+			height: var(--grid-height);
+			transform: rotateX(var(--angle));
+			transform-origin: top;
+			display: grid;
+			grid-auto-flow: row reverse;
 
-		mask:
-			linear-gradient(
-				178deg,
-				white calc(100% / var(--total-height)),
-				transparent calc(100% * (1 + var(--wipe-gradient-height)) / var(--total-height))
-			)
-			50% / 100% calc(100% * var(--total-height))
-			no-repeat;
+			transition: var(--transition-duration);
+			will-change: transform;
+			animation-delay: var(--transition-duration);
 
-		-webkit-mask-position-y: calc(50% + var(--grid-height) / 2);
+			&:focus-within,
+			.show3D:not(.showImagesOnly) .nft-contract:focus-within &,
+			&:focus {
+				height: auto;
+				--angle: 0;
+			}
 
-		backdrop-filter: blur(6px);
+			:global(.nft-image) {
+				/* mask: linear-gradient(white, transparent) no-repeat; */
+				/* mask-image: linear-gradient(white, transparent); */
+				/* mask-image: linear-gradient(white 20%, transparent 80%); */
 
-		transition: var(--transition-duration);
-		will-change: transform;
-	}
-	.show3D .nft:focus-within :global(.nft-image),
-	.show3D:not(.showImagesOnly) .nft-contract:focus-within .nft :global(.nft-image) {
-		-webkit-mask-position-y: top;
-	}
-	.show3D .nft figcaption {
-		order: -1;
-		transform: rotateX(calc(-1 * var(--angle)));
-		transform-origin: bottom;
-		transform-origin: top;
-		transition: var(--transition-duration);
+				--wipe-gradient-height: 0.333;
+				--total-height: calc(2 + var(--wipe-gradient-height));
+
+				mask:
+					linear-gradient(
+						178deg,
+						white calc(100% / var(--total-height)),
+						transparent calc(100% * (1 + var(--wipe-gradient-height)) / var(--total-height))
+					)
+					50% / 100% calc(100% * var(--total-height))
+					no-repeat;
+
+					-webkit-mask-position-y: calc(50% + var(--grid-height) / 2);
+
+				backdrop-filter: blur(6px);
+
+				transition: var(--transition-duration);
+				will-change: transform;
+			}
+
+			&:focus-within :global(.nft-image),
+			.nft-contract:focus-within & :global(.nft-image) {
+				-webkit-mask-position-y: top;
+			}
+
+			figcaption {
+				order: -1;
+				transform: rotateX(calc(-1 * var(--angle)));
+				transform-origin: bottom;
+				transform-origin: top;
+				transition: var(--transition-duration);
+			}
+		}
 	}
 
 	.empty {
