d66779c25fcce9e87dff2ce0ed1ae9759a122df9
Darryl Yeo <dev@darryl-yeo.com>
2024-08-08 10:15:32 -0500

    Network providers: format

--- src/data/networkProviders/alchemy.ts
+++ src/data/networkProviders/alchemy.ts
@@ -1,3 +1,4 @@
+// Types/constants
 import type { Ethereum } from '../networks/types'
 
 export const alchemyProviderConfigs: {
@@ -5,54 +6,54 @@ export const alchemyProviderConfigs: {
 	subdomain: string,
 	networkSlug: Ethereum.NetworkSlug,
 }[] = [
-    {
-        label: 'mainnet',
-        subdomain: 'eth-mainnet',
-        networkSlug: 'ethereum',
-    },
-    {
-        label: 'rinkeby',
-        subdomain: 'eth-rinkeby',
-        networkSlug: 'ethereum-rinkeby',
-    },
-    {
-        label: 'goerli',
-        subdomain: 'eth-goerli',
-        networkSlug: 'ethereum-goerli',
-    },
-    {
-        label: 'sepolia',
-        subdomain: 'eth-sepolia',
-        networkSlug: 'ethereum-sepolia',
-    },
-    {
-        label: 'arbitrum',
-        subdomain: 'arb-mainnet',
-        networkSlug: 'arbitrum-one',
-    },
-    {
-        label: 'arbitrum-goerli',
-        subdomain: 'arb-goerli',
-        networkSlug: 'arbitrum-goerli',
-    },
-    {
-        label: 'matic',
-        subdomain: 'polygon-mainnet',
-        networkSlug: 'polygon',
-    },
-    {
-        label: 'matic-mumbai',
-        subdomain: 'polygon-mumbai',
-        networkSlug: 'polygon-mumbai',
-    },
-    {
-        label: 'optimism',
-        subdomain: 'opt-mainnet',
-        networkSlug: 'optimism',
-    },
-    {
-        label: 'optimism-goerli',
-        subdomain: 'opt-goerli',
-        networkSlug: 'optimistic-goerli',
-    },
+	{
+		label: 'mainnet',
+		subdomain: 'eth-mainnet',
+		networkSlug: 'ethereum',
+	},
+	{
+		label: 'rinkeby',
+		subdomain: 'eth-rinkeby',
+		networkSlug: 'ethereum-rinkeby',
+	},
+	{
+		label: 'goerli',
+		subdomain: 'eth-goerli',
+		networkSlug: 'ethereum-goerli',
+	},
+	{
+		label: 'sepolia',
+		subdomain: 'eth-sepolia',
+		networkSlug: 'ethereum-sepolia',
+	},
+	{
+		label: 'arbitrum',
+		subdomain: 'arb-mainnet',
+		networkSlug: 'arbitrum-one',
+	},
+	{
+		label: 'arbitrum-goerli',
+		subdomain: 'arb-goerli',
+		networkSlug: 'arbitrum-goerli',
+	},
+	{
+		label: 'matic',
+		subdomain: 'polygon-mainnet',
+		networkSlug: 'polygon',
+	},
+	{
+		label: 'matic-mumbai',
+		subdomain: 'polygon-mumbai',
+		networkSlug: 'polygon-mumbai',
+	},
+	{
+		label: 'optimism',
+		subdomain: 'opt-mainnet',
+		networkSlug: 'optimism',
+	},
+	{
+		label: 'optimism-goerli',
+		subdomain: 'opt-goerli',
+		networkSlug: 'optimistic-goerli',
+	},
 ]
