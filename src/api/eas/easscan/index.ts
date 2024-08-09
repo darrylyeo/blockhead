cf7ea4387b4df5ca06b38e037ac686947fa165df
Darryl Yeo <dev@darryl-yeo.com>
2024-08-09 02:28:09 -0500

    Ethereum Attestation Service: query and display schemas and attestations from Easscan

--- src/api/eas/easscan/index.ts
+++ src/api/eas/easscan/index.ts
@@ -95,3 +95,255 @@ export const getClient = ({
 
 	return client
 }
+
+
+// Fragments
+const Schema = graphql(`
+	fragment Schema on Schema @_unmask {
+		id
+		time
+		schema
+		_count {
+			attestations
+			schemaNames
+		}
+		creator
+		id
+		index
+		resolver
+		revocable
+		schema
+		schemaNames {
+			attesterAddress
+			id
+			isCreator
+			name
+			schema {
+				id
+				time
+				schema
+				_count {
+					attestations
+					schemaNames
+				}
+				creator
+				id
+				index
+				resolver
+				revocable
+				schema
+				schemaNames {
+					attesterAddress
+					id
+					isCreator
+					name
+					schema {
+						id
+						time
+						schema
+						_count {
+							attestations
+							schemaNames
+						}
+						creator
+						id
+						index
+						resolver
+						revocable
+						schema
+						# schemaNames {
+							
+						# }
+						time
+						txid
+					}
+					schemaId
+					time
+				}
+				time
+				txid
+			}
+			schemaId
+			time
+		}
+		time
+		txid
+	}
+`)
+
+const Attestation = graphql(`
+	fragment Attestation on Attestation @_unmask {
+		id
+		attester
+		data
+		decodedDataJson
+		expirationTime
+		ipfsHash
+		isOffchain
+		recipient
+		refUID
+		revocable
+		revocationTime
+		revoked
+		schema {
+			...Schema
+		}
+		schemaId
+		time
+		timeCreated
+		txid
+	}
+`, [
+	Schema,
+])
+
+
+// Queries
+export const getSchemas = async ({
+	chainId,
+	orderByTime = 'desc',
+	take = 100,
+	skip = 0,
+}: {
+	chainId: number,
+	orderByTime: 'asc' | 'desc',
+	take: number,
+	skip: number,
+}) => (
+	await getClient({ chainId })
+		.query(
+			graphql(`
+				query Schemas(
+					$orderByTime: SortOrder
+					$take: Int!
+					$skip: Int!
+				) {
+					schemata(
+						orderBy: {
+							time: $orderByTime
+						}
+						take: $take
+						skip: $skip
+					) {
+						...Schema
+						attestations {
+							...Attestation
+						}
+					}
+				}
+			`, [
+				Attestation
+			]),
+			{
+				orderByTime,
+				take,
+				skip,
+			},
+		)
+		.toPromise()
+		.then(handleUrqlResult)
+)
+
+export const getSchema = async ({
+	chainId,
+	schemaId,
+}: {
+	chainId: number,
+	schemaId: string,
+}) => (
+	await getClient({ chainId })
+		.query(
+			graphql(`
+				query Schema(
+					$schemaId: String!
+				) {
+					schema(
+						where: {
+							id: $schemaId
+						}
+					) {
+						...Schema
+					}
+				}
+			`, [
+				Schema
+			]),
+			{ schemaId },
+		)
+		.toPromise()
+		.then(handleUrqlResult)
+)
+
+export const getAttestations = async ({
+	chainId,
+	orderByTime = 'desc',
+	take = 100,
+	skip = 0,
+}: {
+	chainId: number,
+	orderByTime: 'asc' | 'desc',
+	take: number,
+	skip: number,
+}) => (
+	await getClient({ chainId })
+		.query(
+			graphql(`
+				query Attestations(
+					$orderByTime: SortOrder
+					$take: Int!
+					$skip: Int!
+				) {
+					attestations(
+						orderBy: {
+							time: $orderByTime
+						}
+						take: $take
+						skip: $skip
+					) {
+						...Attestation
+					}
+				}
+			`, [
+				Attestation
+			]),
+			{
+				orderByTime,
+				take,
+				skip,
+			},
+		)
+		.toPromise()
+		.then(handleUrqlResult)
+)
+
+export const getAttestation = async ({
+	chainId,
+	attestationId,
+}: {
+	chainId: number,
+	attestationId: string,
+}) => (
+	await getClient({ chainId })
+		.query(
+			graphql(`
+				query Attestation(
+					$attestationId: String!
+				) {
+					attestation(
+						where: {
+							id: $attestationId
+						}
+					) {
+						...Attestation
+					}
+				}
+			`, [
+				Attestation
+			]),
+			{
+				attestationId,
+			},
+		)
+		.toPromise()
+		.then(handleUrqlResult)
+)
