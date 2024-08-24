b2eb8ec64685abde257a16833853dfdea1b0587a
Darryl Yeo <dev@darryl-yeo.com>
2024-08-23 20:18:59 -0500

    Moxie: set up GraphQL fragments for auction-related entities

--- src/api/moxie/auction/index.ts
+++ src/api/moxie/auction/index.ts
@@ -60,3 +60,119 @@ export const getClient = ({
 
 	return client
 }
+
+// Fragments
+export const Token = graphql(`
+	fragment Token on Token @_unmask {
+		id
+		symbol
+		decimals
+	}
+`)
+
+export const BlockInfo = graphql(`
+	fragment BlockInfo on BlockInfo @_unmask {
+		id
+		hash
+		blockNumber
+		timestamp
+	}
+`)
+
+export const User = graphql(`
+	fragment User on User @_unmask {
+		id
+		address
+		totalMoxieBid
+	}
+`)
+
+export const Order = graphql(`
+	fragment Order on Order @_unmask {
+		id
+		sellAmount
+		buyAmount
+		price
+		volume
+		timestamp
+		auctionId
+		auction {
+			id
+		}
+		user {
+			...User
+		}
+		userWalletAddress
+		status
+		txHash
+		blockInfo {
+			...BlockInfo
+		}
+		isExactOrder
+		encodedOrderId
+		lastUpdatedIndex
+		finalizedBuyAmount
+		finalizedSellAmount
+		refundAmount
+		finalTxHash
+	}
+`, [
+	User,
+	BlockInfo,
+])
+
+export const AuctionDetail = graphql(`
+	fragment AuctionDetail on AuctionDetail @_unmask {
+		id
+		auctionId
+		exactOrder {
+			...Order
+		}
+		highestPriceBidOrder {
+			...Order
+		}
+		lowestPriceBidOrder {
+			...Order
+		}
+		auctioningToken {
+			...Token
+		}
+		biddingToken {
+			...Token
+		}
+		auctionEndDate
+		orderCancellationEndDate
+		startingTimeStamp
+		minimumBiddingAmountPerOrder
+		minFundingThreshold
+		minBuyAmount
+		auctionSupply
+		allowListManager
+		allowListSigner
+		currentVolume
+		volumeClearingPriceOrder
+		currentClearingOrderSellAmount
+		currentClearingOrderBuyAmount
+		currentClearingOrderUserId
+		currentClearingPrice
+		currentBiddingAmount
+		currentSubjectTokenBidAmount
+		currentClearingOrderId
+		isAtomicClosureAllowed
+		isPrivateAuction
+		interestScore
+		uniqueBidders
+		isCleared
+		totalOrders
+		activeOrderCount
+		txHash
+		blockInfo {
+			...BlockInfo
+		}
+		minimumPriceInMoxie
+	}
+`, [
+	BlockInfo,
+	Order,
+	Token,
+])
