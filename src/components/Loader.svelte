1273fe2efc3b25c0c6023144af3665c24abc971b
Darryl Yeo <dev@darryl-yeo.com>
2024-11-05 03:51:35 -0600

    <Loader>: rewrite state and effects for Svelte 5

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -26,42 +26,94 @@
 
 
 	// Inputs
-	export let startImmediately = true
+	let {
+		startImmediately = true,
 
-	export let loadingIcon: string
-	export let loadingIconName: string
-	export let loadingMessage: string
-	export let loadingIconWidth = 25
+		loadingIcon,
+		loadingIconName,
+		loadingMessage,
+		loadingIconWidth = 25,
 
-	export let errorMessage: string
-	export let errorFunction: ((error: LoaderError) => string) | undefined
-	export let hideError = false
+		errorMessage,
+		errorFunction,
+		hideError = false,
 
-	export let fromPromise: (() => Promise<LoaderResult>) | undefined
-	export let fromStore: (() => Readable<ApolloResult<LoaderResult>> | Promise<Readable<ApolloResult<LoaderResult>>>) | undefined
-	// export let fromHoudiniQuery: (() => QueryStore<LoaderResult extends GraphQLObject ? LoaderResult : never, HoudiniQueryInput extends GraphQLVariables ? HoudiniQueryInput : never>) | undefined
-	export let fromQuery: (CreateQueryResult<LoaderResult, LoaderError>) | undefined
-	export let fromInfiniteQuery: (CreateInfiniteQueryResult<LoaderResult, LoaderError>) | undefined
+		fromPromise,
+		fromStore,
+		// fromHoudiniQuery,
+		fromQuery,
+		fromInfiniteQuery,
 
-	export let then: ((result: LoaderResult) => LoaderReturnResult) | undefined
-	export let whenLoaded: ((result: LoaderReturnResult) => void) | undefined
-	export let whenErrored: ((error: LoaderError) => void) | undefined
-	export let whenCanceled: (() => Promise<any>) | undefined
+		then,
+		whenLoaded,
+		whenErrored,
+		whenCanceled,
 
-	// (View options)
-	export let viewOptions: Partial<{
-		layout: LoaderLayout,
-		collapsibleType: 'label' | 'details',
+		// View options
+		viewOptions = {} as Partial<{
+			layout: LoaderLayout,
+			collapsibleType: 'label' | 'details',
 
-		showIf: ((then: LoaderReturnResult | undefined) => boolean | any) | undefined,
-		isOpen: boolean,
-		clip: boolean,
+			showIf: ((then: LoaderReturnResult | undefined) => boolean | any) | undefined,
+			isOpen: boolean,
+			clip: boolean,
 
-		containerClass: string,
-		contentClass: string,
-	}> = {}
+			containerClass: string,
+			contentClass: string,
+		}>,
 
-	$: ({
+		showStatusAndActions = false,
+
+		// Outputs
+		result = $bindable(),
+		pagination = $bindable(),
+
+		// Debugging
+		debug = dev,
+	}: {
+		startImmediately?: boolean
+
+		loadingIcon: string
+		loadingIconName: string
+		loadingMessage: string
+		loadingIconWidth?: number
+
+		errorMessage: string
+		errorFunction?: ((error: LoaderError) => string) | undefined
+		hideError?: boolean
+
+		fromPromise?: (() => Promise<LoaderResult>) | undefined
+		fromStore?: (() => Readable<ApolloResult<LoaderResult>> | Promise<Readable<ApolloResult<LoaderResult>>>) | undefined
+		fromQuery?: (CreateQueryResult<LoaderResult, LoaderError>) | undefined
+		fromInfiniteQuery?: (CreateInfiniteQueryResult<LoaderResult, LoaderError>) | undefined
+
+		then?: ((result: LoaderResult) => LoaderReturnResult) | undefined
+		whenLoaded?: ((result: LoaderReturnResult) => void) | undefined
+		whenErrored?: ((error: LoaderError) => void) | undefined
+		whenCanceled?: (() => Promise<any>) | undefined
+
+		// View options
+		viewOptions?: Partial<{
+			layout: LoaderLayout,
+			collapsibleType: 'label' | 'details',
+			showIf: ((then: LoaderReturnResult | undefined) => boolean | any) | undefined,
+			isOpen: boolean,
+			clip: boolean,
+			containerClass: string,
+			contentClass: string,
+		}>,
+
+		showStatusAndActions?: boolean
+
+		// Outputs
+		result: LoaderReturnResult
+		pagination: Pick<InfiniteQueryObserverResult<LoaderResult, LoaderError>, 'hasPreviousPage' | 'isFetchingPreviousPage' | 'fetchPreviousPage' | 'hasNextPage' | 'isFetchingNextPage' | 'fetchNextPage'> | undefined
+
+		// Debugging
+		debug: boolean
+	} = $props()
+
+	let {
 		layout = 'default',
 		collapsibleType = 'details',
 
@@ -71,38 +123,44 @@
 
 		containerClass = 'column-block',
 		contentClass = 'column-block',
-	} = viewOptions)
-
-	export let showStatusAndActions = false
-
-	// (Computed)
-	$: isHidden = layout === 'headless' || (status === LoadingStatus.Resolved && showIf && !showIf(result))
+	} = viewOptions
 
 
 	// Internal state
-	let promise: ReturnType<NonNullable<typeof fromPromise>> | undefined
-	let store: Awaited<ReturnType<NonNullable<typeof fromStore>>> | undefined
-	// let houdiniQuery: ReturnType<NonNullable<typeof fromHoudiniQuery>> | undefined
+	let promise = $state<ReturnType<NonNullable<typeof fromPromise>> | undefined>()
+	let store = $state<Awaited<ReturnType<NonNullable<typeof fromStore>>> | undefined>()
+	// let houdiniQuery = $state<ReturnType<NonNullable<typeof fromHoudiniQuery>> | undefined>()
 
 
 	// Outputs
-	let _result: LoaderResult
-	export let result: LoaderReturnResult
+	let _result = $state<LoaderResult>()
 
-	let status = result ? LoadingStatus.Resolved : startImmediately ? LoadingStatus.Loading : LoadingStatus.Idle
+	let status = $state(
+		result ?
+			LoadingStatus.Resolved
+		: startImmediately ?
+			LoadingStatus.Loading
+		:
+			LoadingStatus.Idle
+	)
 
-	let error: LoaderError
+	let error = $state<LoaderError>()
 
-	export let pagination: Pick<InfiniteQueryObserverResult<LoaderResult, LoaderError>, 'hasPreviousPage' | 'isFetchingPreviousPage' | 'fetchPreviousPage' | 'hasNextPage' | 'isFetchingNextPage' | 'fetchNextPage'> | undefined
-	$: pagination = $fromInfiniteQuery && {
-		hasPreviousPage: $fromInfiniteQuery.hasPreviousPage,
-		isFetchingPreviousPage: $fromInfiniteQuery.isFetchingPreviousPage,
-		fetchPreviousPage: $fromInfiniteQuery.fetchPreviousPage,
+	const isHidden = $derived(
+		layout === 'headless' || (status === LoadingStatus.Resolved && showIf && !showIf(result))
+	)
 
-		hasNextPage: $fromInfiniteQuery.hasNextPage,
-		isFetchingNextPage: $fromInfiniteQuery.isFetchingNextPage,
-		fetchNextPage: $fromInfiniteQuery.fetchNextPage,
-	}
+	$effect(() => {
+		pagination = $fromInfiniteQuery && {
+			hasPreviousPage: $fromInfiniteQuery.hasPreviousPage,
+			isFetchingPreviousPage: $fromInfiniteQuery.isFetchingPreviousPage,
+			fetchPreviousPage: $fromInfiniteQuery.fetchPreviousPage,
+
+			hasNextPage: $fromInfiniteQuery.hasNextPage,
+			isFetchingNextPage: $fromInfiniteQuery.isFetchingNextPage,
+			fetchNextPage: $fromInfiniteQuery.fetchNextPage,
+		}
+	})
 
 	// (Types)
 	type CollapsibleSlotProps = LoaderLayout extends 'collapsible'
@@ -181,159 +239,169 @@
 		// 	load()
 	}
 
-	$: if(status === LoadingStatus.Loading){
-		try {
-			if(fromPromise)
-				promise = fromPromise()
 
-			if(fromStore){
-				const storeOrPromise = fromStore()
+	// Effects
+	$effect(() => {
+		if(status === LoadingStatus.Loading){
+			try {
+				if(fromPromise)
+					promise = fromPromise()
 
-				if('then' in storeOrPromise)
-					storeOrPromise
-						.then(_ => store = _)
-						.catch?.(e => {
-							error = e
-							status = LoadingStatus.Errored
-						})
-				else
-					store = storeOrPromise
+				if(fromStore){
+					const storeOrPromise = fromStore()
+
+					if('then' in storeOrPromise)
+						storeOrPromise
+							.then(_ => store = _)
+							.catch?.(e => {
+								error = e
+								status = LoadingStatus.Errored
+							})
+					else
+						store = storeOrPromise
+				}
+
+				// if(fromHoudiniQuery)
+				// 	houdiniQuery = fromHoudiniQuery()
+
+				// if(fromQuery)
+				// 	fromQuery.setEnabled(true)
+
+				// if(fromInfiniteQuery)
+				// 	fromInfiniteQuery.setEnabled(true)
+			}catch(e){
+				error = e as LoaderError
+				status = LoadingStatus.Errored
 			}
-
-			// if(fromHoudiniQuery)
-			// 	houdiniQuery = fromHoudiniQuery()
-
-			// if(fromQuery)
-			// 	fromQuery.setEnabled(true)
-
-			// if(fromInfiniteQuery)
-			// 	fromInfiniteQuery.setEnabled(true)
-		}catch(e){
-			error = e as LoaderError
-			status = LoadingStatus.Errored
 		}
-	}
-	// else{
-	// 	if(fromQuery)
-	// 		fromQuery.setEnabled(false)
+		// else{
+		// 	if(fromQuery)
+		// 		fromQuery.setEnabled(false)
 
-	// 	if(fromInfiniteQuery)
-	// 		fromInfiniteQuery.setEnabled(false)
-	// }
+		// 	if(fromInfiniteQuery)
+		// 		fromInfiniteQuery.setEnabled(false)
+		// }
+	})
 
-	$: if(promise){
-		promise.then(result => {
-			_result = result
-			status = LoadingStatus.Resolved
-		}, _error => {
-			error = _error
-			status = LoadingStatus.Errored
-		})
-	}
-	else if(store && $store){
-		if($store.loading){
-			if($store.data)
+	$effect(() => {
+		if(promise){
+			promise.then(result => {
+				_result = result
+				status = LoadingStatus.Resolved
+			}, _error => {
+				error = _error
+				status = LoadingStatus.Errored
+			})
+		}
+		else if(store && $store){
+			if($store.loading){
+				if($store.data)
+					_result = $store.data
+				status = LoadingStatus.Loading
+			}
+			else if($store.error){
+				error = $store.error
+				status = LoadingStatus.Errored
+			}
+			else if($store.data){
 				_result = $store.data
-			status = LoadingStatus.Loading
+				status = LoadingStatus.Resolved
+			}
 		}
-		else if($store.error){
-			error = $store.error
-			status = LoadingStatus.Errored
-		}
-		else if($store.data){
-			_result = $store.data
-			status = LoadingStatus.Resolved
-		}
-	}
-	// else if(houdiniQuery && $houdiniQuery){
-	// 	if($houdiniQuery.loading){
-	// 		status = LoadingStatus.Loading
-	// 	}
-	// 	else if($houdiniQuery.error){
-	// 		error = $houdiniQuery.error
-	// 		status = LoadingStatus.Errored
-	// 	}
-	// 	else if($houdiniQuery.data){
-	// 		_result = $houdiniQuery.data
-	// 		status = LoadingStatus.Resolved
-	// 	}
-	// }
-	else if(fromQuery && $fromQuery){
-		if($fromQuery.isPlaceholderData){
-			status = LoadingStatus.Idle
-		}
-		else if($fromQuery.isInitialLoading){
-			if($fromQuery.data !== undefined)
+		// else if(houdiniQuery && $houdiniQuery){
+		// 	if($houdiniQuery.loading){
+		// 		status = LoadingStatus.Loading
+		// 	}
+		// 	else if($houdiniQuery.error){
+		// 		error = $houdiniQuery.error
+		// 		status = LoadingStatus.Errored
+		// 	}
+		// 	else if($houdiniQuery.data){
+		// 		_result = $houdiniQuery.data
+		// 		status = LoadingStatus.Resolved
+		// 	}
+		// }
+		else if(fromQuery && $fromQuery){
+			if($fromQuery.isPlaceholderData){
+				status = LoadingStatus.Idle
+			}
+			else if($fromQuery.isInitialLoading){
+				if($fromQuery.data !== undefined)
+					_result = $fromQuery.data
+				status = LoadingStatus.Loading
+			}
+			else if($fromQuery.isLoading){
+				status = LoadingStatus.Loading
+			}
+			else if($fromQuery.isRefetching){
+				status = LoadingStatus.Reloading
+			}
+			else if($fromQuery.isSuccess){
 				_result = $fromQuery.data
-			status = LoadingStatus.Loading
+				status = LoadingStatus.Resolved
+			}
+			else if($fromQuery.isError){
+				error = $fromQuery.error
+				status = LoadingStatus.Errored
+			}
 		}
-		else if($fromQuery.isLoading){
-			status = LoadingStatus.Loading
+		else if(fromInfiniteQuery && $fromInfiniteQuery){
+			if($fromInfiniteQuery.isPlaceholderData){
+				status = LoadingStatus.Idle
+			}
+			else if($fromInfiniteQuery.isLoading){
+				status = LoadingStatus.Loading
+			}
+			else if($fromInfiniteQuery.isRefetching){
+				status = LoadingStatus.Reloading
+			}
+			else if($fromInfiniteQuery.isSuccess){
+				_result = $fromInfiniteQuery.data
+				status = LoadingStatus.Resolved
+			}
+			else if($fromInfiniteQuery.isError){
+				error = $fromInfiniteQuery.error
+				status = LoadingStatus.Errored
+			}
 		}
-		else if($fromQuery.isRefetching){
-			status = LoadingStatus.Reloading
-		}
-		else if($fromQuery.isSuccess){
-			_result = $fromQuery.data
-			status = LoadingStatus.Resolved
-		}
-		else if($fromQuery.isError){
-			error = $fromQuery.error
-			status = LoadingStatus.Errored
-		}
-	}
-	else if(fromInfiniteQuery && $fromInfiniteQuery){
-		if($fromInfiniteQuery.isPlaceholderData){
-			status = LoadingStatus.Idle
-		}
-		else if($fromInfiniteQuery.isLoading){
-			status = LoadingStatus.Loading
-		}
-		else if($fromInfiniteQuery.isRefetching){
-			status = LoadingStatus.Reloading
-		}
-		else if($fromInfiniteQuery.isSuccess){
-			_result = $fromInfiniteQuery.data
-			status = LoadingStatus.Resolved
-		}
-		else if($fromInfiniteQuery.isError){
-			error = $fromInfiniteQuery.error
-			status = LoadingStatus.Errored
-		}
-	}
+	})
 
-	$: if(status === LoadingStatus.Resolved){
-		// (Computed)
-		try {
-			result = then ? then(_result) : _result as LoaderReturnResult
+	$effect(() => {
+		if(status === LoadingStatus.Resolved){
+			// (Computed)
+			try {
+				result = then ? then(_result) : _result as LoaderReturnResult
 
-			whenLoaded?.(result)
-		}catch(_error){
-			error = _error as LoaderError
-			status = LoadingStatus.Errored
+				whenLoaded?.(result)
+			}catch(_error){
+				error = _error as LoaderError
+				status = LoadingStatus.Errored
+			}
 		}
-	}
+	})
 
-	$: if(status === LoadingStatus.Errored){
-		console.error(error)
-		whenErrored?.(error)
-	}
+	$effect(() => {
+		if(status === LoadingStatus.Errored){
+			console.error(error)
+			whenErrored?.(error)
+		}
+	})
 
 
 	// Debugging
-	export let debug: boolean = dev
-
-	$: if(debug && result){
-		// (console.groupCollapsed || console.log)(loadingMessage ?? '', { status })
-		(console.group || console.log)(loadingMessage ?? '', { status })
-		// if(Array.isArray(result)){
-		// 	console.log({ status })
-		// 	console.table(result)
-		// }else{
-			console.log(result)
-		// }
-		console.groupEnd?.()
-	}
+	$effect(() => {
+		if(debug && result){
+			// (console.groupCollapsed || console.log)(loadingMessage ?? '', { status })
+			(console.group || console.log)(loadingMessage ?? '', { status })
+			// if(Array.isArray(result)){
+			// 	console.log({ status })
+			// 	console.table(result)
+			// }else{
+				console.log(result)
+			// }
+			console.groupEnd?.()
+		}
+	})
 
 
 	// Components
