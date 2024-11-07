1ba7437b3035582ab00db70aae7ea44c84106d74
Darryl Yeo <dev@darryl-yeo.com>
2024-11-07 06:43:11 -0600

    Token icons: designate more network icons as token icons

--- src/assets/tokenIcons.ts
+++ src/assets/tokenIcons.ts
@@ -2,14 +2,18 @@ import AAVE from './tokens/AAVE.svg'
 import AVAX from './tokens/AVAX.svg'
 import BNB from './tokens/BNB.svg'
 import BTC from './tokens/BTC.svg'
+import CELO from './networks/Celo.svg'
 import COMP from './tokens/COMP.svg'
+import CRO from './networks/Cronos.svg'
 import DAI from './tokens/DAI.svg'
 import DEGEN from './tokens/DEGEN.svg'
 import ENS from './tokens/ENS.svg'
 import ETH from './tokens/ETH.svg'
+import EVMOS from './networks/Evmos.svg'
 import FIL from './networks/Filecoin.svg'
 import FTM from './tokens/FTM.svg'
 import MATIC from './tokens/MATIC.svg'
+import METIS from './networks/Metis.svg'
 import MNT from './tokens/MNT.svg'
 import MOXIE from './icons/Moxie.svg'
 // import MOXIE from './tokens/MOXIE.svg'
@@ -25,14 +29,18 @@ export const tokenIcons = {
 	AVAX,
 	BNB,
 	BTC,
+	CELO,
+	CRO,
 	COMP,
 	DAI,
 	DEGEN,
+	EVMOS,
 	ENS,
 	ETH,
 	FIL,
 	FTM,
 	MATIC,
+	METIS,
 	MOXIE,
 	MKR,
 	MNT,
