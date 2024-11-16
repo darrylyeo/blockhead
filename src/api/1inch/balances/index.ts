aea75e6a8e7631fe9634288b080bb56b7d474489
Darryl Yeo <dev@darryl-yeo.com>
2024-11-16 16:17:42 -0600

    1inch: set up OpenAPI fetch client for Balance API

---
+++ src/api/1inch/balances/index.ts
@@ -0,0 +1,31 @@
+// Types
+import type { paths } from './api.d.ts'
+
+
+// Context
+import * as publicEnv from '$env/static/public'
+
+
+// Methods
+import { Fetcher, type FetchReturnType } from 'openapi-typescript-fetch'
+
+const fetcher = Fetcher.for<paths>()
+
+fetcher.configure({
+	baseUrl: '/api-proxy/https://api.1inch.dev/balance',
+	init: {
+		headers: new Headers({
+			'Authorization': `Bearer ${publicEnv.PUBLIC_ONEINCH_API_KEY}`,
+		}),
+	},
+	use: [
+		async (url, init, next) => {
+			const response = await next(url, init)
+
+			if(!response.ok)
+				throw new Error(`Error ${response.status}: ${response.statusText}`)
+
+			return response.data as FetchReturnType<typeof next>['data']
+		},
+	],
+})
