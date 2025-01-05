e61626b2b5f32857e146119d71716f216bc978c4
Darryl Yeo <dev@darryl-yeo.com>
2025-01-05 15:14:39 -0600

    useAriaGrabbed: use only `drag` and `pointer` events; account for `pointercancel` firing after `dragstart`; simulate `dragend` using `pointerup`

--- src/utils/useAriaGrabbed.ts
+++ src/utils/useAriaGrabbed.ts
@@ -2,53 +2,86 @@ export const useAriaGrabbed = () => {
 	const controller = new AbortController()
 	const signal = controller.signal
 
-	for(const [startEvent, endEvents] of (
-		[
-			['dragstart', ['dragend']],
-			['pointerdown', ['pointerup', 'pointercancel']],
-			['touchstart', ['touchend', 'touchcancel']],
-		] as const
-	))
-		globalThis.window?.addEventListener(
-			startEvent,
-			event => {
-				if(!(event.target instanceof Element)) return
+	globalThis.window?.addEventListener(
+		'dragstart',
+		event => {
+			const element = event.target
 
-				const element = (
-					startEvent === 'dragstart' ?
-						event.target
-					:
-						event.target?.closest('[draggable="true"]')
-				)
+			if(!(element instanceof Element)) return
 
-				if(!(element instanceof Element)) return
+			element.setAttribute('aria-grabbed', 'true')
 
-				element.setAttribute('aria-grabbed', 'false')
+			element.addEventListener(
+				'dragend',
+				() => {
+					element.removeAttribute('aria-grabbed')
+				},
+				{
+					capture: true,
+					passive: true,
+					once: true,
+				}
+			)
+		},
+		{
+			capture: true,
+			passive: true,
+			signal,
+		}
+	)
 
-				const controller = new AbortController()
-				const signal = controller.signal
+	globalThis.window?.addEventListener(
+		'pointerdown',
+		event => {
+			if(!(event.target instanceof Element)) return
 
-				for(const endEvent of endEvents)
-					element.addEventListener(
-						endEvent,
-						() => {
-							element.removeAttribute('aria-grabbed')
-							controller.abort()
-						},
-						{
-							capture: true,
-							passive: true,
-							once: true,
-							signal,
-						}
-					)
-			},
-			{
-				capture: true,
-				passive: true,
-				signal,
-			}
-		)
+			const element = event.target?.closest('[draggable="true"]')
+
+			if(!(element instanceof Element)) return
+
+			element.setAttribute('aria-grabbed', 'false')
+
+			const controller = new AbortController()
+			const signal = controller.signal
+
+			element.addEventListener(
+				'pointerup',
+				event => {
+					element.removeAttribute('aria-grabbed')
+					controller.abort()
+
+					element.dispatchEvent(new DragEvent('dragend', event))
+				},
+				{
+					capture: true,
+					passive: true,
+					once: true,
+					signal,
+				}
+			)
+
+			element.addEventListener(
+				'pointercancel',
+				() => {
+					if(element.getAttribute('aria-grabbed') === 'false')
+						element.removeAttribute('aria-grabbed')
+
+					controller.abort()
+				},
+				{
+					capture: true,
+					passive: true,
+					once: true,
+					signal,
+				}
+			)
+		},
+		{
+			capture: true,
+			passive: true,
+			signal,
+		}
+	)
 
 	return () => {
 		controller.abort()
