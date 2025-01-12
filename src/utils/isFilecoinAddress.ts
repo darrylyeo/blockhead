1c750c4a9290e6ebcf41ea78dad5ccdefca9cc15
Darryl Yeo <dev@darryl-yeo.com>
2025-01-12 06:41:51 -0600

    Filecoin: improve address matching pattern

--- src/utils/isFilecoinAddress.ts
+++ src/utils/isFilecoinAddress.ts
@@ -1,5 +1,5 @@
-export const filecoinAddressPattern = /[ft][0-4][0-9a-z]+/
+export const filecoinAddressPattern = /(?:[ft]0(?:[0]|[1-9][0-9]*)|[ft]1[1-9a-z]{39}|[ft]2[1-9a-z]{39}|[ft]3[1-9a-z]{84}|[ft]4[0-9]{2}f[0-9a-z]{39}|0x[0-9a-f]{40})/i
 
 export const isFilecoinAddress = (string: string) => (
-	string.match(new RegExp(`^${filecoinAddressPattern.source}$`)) !== null
+	string.match(new RegExp(`^${filecoinAddressPattern.source}$`, filecoinAddressPattern.flags)) !== null
 )
