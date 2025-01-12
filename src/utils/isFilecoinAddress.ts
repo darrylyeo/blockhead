75b9c572de1d610adf3718a2eac9614ca10b3b5e
Darryl Yeo <dev@darryl-yeo.com>
2025-01-12 15:25:49 -0600

    isFilecoinAddress: implement type predicate

--- src/utils/isFilecoinAddress.ts
+++ src/utils/isFilecoinAddress.ts
@@ -1,5 +1,11 @@
+// Types
+import type { Filecoin } from '$/data/filecoin'
+import type { Ethereum } from '$/data/networks/types'
+
+
+// Functions
 export const filecoinAddressPattern = /(?:[ft]0(?:[0]|[1-9][0-9]*)|[ft]1[1-9a-z]{39}|[ft]2[1-9a-z]{39}|[ft]3[1-9a-z]{84}|[ft]4[0-9]{2}f[0-9a-z]{39}|0x[0-9a-f]{40})/i
 
-export const isFilecoinAddress = (string: string) => (
+export const isFilecoinAddress = (string: string): string is Filecoin.Address | Ethereum.Address => (
 	string.match(new RegExp(`^${filecoinAddressPattern.source}$`, filecoinAddressPattern.flags)) !== null
 )
