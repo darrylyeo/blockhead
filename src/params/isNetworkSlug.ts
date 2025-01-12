0e44f90d1af34df589afab8d4fb1671a9849cbc1
Darryl Yeo <dev@darryl-yeo.com>
2025-01-11 22:18:20 -0600

    isNetworkSlug: check for non-Filecoin networks

--- src/params/isNetworkSlug.ts
+++ src/params/isNetworkSlug.ts
@@ -1,9 +1,17 @@
-import type { ParamMatcher } from '@sveltejs/kit'
-
+// Constants
 import { networkBySlug } from '$/data/networks'
+
+
+// Functions
+import { match as isNetworkSlugFilecoin } from '$/params/isNetworkSlugFilecoin'
+
+
+// Param matcher
+import type { ParamMatcher } from '@sveltejs/kit'
 
 const isNetworkSlug: ParamMatcher = (param) => (
 	networkBySlug.has(param)
+	&& !isNetworkSlugFilecoin(param)
 )
 
 export const match = isNetworkSlug
