f58857145b43a340242a80faf0e838d192df3024
Darryl Yeo <dev@darryl-yeo.com>
2025-01-20 17:10:19 -0600

    Filfox: set up methods with patched response types

--- src/api/filfox/index.ts
+++ src/api/filfox/index.ts
@@ -1,11 +1,52 @@
 // Types
 import type { paths } from './api.d.ts'
+import type { Overview, AddressInfo, AddressMessages, AddressTransfers, AddressMessageTransfers, AddressTokenTransfers, Tipset, Block, BlockMessages, Message } from './types'
+import type { DeepMerge } from '$/utils/DeepMerge'
+
+type GetJsonResponse<T> = {
+	get: {
+		responses: {
+			200: {
+				content: {
+					'application/json': T
+				}
+			}
+		}
+	}
+}
+
+type allPaths = DeepMerge<[
+	paths,
+	{
+		'/overview': GetJsonResponse<Overview>
+
+		'/address/{address}': GetJsonResponse<AddressInfo>
+
+		'/address/{address}/messages': GetJsonResponse<AddressMessages>
+
+		'/address/{address}/pending-messages': GetJsonResponse<AddressMessages>
+
+		'/address/{address}/transfers': GetJsonResponse<AddressTransfers>
+
+		'/address/{address}/message-transfers': GetJsonResponse<AddressMessageTransfers>
+
+		'/address/{address}/token-transfers': GetJsonResponse<AddressTokenTransfers>
+
+		'/tipset/{height}': GetJsonResponse<Tipset>
+
+		'/block/{cid}': GetJsonResponse<Block>
+
+		'/block/{cid}/messages': GetJsonResponse<BlockMessages>
+
+		'/message/{cid}': GetJsonResponse<Message>
+	}
+]>
 
 
 // Methods
 import { Fetcher, type FetchReturnType } from 'openapi-typescript-fetch'
 
-const fetcher = Fetcher.for<paths>()
+const fetcher = Fetcher.for<allPaths>()
 
 fetcher.configure({
 	baseUrl: 'https://filfox.info/api/v1',
@@ -16,7 +57,29 @@ fetcher.configure({
 			if(!response.ok)
 				throw new Error(`Error ${response.status}: ${response.statusText}`)
 
-			return response.data as FetchReturnType<typeof next>['data']
+			return response.data as FetchReturnType<typeof next>
 		},
 	],
 })
+
+export const getOverview = fetcher.path('/overview').method('get').create()
+
+export const getAddress = fetcher.path('/address/{address}').method('get').create()
+
+export const getAddressMessages = fetcher.path('/address/{address}/messages').method('get').create()
+
+export const getAddressPendingMessages = fetcher.path('/address/{address}/pending-messages').method('get').create()
+
+export const getAddressTransfers = fetcher.path('/address/{address}/transfers').method('get').create()
+
+export const getAddressMessageTransfers = fetcher.path('/address/{address}/message-transfers').method('get').create()
+
+export const getAddressTokenTransfers = fetcher.path('/address/{address}/token-transfers').method('get').create()
+
+export const getTipset = fetcher.path('/tipset/{height}').method('get').create()
+
+export const getBlock = fetcher.path('/block/{cid}').method('get').create()
+
+export const getBlockMessages = fetcher.path('/block/{cid}/messages').method('get').create()
+
+export const getMessage = fetcher.path('/message/{cid}').method('get').create()
---
+++ src/api/filfox/types.d.ts
@@ -0,0 +1,273 @@
+export type Overview = {
+	height: number
+	timestamp: number
+	totalRawBytePower: `${bigint}`
+	totalQualityAdjPower: `${bigint}`
+	totalRawBytePowerDelta: `${bigint}`
+	totalQualityAdjPowerDelta: `${bigint}`
+	accounts: number
+	activeMiners: number
+	activeMinersGrowth: number
+	totalMaxSupply: `${bigint}`
+	totalSupply: `${bigint}`
+	circulatingSupply: `${bigint}`
+	burntSupply: `${bigint}`
+	totalPledgeCollateral: `${bigint}`
+	totalMultisigLockedBalance: `${bigint}`
+	totalMarketPledge: `${bigint}`
+	blockReward: `${bigint}`
+	dailyMessages: number
+	dailyCoinsMined: `${bigint}`
+	averageTipsetInterval: number
+	averageTipsetBlocks: number
+	averageTipsetWeightedBlocks: number
+	baseFee: `${bigint}`
+	averageRewardPerByte: number
+	estimatedInitialPledgeCollateral: number
+	sealCost: `${bigint}`
+	price: number
+	priceChangePercentage: number
+}
+
+export type AddressInfo = {
+	id: string
+	robust: string
+	actor: string
+	createHeight: number
+	createTimestamp: number
+	lastSeenHeight: number
+	lastSeenTimestamp: number
+	balance: `${bigint}`
+	messageCount: number
+	transferCount: number
+	tokenTransferCount: number
+	timestamp: number
+	tokens: number
+	ownedMiners: string[]
+	workerMiners: string[]
+	benefitedMiners: string[]
+	address: string
+}
+
+export type Tipset = {
+	height: number
+	timestamp: number
+	messageCount: number
+	blocks: Block<false>[]
+}
+
+export type Block<
+	HasParentInfo extends boolean = boolean
+> = (
+	& {
+		cid: string
+
+		miner: string
+		minerTag?: {
+			name: string
+			signed: boolean
+		}
+
+		winCount: number
+		reward: `${bigint}`
+		penalty: `${bigint}`
+	}
+
+	& (
+		HasParentInfo extends true ?
+			{
+				height: number
+				timestamp: number
+				size: number
+
+				parents: string[]
+				parentBaseFee: `${bigint}`
+				parentStateRoot: string
+				parentWeight: `${bigint}`
+			}
+		:
+			{
+				height?: never
+				timestamp?: never
+				size?: never
+
+				parents?: never
+				parentBaseFee?: never
+				parentStateRoot?: never
+				parentWeight?: never
+			}
+	)
+
+	& {
+		messageCount: number
+	}
+)
+
+export type BlockMessages = {
+	totalCount: number
+	messages: BlockMessage[]
+	methods: string[]
+}
+
+export type BlockMessage = {
+	cid: string
+	from: string
+	fromTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	to: string
+	toTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	value: `${bigint}`
+	method: string
+	evmMethod?: string
+	params: string // `ghkr${string}`
+	receipt?: {
+		exitCode: 0 | 1
+		return?: string
+		gasUsed?: number
+	}
+}
+
+export type Message = {
+	cid: string
+	height: number
+	timestamp: number
+	confirmations: number
+	blocks: string[]
+	version: number
+	from: string
+	fromId: string
+	fromActor: string
+	fromTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	to: string
+	toId: string
+	toActor: string
+	toTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	nonce: number
+	value: `${bigint}`
+	gasLimit: number
+	gasFeeCap: `${bigint}`
+	gasPremium: `${bigint}`
+	method: string
+	methodNumber: number
+	evmMethod: string
+	params: string // `ghkr${string}` | `0x${string}`
+	receipt?: {
+		exitCode: 0 | 1
+		return: string // '' | `0x${string}`
+		gasUsed: number
+	}
+	size: number
+	error: string
+	baseFee: `${bigint}`
+	fee: {
+		baseFeeBurn: `${bigint}`
+		overEstimationBurn: `${bigint}`
+		minerPenalty: `${bigint}`
+		minerTip: `${bigint}`
+		refund: `${bigint}`
+	}
+	transfers: Transfer[]
+	ethTransactionHash?: `0x${string}`
+	eventLogCount: number
+	subcallCount: number
+	tokenTransfers: TokenTransfer[]
+}
+
+export type Transfer = {
+	from: string
+	fromId: string
+	fromTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	to: string
+	toId: string
+	toTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	value: `${bigint}`
+	type: 'miner-fee' | 'burn-fee' | 'transfer'
+}
+
+export type AddressMessages = {
+	messages: Message[]
+}
+
+export type AddressTransferType = (
+	| 'genesis'
+	| 'burn-fee'
+	| 'miner-fee'
+	| 'rebalance'
+	| 'receive'
+	| 'reward'
+	| 'send'
+	| 'transfer'
+)
+
+export type AddressTransfers = {
+	totalCount: number
+	transfers: AddressTransfer[]
+	types: AddressTransferType[]
+}
+
+export type AddressTransfer = {
+	height: number
+	timestamp: number
+	message?: string
+	from: string
+	fromTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	to: string
+	toTag?: {
+		name: '-' | string
+		signed: boolean
+	}
+	value: `${bigint}`
+	type: AddressTransferType
+}
+
+export type AddressMessageTransfers = {
+	totalCount: number
+	transfers: MessageTransfer[]
+}
+
+export type MessageTransfer = {
+	message: string
+	height: number
+	timestamp: number
+	value: `${bigint}`
+}
+
+export type AddressTokenTransfers = {
+	totalCount: number
+	transfers: TokenTransfer[]
+}
+
+export type TokenTransfer = {
+	height: number
+	timestamp: number
+	message: string
+	evmMethod: string
+	from: string
+	to: string
+	token: string
+	value: `${bigint}`
+	type: string
+	name: string
+	symbol: string
+	decimals: number
+}
