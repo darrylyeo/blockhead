2cc07edab90806ccd9f7dbf122a257d58f72571c
Darryl Yeo <dev@darryl-yeo.com>
2025-01-25 12:28:02 -0600

    <BytesCount>: format number with byte units

---
+++ src/components/BytesCount.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	// Inputs
+	export let bytesCount: number | bigint
+	export let format: Parameters<typeof formatByteCount>[1] | undefined = {
+		showDecimalPlaces: 2,
+	}
+
+
+	// Functions
+	import { formatByteCount } from '$/utils/formatByteCount'
+
+
+	// Internal state
+	$: (
+		[value, units] = (
+			formatByteCount(
+				bytesCount,
+				format
+			)
+				.split(' ')
+		)
+	)
+
+
+	// Components
+	import TweenedNumber from './TweenedNumber.svelte'
+</script>
+
+
+<span title={`${bytesCount} bytes`}
+	><TweenedNumber
+		value={Number(value)}
+		{format}
+	/>
+	{units
+}</span>
