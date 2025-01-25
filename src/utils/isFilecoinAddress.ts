0cb8f9cc418587f77abdc5ddf3105dac3b0b510a
Darryl Yeo <dev@darryl-yeo.com>
2025-01-24 23:12:43 -0600

    Filecoin: add support for 40-character `f2` addresses

--- src/utils/isFilecoinAddress.ts
+++ src/utils/isFilecoinAddress.ts
@@ -4,7 +4,7 @@ import type { Ethereum } from '$/data/networks/types'
 
 
 // Functions
-export const filecoinAddressPattern = /(?:[ft]0(?:[0-9]+)|[ft]1[1-9a-z]{39}|[ft]2[1-9a-z]{39}|[ft]3[1-9a-z]{84}|[ft]4[0-9]+f[0-9a-z]{39}|0x[0-9a-f]{40})/i
+export const filecoinAddressPattern = /(?:[ft]0(?:[0-9]+)|[ft]1[1-9a-z]{39}|[ft]2[1-9a-z]{38,39}|[ft]3[1-9a-z]{84}|[ft]4[0-9]+f[0-9a-z]{39}|0x[0-9a-f]{40})/i
 
 export const isFilecoinAddress = (string: string): string is Filecoin.Address | Ethereum.Address => (
 	string.match(new RegExp(`^${filecoinAddressPattern.source}$`, filecoinAddressPattern.flags)) !== null
