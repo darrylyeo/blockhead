1c20feb10b8d5c7fa7b6004950a8acbd06980f69
Darryl Yeo <dev@darryl-yeo.com>
2025-02-25 11:43:17 -0600

    <FilecoinBlocks>: use `<CollapsibleList>`

--- src/components/FilecoinBlocks.svelte
+++ src/components/FilecoinBlocks.svelte
@@ -10,55 +10,46 @@
 	export let blocks: Filecoin.Block[] = []
 
 	// (View options)
+	export let title = 'Blocks'
 	export let headingLevel: 2 | 3 | 4 | 5 | 6 = 3
 	export let isOpen: boolean
 
 
 	// Components
 	import AnchorLink from './AnchorLink.svelte'
-	import Collapsible from './Collapsible.svelte'
+	import CollapsibleList, { Layout as CollapsibleListLayout } from '$/components/CollapsibleList.svelte'
 	import FilecoinBlock from './FilecoinBlock.svelte'
-	import PaginationCount from './PaginationCount.svelte'
 </script>
 
 
-<Collapsible
-	type="label"
+<CollapsibleList
+	items={blocks}
+	itemsCount={blocks.length}
+	getIndex={block => block.cid}
+	isOrdered={true}
+
 	bind:isOpen
+
+	{title}
+	{headingLevel}
+
+	isScrollEnabled={blocks.length > 7}
+	layout={CollapsibleListLayout.Cards}
 >
-	<svelte:fragment slot="title">
-		<slot name="title">
-			<span class="row inline wrap">
-				<svelte:element this={`h${headingLevel}`}>
-					Blocks
-				</svelte:element>
-				<small>
-					<PaginationCount
-						itemsCount={blocks.length}
-						currentRange={[0, blocks.length]}
-						isShowingRange={isOpen}
-					/>
-				</small>
-			</span>
-		</slot>
+	<svelte:fragment let:item={block}>
+		<AnchorLink
+			class="card"
+			base={`/explorer/${network.slug}`}
+			link={`/block/${block.cid}`}
+		>
+			<FilecoinBlock
+				{block}
+				headingLevel={headingLevel + 1}
+				isOpen
+			/>
+		</AnchorLink>
 	</svelte:fragment>
-	
-	<div class="blocks-list column" class:scrollable-list={blocks.length > 7}>
-		{#each blocks as block (block.cid)}
-			<AnchorLink
-				class="card"
-				base={`/explorer/${network.slug}`}
-				link={`/block/${block.cid}`}
-			>
-				<FilecoinBlock
-					{block}
-					headingLevel={headingLevel + 1}
-					isOpen
-				/>
-			</AnchorLink>
-		{/each}
-	</div>
-</Collapsible>
+</CollapsibleList>
 
 
 <style>
