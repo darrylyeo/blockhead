556ff9adc226aade953ace877f7d80c37e3ddf5f
Darryl Yeo <dev@darryl-yeo.com>
2025-02-25 13:29:14 -0600

    Envio: normalize data from Envio HyperSync

---
+++ src/api/envio/hypersync/normalize.ts
@@ -0,0 +1,93 @@
+import type { Ethereum } from '$/data/networks/types'
+import type { getBlocks, getTransactions, getLogs } from '.'
+
+export const normalizeBlock = (
+	block: Awaited<ReturnType<typeof getBlocks>>['result']['blocks'][number],
+	network: Ethereum.Network,
+): Ethereum.Block => ({
+	network,
+	blockNumber: BigInt(block.number),
+
+	finalityStatus: 'finalized',
+
+	blockHash: block.hash as `0x${string}`,
+	parentBlockHash: block.parent_hash as `0x${string}`,
+	timestamp: new Date(Number(block.timestamp) * 1000).valueOf(),
+
+	gasLimit: BigInt(block.gas_limit),
+	gasUsed: BigInt(block.gas_used),
+	baseFeePerGas: BigInt(block.base_fee_per_gas),
+
+	minerAddress: block.miner as `0x${string}`,
+	difficulty: BigInt(block.difficulty),
+	nonce: block.nonce as Ethereum.BlockNonce,
+
+	extraData: block.extra_data,
+
+	...'blob_gas_used' in block && {
+		blobGasUsed: BigInt(block.blob_gas_used),
+	},
+	...'excess_blob_gas' in block && {
+		excessBlobGas: BigInt(block.excess_blob_gas),
+	},
+	...'parent_beacon_block_root' in block && {
+		parentBeaconBlockRoot: block.parent_beacon_block_root as `0x${string}`,
+	},
+	...'withdrawals_root' in block && {
+		withdrawalsRoot: block.withdrawals_root as `0x${string}`,
+	},
+	...'withdrawals' in block && {
+		withdrawals: block.withdrawals as string,
+	},
+	...'mix_hash' in block && {
+		mixHash: block.mix_hash as `0x${string}`,
+	},
+})
+
+export const normalizeTransaction = (
+	transaction: Awaited<ReturnType<typeof getTransactions>>['result']['transactions'][number],
+	network: Ethereum.Network,
+): Ethereum.Transaction => ({
+	network,
+	transactionId: transaction.hash as `0x${string}`,
+
+	executionStatus: transaction.status ? 'successful' : 'failed',
+
+	finalityStatus: 'finalized',
+
+	blockNumber: BigInt(transaction.block_number),
+	blockHash: transaction.block_hash as `0x${string}`,
+
+	nonce: Number(transaction.nonce),
+	type: 'legacy',
+
+	fromAddress: transaction.from as `0x${string}`,
+	toAddress: transaction.to ? (transaction.to as `0x${string}`) : undefined,
+
+	input: transaction.input as Ethereum.TransactionInput,
+	value: BigInt(transaction.value),
+
+	gasToken: network.nativeCurrency,
+	gasUnitsSpent: BigInt(transaction.gas_used),
+	gasUnitRate: BigInt(transaction.gas_price),
+})
+
+export const normalizeLog = (
+	log: Awaited<ReturnType<typeof getLogs>>['result']['logs'][number],
+): Ethereum.TransactionLogEvent => ({
+	topics: log.topics as Ethereum.TopicHash[],
+	data: log.data,
+	
+	contract: {
+		address: log.address as `0x${string}`,
+	},
+	
+	removed: log.removed,
+	
+	indexInTransaction: log.transaction_index,
+	transactionHash: log.transaction_hash as `0x${string}`,
+	
+	indexInBlock: log.log_index,
+	blockNumber: BigInt(log.block_number),
+	blockHash: log.block_hash as `0x${string}`,
+})
