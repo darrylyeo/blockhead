7216e31fa16aeb84fa6010660f7c301be67278cd
Darryl Yeo <dev@darryl-yeo.com>
2025-02-27 09:36:02 -0600

    <VerifiableCredentials>: use `<CollapsibleList>`

--- src/components/VerifiableCredentials.svelte
+++ src/components/VerifiableCredentials.svelte
@@ -7,35 +7,31 @@
 	export let credentials: Disco.VerifiableCredential[] = []
 	// (View options)
 	export let headingLevel: 2 | 3 | 4 | 5 | 6 = 4
-	export let isOpen: boolean
+	export let isOpen = true
 
 
 	// Components
-	import Collapsible from './Collapsible.svelte'
+	import CollapsibleList from './CollapsibleList.svelte'
 	import VerifiableCredential from './VerifiableCredential.svelte'
-	import TweenedNumber from './TweenedNumber.svelte'
 </script>
 
 
-<Collapsible
-	type="label"
+<CollapsibleList
+	items={credentials}
+	getIndex={credential => credential.id}
 	bind:isOpen
+	title="Credentials"
+	headingLevel={headingLevel}
+	isScrollEnabled={credentials.length > 2}
 >
-	<svelte:fragment slot="title">
-		<slot name="title">
-			<svelte:element this={`h${headingLevel}`}>
-				Credentials
-				<span>(<TweenedNumber value={credentials.length} />)</span>
-			</svelte:element>
-		</slot>
+	<svelte:fragment let:item={credential}>
+		<VerifiableCredential
+			{credential}
+			headingLevel={headingLevel + 1}
+		/>
 	</svelte:fragment>
 
-	<div class="transactions-list column" class:scrollable-list={credentials.length > 2}>
-		{#each credentials as credential}
-			<VerifiableCredential
-				{credential}
-				headingLevel={headingLevel + 1}
-			/>
-		{/each}
-	</div>
-</Collapsible>
+	<svelte:fragment slot="empty">
+		<div class="card">No credentials available.</div>
+	</svelte:fragment>
+</CollapsibleList>
