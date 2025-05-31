88ef2813ca379e01e5e1ceff5af31fe0a063b209
Darryl Yeo <dev@darryl-yeo.com>
2025-05-31 23:33:07 +0200

    GraphQL / Entities: use ID getter functions to define key getters for urql cache exchange

--- src/state/graphql/index.ts
+++ src/state/graphql/index.ts
@@ -1,3 +1,7 @@
+// Types/constants
+import { EntityType, getEntityId } from '$/entities'
+
+
 // gql.tada
 import { initGraphQLTada } from 'gql.tada'
 import type { introspection } from './api.d.ts'
@@ -19,7 +23,7 @@ export const graphql = initGraphQLTada<{
 // urql
 import { createClient } from '@urql/svelte'
 import schema from './schema.json'
-import { cacheExchange } from '@urql/exchange-graphcache'
+import { cacheExchange, type KeyGenerator } from '@urql/exchange-graphcache'
 
 export const client = createClient({
 	url: 'local:',
@@ -27,30 +31,29 @@ export const client = createClient({
 		cacheExchange({
 			schema,
 			keys: {
-				Actor: (data: any) => `${data.chainId}:${data.address}`,
-				App: (data: any) => data.slug,
-				Balance: (data: any) => `${data.chainId}:${data.id}`,
-				Block: (data: any) => `${data.chainId}:${data.blockNumber || data.blockHash}`,
-				Contract: (data: any) => `${data.chainId}:${data.address}`,
-				Event: (data: any) => `${data.chainId}:${data.id}`,
-				Network: (data: any) => `${data.chainId}`,
-				Nft: (data: any) => `${data.chainId}:${data.contractAddress}:${data.tokenId}`,
-				Storage: (data: any) => `${data.chainId}:${data.id}`,
-				Token: (data: any) => `${data.chainId}:${data.address}`,
-				Trace: (data: any) => `${data.chainId}:${data.id}`,
-				Transaction: (data: any) => `${data.chainId}:${data.id}`,
-				Transfer: (data: any) => `${data.chainId}:${data.id}`,
-				Validator: (data: any) => `${data.chainId}:${data.index}`,
-				// Interface implementations
-				EoaActor: (data: any) => `${data.chainId}:${data.address}`,
-				ContractActor: (data: any) => `${data.chainId}:${data.address}`,
-				NativeToken: (data: any) => `${data.chainId}:${data.address}`,
-				Erc20Token: (data: any) => `${data.chainId}:${data.address}`,
-				NativeBalance: (data: any) => `${data.chainId}:${data.id}`,
-				Erc20Balance: (data: any) => `${data.chainId}:${data.id}`,
-				TransferEvent: (data: any) => `${data.chainId}:${data.id}`,
-				SwapEvent: (data: any) => `${data.chainId}:${data.id}`,
-				ApprovalEvent: (data: any) => `${data.chainId}:${data.id}`,
+				[EntityType.Actor]: getEntityId[EntityType.Actor] as unknown as KeyGenerator,
+				[EntityType.App]: getEntityId[EntityType.App] as unknown as KeyGenerator,
+				[EntityType.Balance]: getEntityId[EntityType.Balance] as unknown as KeyGenerator,
+				[EntityType.Block]: getEntityId[EntityType.Block] as unknown as KeyGenerator,
+				[EntityType.Contract]: getEntityId[EntityType.Contract] as unknown as KeyGenerator,
+				[EntityType.Event]: getEntityId[EntityType.Event] as unknown as KeyGenerator,
+				[EntityType.Network]: getEntityId[EntityType.Network] as unknown as KeyGenerator,
+				[EntityType.Nft]: getEntityId[EntityType.Nft] as unknown as KeyGenerator,
+				[EntityType.Storage]: getEntityId[EntityType.Storage] as unknown as KeyGenerator,
+				[EntityType.Token]: getEntityId[EntityType.Token] as unknown as KeyGenerator,
+				[EntityType.Trace]: getEntityId[EntityType.Trace] as unknown as KeyGenerator,
+				[EntityType.Transaction]: getEntityId[EntityType.Transaction] as unknown as KeyGenerator,
+				[EntityType.Transfer]: getEntityId[EntityType.Transfer] as unknown as KeyGenerator,
+				[EntityType.Validator]: getEntityId[EntityType.Validator] as unknown as KeyGenerator,
+				EoaActor: getEntityId[EntityType.Actor] as unknown as KeyGenerator,
+				ContractActor: getEntityId[EntityType.Actor] as unknown as KeyGenerator,
+				NativeToken: getEntityId[EntityType.Token] as unknown as KeyGenerator,
+				Erc20Token: getEntityId[EntityType.Token] as unknown as KeyGenerator,
+				NativeBalance: getEntityId[EntityType.Balance] as unknown as KeyGenerator,
+				Erc20Balance: getEntityId[EntityType.Balance] as unknown as KeyGenerator,
+				TransferEvent: getEntityId[EntityType.Event] as unknown as KeyGenerator,
+				SwapEvent: getEntityId[EntityType.Event] as unknown as KeyGenerator,
+				ApprovalEvent: getEntityId[EntityType.Event] as unknown as KeyGenerator,
 			},
 		}),
 	],
