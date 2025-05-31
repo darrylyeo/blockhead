d854c2321e7f52fb0e095f65e3eb1065faafbe86
Darryl Yeo <dev@darryl-yeo.com>
2025-05-31 23:29:51 +0200

    Entities: model umbrella `Entity` type

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,12 +1,28 @@
+// Types/constants
+import type { Actor } from './actor'
+import type { App } from './app'
+import type { Balance } from './balance'
+import type { Block } from './block'
+import type { Contract } from './contract'
+import type { Event } from './event'
+import type { Network } from './network'
+import type { Nft } from './nft'
+import type { Storage } from './storage'
+import type { Token } from './token'
+import type { Trace } from './trace'
+import type { Transaction } from './transaction'
+import type { Transfer } from './transfer'
+import type { Validator } from './validator'
+
 export enum EntityType {
 	Actor = 'Actor',
 	App = 'App',
 	Balance = 'Balance',
-	Beacon = 'Beacon',
-	Blob = 'Blob',
+	// Beacon = 'Beacon',
+	// Blob = 'Blob',
 	Block = 'Block',
 	Contract = 'Contract',
-	Ens = 'Ens',
+	// Ens = 'Ens',
 	Event = 'Event',
 	Network = 'Network',
 	Nft = 'Nft',
@@ -15,6 +31,38 @@ export enum EntityType {
 	Trace = 'Trace',
 	Transaction = 'Transaction',
 	Transfer = 'Transfer',
-	Types = 'Types',
-	Validator = 'Validator'
+	Validator = 'Validator',
 }
+
+export type Entity<_EntityType extends EntityType = EntityType> = (
+	_EntityType extends EntityType.Actor ?
+		Actor
+	: _EntityType extends EntityType.App ?
+		App
+	: _EntityType extends EntityType.Balance ?
+		Balance
+	: _EntityType extends EntityType.Block ?
+		Block
+	: _EntityType extends EntityType.Contract ?
+		Contract
+	: _EntityType extends EntityType.Event ?
+		Event
+	: _EntityType extends EntityType.Network ?
+		Network
+	: _EntityType extends EntityType.Nft ?
+		Nft
+	: _EntityType extends EntityType.Storage ?
+		Storage
+	: _EntityType extends EntityType.Token ?
+		Token
+	: _EntityType extends EntityType.Trace ?
+		Trace
+	: _EntityType extends EntityType.Transaction ?
+		Transaction
+	: _EntityType extends EntityType.Transfer ?
+		Transfer
+	: _EntityType extends EntityType.Validator ?
+		Validator
+	:
+		never
+)
