da2c524c1c198b080f6d429602ce7b97690c60c3
Darryl Yeo <dev@darryl-yeo.com>
2025-05-31 09:41:19 +0200

    <Heading>: implement

---
+++ src/components/Heading.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// Types/constants
+	import type { Snippet } from 'svelte'
+
+
+	// Props
+	let {
+		level = 1,
+		children,
+	}: {
+		level?: 1 | 2 | 3 | 4 | 5 | 6
+		children: Snippet
+	} = $props()
+</script>
+
+
+<svelte:element this={`h${level}`}>
+	{@render children()}
+</svelte:element>
