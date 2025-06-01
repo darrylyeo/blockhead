1eec384bcaaa44a939f6d937245d7fd854129bff
Darryl Yeo <dev@darryl-yeo.com>
2025-06-01 09:16:36 +0200

    vite-plugin-kit-routes: setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { kitRoutes } from 'vite-plugin-kit-routes'
 
 import { parseGraphQLSchema } from '@magidoc/rollup-plugin-gql-schema'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		kitRoutes({
+			generated_file_path: './src/ROUTES.ts',
+		}),
 		parseGraphQLSchema({
 			paths: ['./src/state/graphql/schema.graphql'],
 			target: './src/state/graphql/schema.json',
