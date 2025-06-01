e6a536cecca59af332a4c2d0df34b6063696be61
Darryl Yeo <dev@darryl-yeo.com>
2025-06-01 06:30:21 +0200

    Blockscout: enable caching

--- src/api/blockscout/graphql/index.ts
+++ src/api/blockscout/graphql/index.ts
@@ -1,5 +1,5 @@
 // Types/constants
-import type { ChainId } from '$/entities/chain.js'
+import type { ChainId } from '$/entities/network'
 
 export const networks = {
 	1: 'https://eth.blockscout.com/api/v1/graphql',
@@ -11,7 +11,7 @@ export const networks = {
 // gql.tada
 import { initGraphQLTada } from 'gql.tada'
 import type { introspection } from './api.d.ts'
-const graphql = initGraphQLTada<{
+export const graphql = initGraphQLTada<{
 	introspection: introspection,
 }>()
 
@@ -22,8 +22,21 @@ import { cacheExchange } from '@urql/exchange-graphcache'
 
 export const createClient = (chainId: ChainId = 1) => (
 	new Client({
-		url: networks[chainId],
+		url: networks[chainId as keyof typeof networks],
 		exchanges: [
+			cacheExchange({
+				keys: {
+					Address: (data: any) => data.hash as string,
+					Block: (data: any) => data.hash as string,
+					InternalTransaction: (data: any) => data.id as string,
+					SmartContract: (data: any) => data.addressHash as string,
+					Token: (data: any) => data.contractAddressHash as string,
+					TokenTransfer: (data: any) => data.id as string,
+					Transaction: (data: any) => data.hash as string,
+				},
+				resolvers: {},
+				updates: {},
+			}),
 			fetchExchange,
 		],
 		fetchOptions: {
