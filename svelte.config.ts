77fdd35dc0e01ce77c8abcf2b705386367ffa31b
Darryl Yeo <dev@darryl-yeo.com>
2025-11-21 19:58:41 -0500

    SvelteKit: alias `$` to `src`

--- svelte.config.ts
+++ svelte.config.ts
@@ -11,6 +11,10 @@ export default {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+
+		alias: {
+			'$': './src',
+		},
+	},
 } as Config
