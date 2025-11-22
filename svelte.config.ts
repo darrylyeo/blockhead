98ca5101d1cd37da57c82da71582236ea3c69f1d
Darryl Yeo <dev@darryl-yeo.com>
2025-11-21 19:57:52 -0500

    Svelte: use TypeScript config

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,3 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import type { Config } from '@sveltejs/kit'
 
-export default { kit: { adapter: adapter() } };
+export default {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
+} as Config
