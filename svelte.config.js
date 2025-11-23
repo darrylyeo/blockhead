7eee02b46975ce308af6b76befca4499831f3b73
Darryl Yeo <dev@darryl-yeo.com>
2025-11-22 20:57:14 -0800

    Svelte: let Vite preprocess `<script>` tags

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		script: true,
+	}),
 
 	compilerOptions: {
 		runes: true,
