e0619ad71606c959e57a11f5ab770b81ddba4955
Darryl Yeo <dev@darryl-yeo.com>
2025-11-22 20:57:29 -0800

    Vite: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
-});
+	plugins: [
+		sveltekit(),
+		devtoolsJson(),
+	],
+})
